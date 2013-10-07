1c1d7a8b87d5863035379d173d981c5189837b35
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-10-06 21:31:42 -0400

    distance between two points working

--- test/distance.js
+++ test/distance.js
@@ -3,9 +3,15 @@ var g = require('../index')
 describe('distance', function(){
   describe('#index', function(){
     it('should return the distance between two points', function(done){
-      g.distance([], 'miles', function(err, distance){
+      g.load('../test/testFiles/Point1.geojson', function(err, point1){
         if(err) throw err
-        done()
+        g.load('../test/testFiles/Point2.geojson', function(err, point2){
+          if(err) throw err
+          g.distance(point1, point2, 'miles', function(err, distance){
+            if(err) throw err
+            done()
+          })
+        })
       })
     })
   })
