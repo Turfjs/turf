28f95786fee9dd3bce223a8d67d7d7a4bf7ac04f
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-10-22 13:59:12 -0400

    contours being output as a feature collection now. shapes look off.

--- test/contour.js
+++ test/contour.js
@@ -5,7 +5,7 @@ var g = require('../index'),
 describe('contour', function(){
   it('should take a set of points with z values and output a set of contour polygons', function(done){
     g.load('../test/testIn/Points3.geojson', function(err, points){
-      g.contour(points, 'elevation', [0,20,40,60,80,100,120,140,160], function(err, contours){
+      g.contour(points, 'elevation', [0, 1,2,3,4,5,6,7,8,9,10,20,40,60,80,100,120,140,160], function(err, contours){
         if(err) throw err
         contours.should.be.ok
         done()
