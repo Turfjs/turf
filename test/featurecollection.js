8daf8efeb152f8fb09d16fb3dd38a97dfdfd90be
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-10-24 09:52:19 -0400

    improved feature collection test

--- test/featurecollection.js
+++ test/featurecollection.js
@@ -1,10 +1,15 @@
 var g = require('../index'),
   should = require('should')
 
-describe('feature', function(){
+describe('featurecollection', function(){
   it('should return a feature collection', function(done){
-    var fc = g.featurecollection(null)
+    var p1 = g.point(0,0),
+        p2 = g.point(0,10),
+        p3 = g.point(10,10),
+        p4 = g.point(10,0)
+    var fc = g.featurecollection([p1,p2,p3,p4])
     fc.should.be.ok
+    fc.feature.length.should.eqaul(4)
     done()
   })
 }) 
