62bc57360b522b1b7a4b1d2f1082a316dfe578ce
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-10-24 09:07:37 -0400

    stubbed out explode()

---
+++ test/explode.js
@@ -0,0 +1,9 @@
+var g = require('../index'),
+  should = require('should')
+
+describe('explode', function(){
+  it('should take a feature or feature collection and return all vertices', function(done){
+
+    done()
+  })
+}) 
