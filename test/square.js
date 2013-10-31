12a9dab7a5490de308523372d62e27ee7521ab50
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-10-31 16:36:52 -0400

    stubbed out t.square()

---
+++ test/square.js
@@ -0,0 +1,9 @@
+var t = require('../index'),
+  should = require('should')
+
+describe('square', function(){
+  it('should return a bbox representing the smallest square that contains the input bbox', function(done){
+    
+    done()
+  })
+}) 
