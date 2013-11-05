fae1dd724eb2f5c9da04dfdb3e0ecbdf1e83fb45
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-05 16:55:30 -0500

    implemented reclass and defaulted save to geojson

--- test/reclass.js
+++ test/reclass.js
@@ -1,8 +1,22 @@
-var t = require('../lib/reclass')
+var t = require('../index'),
+    should = require('should')
 
 describe('reclass', function(){
-  xit('should ', function(done){
-    throw new Error('not implemented')
-    done()
+  it('should take a feature collection and an array of translations and return a new featurecollection reclassed', function(done){
+    var inField = 'elevation',
+        outField = 'heightIndex',
+        translations = [[0, 20, 1], [20, 40, 2], [40, 60 , 3], [60, Infinity, 4]]
+
+    t.load('./testIn/Points3.geojson', function(err, pts){
+      if(err) throw err
+      pts.should.be.ok
+      t.reclass(pts, inField, outField, translations, function(err, outPts){
+        if(err) throw err
+        outPts.should.be.ok
+        outPts.features[0].geometry.type.should.equal('Point')
+        t.save('./testOut/reclassed.geojson', outPts, 'geojson')
+        done()
+      })
+    })
   })
-}) 
+})
