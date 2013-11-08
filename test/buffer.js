d46c4a3d0c53ef0f02de8cb0f4dcfb06d1971da2
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-07 21:57:33 -0500

    added unit option to buffer

--- test/buffer.js
+++ test/buffer.js
@@ -3,8 +3,8 @@ var t = require('../index'), fs = require('fs')
 describe('buffer', function(){
   it('should buffer a Point', function(done){
     t.load('../test/testIn/Point1.geojson', function(err, pt){
-      t.buffer(pt, 10, function(err, buffered){
-        //fs.writeFileSync('./testOut/test.geojson',JSON.stringify(buffered))
+      t.buffer(pt, 10, 'miles', function(err, buffered){
+        fs.writeFileSync('./testOut/buffered.geojson',JSON.stringify(buffered))
         if(err) throw err
         done()
       })
