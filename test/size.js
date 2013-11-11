66f16b2cf805cd7e84488a2fe4c4154b9d599475
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-10 23:26:40 -0500

    testing for 50%

--- test/size.js
+++ test/size.js
@@ -14,4 +14,24 @@ describe('size', function(){
       done()
     })
   })
+  it('should double the size of a bbox at -10,-10', function(done){
+    var bbox = [-10, -10, 0, 0]
+
+    t.size(bbox, 2, function(err, doubled){
+      if(err) throw err
+      doubled.should.be.ok
+      _.isEqual(doubled, [-20, -20, 10, 10])
+      done()
+    })
+  })
+  it('should expand the size of a bbox by 50%', function(done){
+    var bbox = [-10, -10, 0, 0]
+
+    t.size(bbox, 2, function(err, doubled){
+      if(err) throw err
+      doubled.should.be.ok
+      _.isEqual(doubled, [-20, -20, 10, 10])
+      done()
+    })
+  })
 }) 
