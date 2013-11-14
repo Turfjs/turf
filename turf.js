2c557f72203f0e26a9ef1b1b8faeff725062f3a1
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-14 15:20:40 -0500

    release

--- turf.js
+++ turf.js
@@ -1330,8 +1330,20 @@ module.exports = function(fc, inField, outField, translations, done){
   done(null, reclassed)
 }
 },{"./featurecollection":12}],27:[function(require,module,exports){
-module.exports=require(13)
-},{}],28:[function(require,module,exports){
+var t = {}
+var featurecollection = require('./featurecollection')
+t.featurecollection = featurecollection
+
+module.exports = function(collection, key, val, done) {
+  var newFC = t.featurecollection([]);
+  for(var i = 0; i < collection.features.length; i++) {
+    if(collection.features[i].properties[key] != val) {
+      newFC.features.push(collection.features[i])
+    }
+  }
+  done(null, newFC)
+}
+},{"./featurecollection":12}],28:[function(require,module,exports){
 var t = {}
 var _ = require('lodash'),
     featurecollection = require('./featurecollection')
