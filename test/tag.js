1cadfa3235f299ec99881203797eeb3aa237a69f
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-14 21:39:12 -0500

    wrote test for tag

--- test/tag.js
+++ test/tag.js
@@ -2,8 +2,18 @@ var t = require('../index'),
   should = require('should')
 
 describe('tag', function(){
-  it('should ', function(done){
-
-    done()
+  it('should tag a point layer with a property from a polygon layer', function(done){
+    t.load('./testIn/tagPoints.geojson', function(err, points){
+      if(err) throw err
+      t.load('./testIn/tagPolygons.geojson', function(err, polygons){
+        if(err) throw err
+        t.tag(points, polygons, 'polyID', function(err, taggedPoints){
+          if(err) throw err
+          taggedPoints.should.be.ok
+          taggedPoints.features.should.be.ok
+          done()
+        })
+      })
+    })
   })
 }) 
