0f979c31da5b45732df6c470b41901e2ab9258f0
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-15 11:05:59 -0500

    bezier test written

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  bezier: require('./lib/bezier'),
   tag: require('./lib/tag'),
   size: require('./lib/size'),
   sample: require('./lib/sample'),
--- test/bezier.js
+++ test/bezier.js
@@ -2,9 +2,16 @@ var t = require('../index'),
   should = require('should'),
   fs = require('fs')
 
-describe('feature', function(){
-  xit('should ', function(done){
-
-    done()
+describe('bezier', function(){
+  it('should take a line and return a smoothed version of the line', function(done){
+    t.load('./testIn/bezierIn.geojson', function(err, lineIn){
+      if(err) throw err
+      t.bezier(lineIn, 5000, function(err, lineOut){
+        if(err) throw err
+        lineout.should.be.ok
+        lineOut.features.should.be.ok
+        done()        
+      })
+    })
   })
-}) 
+})
