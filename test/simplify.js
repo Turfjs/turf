399e3c1d2b43c68c0a088d0d959c61cc57ebe80d
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-04 18:42:10 -0500

    simplification implemented

--- test/simplify.js
+++ test/simplify.js
@@ -3,7 +3,7 @@ var t = require('../index'),
   fs = require('fs')
 
 describe('simplify', function(){
-  xit('should simplify a complex polygon or linestring', function(done){
+  it('should simplify a complex polygon or linestring', function(done){
     t.load('./testOut/contours.geojson', function(err, polys){
       if(err) throw err
       polys.should.be.ok
