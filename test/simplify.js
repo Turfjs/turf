e10160a2bd4b0491be4448ef3390048ff3eeb54f
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-05 09:43:51 -0500

    new tests for various types of features for simplification

--- test/simplify.js
+++ test/simplify.js
@@ -3,16 +3,52 @@ var t = require('../index'),
   fs = require('fs')
 
 describe('simplify', function(){
-  it('should simplify a complex polygon or linestring', function(done){
-    t.load('./testOut/contours.geojson', function(err, polys){
+  it('should simplify a featurecollection of complex polygon', function(done){
+    t.load('./testIn/complexPolygons.geojson', function(err, geo){
       if(err) throw err
-      polys.should.be.ok
-      t.simplify(polys, 50, 0, function(err, simplified){
+      geo.should.be.ok
+      t.simplify(geo, 50, 0, function(err, simplified){
         if(err) throw err
         simplified.should.be.ok
-        fs.writeFileSync('./testOut/simplified.geojson', JSON.stringify(simplified))
+        fs.writeFileSync('./testOut/simplifiedPolygons.geojson', JSON.stringify(simplified))
         done()
       })
     })
   })
-}) 
+  it('should simplify a featurecollection of complex linestrings', function(done){
+    t.load('./testIn/complexLines.geojson', function(err, geo){
+      if(err) throw err
+      geo.should.be.ok
+      t.simplify(geo, 50, 0, function(err, simplified){
+        if(err) throw err
+        simplified.should.be.ok
+        fs.writeFileSync('./testOut/simplifiedLines.geojson', JSON.stringify(simplified))
+        done()
+      })
+    })
+  })
+  it('should simplify a complex polygon', function(done){
+    t.load('./testIn/complexPolygon.geojson', function(err, geo){
+      if(err) throw err
+      geo.should.be.ok
+      t.simplify(geo, 50, 0, function(err, simplified){
+        if(err) throw err
+        simplified.should.be.ok
+        fs.writeFileSync('./testOut/simplifiedPolygon.geojson', JSON.stringify(simplified))
+        done()
+      })
+    })
+  })
+  it('should simplify a complex linestring', function(done){
+    t.load('./testIn/complexLine.geojson', function(err, geo){
+      if(err) throw err
+      geo.should.be.ok
+      t.simplify(geo, 50, 0, function(err, simplified){
+        if(err) throw err
+        simplified.should.be.ok
+        fs.writeFileSync('./testOut/simplifiedLine.geojson', JSON.stringify(simplified))
+        done()
+      })
+    })
+  })
+})
