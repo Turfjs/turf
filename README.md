7a916873a20d71e2ea6dd2075cd9a91a9fa3fd6c
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-20 22:24:24 -0500

    documented sum function

--- README.md
+++ README.md
@@ -688,10 +688,27 @@ t.average(polyFC, ptFC, 'population', 'pop_avg', function(err, averaged){
 
 **sum**
 
-Calculates the average value of a field for points within a set of polygons.
+Calculates the sum value of a field for points within a set of polygons.
 
 ```javascript
+var t = require('turf')
 
+var poly1 = t.polygon([[[0,0],[10,0],[10,10], [0,10]]])
+var poly2 = t.polygon([[[10,0],[20,10],[20,20], [20,0]]])
+var polyFC = t.featurecollection([poly1, poly2])
+var pt1 = t.point(1,1, {population: 500})
+var pt2 = t.point(1,3, {population: 400})
+var pt3 = t.point(14,2, {population: 600})
+var pt4 = t.point(13,1, {population: 500})
+var pt5 = t.point(19,7, {population: 200})
+var ptFC = t.featurecollection([pt1, pt2, pt3, pt4, pt5])
+
+t.sum(polyFC, ptFC, 'population', 'pop_sum', function(err, averaged){
+  if(err) throw err
+
+  console.log(averaged.features[0].properties.pop_sum) // 900
+  console.log(averaged.features[1].properties.pop_sum) // 1300
+})
 ```
 
 
--- test/sum.js
+++ test/sum.js
@@ -6,19 +6,19 @@ describe('sum', function(){
     var poly1 = t.polygon([[[0,0],[10,0],[10,10],[0,10]]])
     var poly2 = t.polygon([[[10,0],[20,10],[20,20], [20,0]]])
     var polyFC = t.featurecollection([poly1, poly2])
-    var pt1 = t.point(1,1, {population: 100})
-    var pt2 = t.point(1,3, {population: 100})
-    var pt3 = t.point(14,2, {population: 100})
-    var pt4 = t.point(13,1, {population: 100})
-    var pt5 = t.point(19,7, {population: 100})
+    var pt1 = t.point(1,1, {population: 500})
+    var pt2 = t.point(1,3, {population: 400})
+    var pt3 = t.point(14,2, {population: 600})
+    var pt4 = t.point(13,1, {population: 500})
+    var pt5 = t.point(19,7, {population: 200})
     var ptFC = t.featurecollection([pt1, pt2, pt3, pt4, pt5])
 
     t.sum(polyFC, ptFC, 'population', 'population_sum', function(err, summed){
       if(err) throw err
       summed.should.be.ok
       summed.features.should.be.ok
-      summed.features[0].properties.population_sum.should.equal(200)
-      summed.features[1].properties.population_sum.should.equal(300)
+      summed.features[0].properties.population_sum.should.equal(900)
+      summed.features[1].properties.population_sum.should.equal(1300)
       done()
     })
   })
