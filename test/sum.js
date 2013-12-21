35ed2c61ec1cd105696cf09d7929aee3a0b447ae
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-20 22:15:52 -0500

    sum implemented and passing tests winding either way

--- test/sum.js
+++ test/sum.js
@@ -3,8 +3,8 @@ var t = require('../index'),
 
 describe('sum', function(){
   it('should create a sum of field for all points within a set of polygons', function(done){
-    var poly1 = t.polygon([[[0,0],[0,10],[10,10],10,0]])
-    var poly2 = t.polygon([[[10,0],[10,10],[20,10],20,0]])
+    var poly1 = t.polygon([[[0,0],[10,0],[10,10],[0,10]]])
+    var poly2 = t.polygon([[[10,0],[20,10],[20,20], [20,0]]])
     var polyFC = t.featurecollection([poly1, poly2])
     var pt1 = t.point(1,1, {population: 100})
     var pt2 = t.point(1,3, {population: 100})
@@ -13,12 +13,12 @@ describe('sum', function(){
     var pt5 = t.point(19,7, {population: 100})
     var ptFC = t.featurecollection([pt1, pt2, pt3, pt4, pt5])
 
-    t.sum(polyFC, ptFC, 'population', 'pop_sum', function(err, summed){
+    t.sum(polyFC, ptFC, 'population', 'population_sum', function(err, summed){
       if(err) throw err
       summed.should.be.ok
       summed.features.should.be.ok
       summed.features[0].properties.population_sum.should.equal(200)
-      summed.features[1].properties.population_sum.should.equal(400)
+      summed.features[1].properties.population_sum.should.equal(300)
       done()
     })
   })
