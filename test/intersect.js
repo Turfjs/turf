9bd00b35388ce896171f9420363ac740164f9422
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-21 23:25:36 -0500

    intersect multi

--- test/intersect.js
+++ test/intersect.js
@@ -9,7 +9,19 @@ describe('intersect', function(){
         t.intersect(polys1, polys2, function(err, intersected){
           if(err) throw err
           intersected.features[0].should.be.ok
-          fs.writeFileSync(__dirname + '/testOut/intersected.geojson', JSON.stringify(intersected))
+          fs.writeFileSync(__dirname + '/testOut/intersected1.geojson', JSON.stringify(intersected))
+          done()
+        })
+      })
+    })
+  })
+  it('should return the overlap of two polygons', function(done){
+    t.load(__dirname + '/testIn/Intersect1.geojson', function(err, polys1){
+      t.load(__dirname + '/testIn/Intersect3.geojson', function(err, polys2){
+        t.intersect(polys1, polys2, function(err, intersected){
+          if(err) throw err
+          intersected.features[0].should.be.ok
+          fs.writeFileSync(__dirname + '/testOut/intersected2.geojson', JSON.stringify(intersected))
           done()
         })
       })
