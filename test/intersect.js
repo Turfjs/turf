7989918053fbfaf002aa94ed5a5bf2bfe5a296b6
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-21 23:09:40 -0500

    intersected implemented

--- test/intersect.js
+++ test/intersect.js
@@ -1,8 +1,18 @@
-var t = require('../lib/intersect')
+var t = require('../index'),
+    should = require('should'),
+    fs = require('fs')
 
 describe('intersect', function(){
-  xit('should ', function(done){
-    throw new Error('not implemented')
-    done()
+  it('should return the overlap of two polygons', function(done){
+    t.load(__dirname + '/testIn/Intersect1.geojson', function(err, polys1){
+      t.load(__dirname + '/testIn/Intersect2.geojson', function(err, polys2){
+        t.intersect(polys1, polys2, function(err, intersected){
+          if(err) throw err
+          intersected.features[0].should.be.ok
+          fs.writeFileSync(__dirname + '/testOut/intersected.geojson', JSON.stringify(intersected))
+          done()
+        })
+      })
+    })
   })
 }) 
