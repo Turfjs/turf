c4c03147ac1766c300c4356174e45844180ab498
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-29 15:29:46 -0500

    release

--- test/buffer.js
+++ test/buffer.js
@@ -10,4 +10,40 @@ describe('buffer', function(){
       })
     })
   })
+  it('should buffer a LineString', function(done){
+    t.load('../test/testIn/Point1.geojson', function(err, pt){
+      t.buffer(pt, 10, 'miles', function(err, buffered){
+        fs.writeFileSync('./testOut/buffered.geojson',JSON.stringify(buffered))
+        if(err) throw err
+        done()
+      })
+    })
+  })
+  it('should buffer a Polygon', function(done){
+    t.load('../test/testIn/Point1.geojson', function(err, pt){
+      t.buffer(pt, 10, 'miles', function(err, buffered){
+        fs.writeFileSync('./testOut/buffered.geojson',JSON.stringify(buffered))
+        if(err) throw err
+        done()
+      })
+    })
+  })
+  it('should buffer a set of Points', function(done){
+    t.load('../test/testIn/Point1.geojson', function(err, pt){
+      t.buffer(pt, 10, 'miles', function(err, buffered){
+        fs.writeFileSync('./testOut/buffered.geojson',JSON.stringify(buffered))
+        if(err) throw err
+        done()
+      })
+    })
+  })
+  it('should buffer a set of Points and union them', function(done){
+    t.load('../test/testIn/Point1.geojson', function(err, pt){
+      t.buffer(pt, 10, 'miles', function(err, buffered){
+        fs.writeFileSync('./testOut/buffered.geojson',JSON.stringify(buffered))
+        if(err) throw err
+        done()
+      })
+    })
+  })
 }) 
