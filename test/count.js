4802446d557c5b87ed24103ab447d625bdd51ce2
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-31 21:02:08 -0500

    implemented count first pass

---
+++ test/count.js
@@ -0,0 +1,25 @@
+var t = require('../index'),
+  should = require('should')
+
+describe('sum', function(){
+  it('should create a sum of field for all points within a set of polygons', function(done){
+    var poly1 = t.polygon([[[0,0],[10,0],[10,10],[0,10]]])
+    var poly2 = t.polygon([[[10,0],[20,10],[20,20], [20,0]]])
+    var polyFC = t.featurecollection([poly1, poly2])
+    var pt1 = t.point(1,1, {population: 500})
+    var pt2 = t.point(1,3, {population: 400})
+    var pt3 = t.point(14,2, {population: 600})
+    var pt4 = t.point(13,1, {population: 500})
+    var pt5 = t.point(19,7, {population: 200})
+    var ptFC = t.featurecollection([pt1, pt2, pt3, pt4, pt5])
+
+    t.sum(polyFC, ptFC, 'population', 'population_sum', function(err, summed){
+      if(err) throw err
+      summed.should.be.ok
+      summed.features.should.be.ok
+      summed.features[0].properties.population_sum.should.equal(2)
+      summed.features[1].properties.population_sum.should.equal(3)
+      done()
+    })
+  })
+})
