80695e719f2c217fe8b6268d2830f03663196541
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-01-09 15:47:26 -0500

    erased working properly with polygons

--- test/erase.js
+++ test/erase.js
@@ -6,34 +6,10 @@ describe('erase', function(){
   it('should return polygon 1 minus polygon 2', function(done){
     t.load(__dirname + '/testIn/Intersect1.geojson', function(err, polys1){
       t.load(__dirname + '/testIn/Intersect2.geojson', function(err, polys2){
-        t.erase(polys1, polys2, function(err, erased){
+        t.erase(polys1.features[0], polys2.features[0], function(err, erased){
           if(err) throw err
-          erased.features[0].should.be.ok
+          erased.should.be.ok
           fs.writeFileSync(__dirname + '/testOut/erased.geojson', JSON.stringify(erased))
-          done()
-        })
-      })
-    })
-  })
-  it('should return polygon 1 minus polygon 2 from a single poly', function(done){
-    t.load(__dirname + '/testIn/Intersect1.geojson', function(err, polys1){
-      t.load(__dirname + '/testIn/Intersect2.geojson', function(err, polys2){
-        t.erase(polys1.features[0], polys2, function(err, erased){
-          if(err) throw err
-          erased.features[0].should.be.ok
-          fs.writeFileSync(__dirname + '/testOut/erased2.geojson', JSON.stringify(erased))
-          done()
-        })
-      })
-    })
-  })
-  it('should return polygon 1 minus polygon 2 from a single poly', function(done){
-    t.load(__dirname + '/testIn/Intersect1.geojson', function(err, polys1){
-      t.load(__dirname + '/testIn/Intersect2.geojson', function(err, polys2){
-        t.erase(polys1, polys2.features[0], function(err, erased){
-          if(err) throw err
-          erased.features[0].should.be.ok
-          fs.writeFileSync(__dirname + '/testOut/erased3.geojson', JSON.stringify(erased))
           done()
         })
       })
