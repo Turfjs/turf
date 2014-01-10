5c48de76ca2f35805cb024576ab7ad08b80b3d60
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-01-10 09:29:28 -0500

    fixed donut center hole issue

--- test/donuts.js
+++ test/donuts.js
@@ -9,7 +9,7 @@ describe('donuts', function(){
       if(err) throw err
       t.donuts(donutsIn, function(err, donuts){
         if(err) throw err
-        //fs.writeFileSync('./testOut/donuts.geojson', JSON.stringify(donuts))
+        fs.writeFileSync('./testOut/donuts.geojson', JSON.stringify(donuts))
         donuts.should.be.ok
         donuts.features.should.be.ok
         donuts.features[0].should.be.ok
