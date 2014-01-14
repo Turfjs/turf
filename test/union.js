2a4cbfd901f56a0b357e522b485db68df95d9cde
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-01-14 14:44:34 -0500

    fixed union

--- test/union.js
+++ test/union.js
@@ -8,7 +8,7 @@ describe('union', function(){
       t.load(__dirname + '/testIn/Intersect2.geojson', function(err, polys2){
         t.union(polys1.features[0], polys2.features[0], function(err, unioned){
           if(err) throw err
-          unioned.features[0].should.be.ok
+          unioned.should.be.ok
           fs.writeFileSync(__dirname + '/testOut/unioned.geojson', JSON.stringify(unioned))
           done()
         })
