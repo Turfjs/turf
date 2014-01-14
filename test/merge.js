4ea476952dd1ea5c785d0d7ce4aa7c3bc727a484
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-01-14 13:02:27 -0500

    merge working

--- test/merge.js
+++ test/merge.js
@@ -5,13 +5,10 @@ var t = require('../index'),
 describe('merge', function(){
   it('should take a set of polygons and merge them into a single polygon', function(done){
     t.load('../test/testIn/mergeIn.geojson', function(err, polygons){
-      //console.log(JSON.stringify(polygons,null,2))
-      //console.log('!!!!!!!!!!!!!!!!!')
       t.merge(polygons, function(err, mergeOut){
         if(err) throw err
         fs.writeFileSync('./testOut/merge.geojson', JSON.stringify(mergeOut))
         mergeOut.should.be.ok
-        mergeOut.features.should.be.ok
         done()
       })
     })
