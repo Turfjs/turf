0a6b62af2fb8f3696bb6c75967de50475e50b42b
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-01-14 12:14:09 -0500

    tin mesh building for convex hulls

---
+++ test/convex.js
@@ -0,0 +1,17 @@
+var t = require('../index'),
+  should = require('should'),
+  fs = require('fs')
+
+describe('convex', function(){
+  it('should take a set of points and return a convex hull polygon', function(done){
+    t.load('../test/testIn/elevation1.geojson', function(err, points){
+      t.convex(points, function(err, hull){
+        if(err) throw err
+        fs.writeFileSync('./testOut/convex.geojson', JSON.stringify(hull))
+        hull.should.be.ok
+        hull.features.should.be.ok
+        done()
+      })
+    })
+  })
+})
