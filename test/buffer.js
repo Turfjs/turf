6d6d131d76b87039ff0760f4b26a24caff3e62ee
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-01-14 20:57:07 -0500

    buffer now supports unioned multi inputs

--- test/buffer.js
+++ test/buffer.js
@@ -4,7 +4,7 @@ var t = require('../index'),
 describe('buffer', function(){
   it('should buffer a Point', function(done){
     t.load('../test/testIn/Point1.geojson', function(err, pt){
-      t.buffer(pt, 10, 'miles', false, function(err, buffered){
+      t.buffer(pt, 10, 'miles', function(err, buffered){
         //fs.writeFileSync('./testOut/buffered.geojson',JSON.stringify(buffered))
         if(err) throw err
         done()
@@ -13,7 +13,7 @@ describe('buffer', function(){
   })
   it('should buffer a LineString', function(done){
     t.load('../test/testIn/bezierIn.geojson', function(err, pt){
-      t.buffer(pt, 10, 'miles', false, function(err, buffered){
+      t.buffer(pt, 10, 'miles', function(err, buffered){
         //fs.writeFileSync('./testOut/bufferedLine.geojson',JSON.stringify(buffered))
         if(err) throw err
         done()
@@ -22,7 +22,7 @@ describe('buffer', function(){
   })
   it('should buffer a Polygon', function(done){
     t.load('../test/testIn/bufferPolygonIn.geojson', function(err, pt){
-      t.buffer(pt, 10, 'miles', false, function(err, buffered){
+      t.buffer(pt, 10, 'miles', function(err, buffered){
         //fs.writeFileSync('./testOut/bufferedPolygon.geojson',JSON.stringify(buffered))
         if(err) throw err
         done()
@@ -31,7 +31,7 @@ describe('buffer', function(){
   })
   it('should buffer a set of Points', function(done){
     t.load('../test/testIn/Point1.geojson', function(err, pt){
-      t.buffer(pt, 10, 'miles', false, function(err, buffered){
+      t.buffer(pt, 10, 'miles', function(err, buffered){
         //fs.writeFileSync('./testOut/buffered.geojson',JSON.stringify(buffered))
         if(err) throw err
         done()
@@ -40,7 +40,7 @@ describe('buffer', function(){
   })
   it('should buffer a set of Points and union them', function(done){
     t.load('../test/testIn/elevation1.geojson', function(err, pts){
-      t.buffer(pts, 10, 'miles', true, function(err, buffered){
+      t.buffer(pts, 10, 'miles', function(err, buffered){
         fs.writeFileSync('./testOut/bufferedPoints.geojson',JSON.stringify(buffered))
         if(err) throw err
         done()
