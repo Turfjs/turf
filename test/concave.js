b7ccfe2fec0834e6e60ebf1d0a8a15ee43a48ebc
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-01-15 11:30:01 -0500

    concave algo filtering first pass

--- test/concave.js
+++ test/concave.js
@@ -3,26 +3,15 @@ var t = require('../index'),
   fs = require('fs')
 
 describe('concave', function(){
-  it('should create a concave polygon', function(done){
-    t.load('../test/testIn/concaveIn.geojson', function(err, points){
-      t.concave(points, 1, function(err, concaved){
+  it('should take a set of points and return a concave hull polygon', function(done){
+    var maxEdge = .5
+
+    t.load('../test/testIn/concaveIn1.geojson', function(err, points){
+      t.concave(points, maxEdge, function(err, hull){
         if(err) throw err
-        concaved.should.be.ok
-        concaved.features[0].geometry.type.should.equal('Polygon')
-        concaved.features[0].geometry.coordinates.should.be.ok
-        fs.writeFileSync(__dirname + '/testOut/concave1.geojson',JSON.stringify(concaved))
-        done()  
-      })
-    })
-  })
-  it('should create a concave polygon with a hole', function(done){
-    t.load('../test/testIn/concaveInHole.geojson', function(err, points){
-      t.concave(points, 1, function(err, concaved){
-        if(err) throw err
-        concaved.should.be.ok
-        concaved.features[0].geometry.type.should.equal('Polygon')
-        concaved.features[0].geometry.coordinates.should.be.ok
-        fs.writeFileSync(__dirname + '/testOut/concave2.geojson',JSON.stringify(concaved))
+        fs.writeFileSync('./testOut/concave.geojson', JSON.stringify(hull))
+        hull.should.be.ok
+        //console.log(hull)
         done()
       })
     })
