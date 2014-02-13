0a0c98169d7998bc337e544e644314f030c3d865
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-02-13 15:01:26 -0500

    implemented isobands

--- test/isobands.js
+++ test/isobands.js
@@ -3,9 +3,9 @@ var t = require('../index'),
   fs = require('fs')
 
 describe('isobands', function(){
-  xit('should take a set of points with z values and output a set of contour polygons', function(done){
+  it('should take a set of points with z values and output a set of contour polygons', function(done){
     t.load(__dirname+'/testIn/elevation1.geojson', function(err, points){
-      t.contour(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180], false, function(err, contours){
+      t.isobands(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180], function(err, contours){
         if(err) throw err
         //fs.writeFileSync('./testOut/contours1.geojson', JSON.stringify(contours))
         contours.should.be.ok
@@ -14,11 +14,11 @@ describe('isobands', function(){
       })
     })
   })
-  xit('should take a set of points with z values and output a set of contour polygons with jenks breaks', function(done){
+  it('should take a set of points with z values and output a set of contour polygons with jenks breaks', function(done){
     t.load(__dirname+'/testIn/elevation1.geojson', function(err, points){
       t.jenks(points, 'elevation', 5, function(err, breaks){
         if(err) throw err
-        t.contour(points, 'elevation', 15, breaks, false, function(err, contours){
+        t.isobands(points, 'elevation', 15, breaks, function(err, contours){
           if(err) throw err
           //fs.writeFileSync('./testOut/contours2.geojson', JSON.stringify(contours))
           contours.should.be.ok
@@ -28,9 +28,9 @@ describe('isobands', function(){
       })
     })
   })
-  xit('should take a set of points with decimal z values and output a set of contour polygons', function(done){
+  it('should take a set of points with decimal z values and output a set of contour polygons', function(done){
     t.load(__dirname+'/testIn/elevation2.geojson', function(err, points){
-      t.contour(points, 'elevation', 15, [-2000,-20, -5, -1, 0, 2, 5, 10, 20, 30, 500 ], false, function(err, contours){
+      t.isobands(points, 'elevation', 15, [-1, 25, 45, 55, 65, 85,  95, 105, 120, 180], function(err, contours){
         if(err) throw err
         //fs.writeFileSync('./testOut/contours3.geojson', JSON.stringify(contours))
         contours.should.be.ok
@@ -39,9 +39,9 @@ describe('isobands', function(){
       })
     })
   })
-  xit('should take a set of points with negative z values and output a set of contour polygons', function(done){
+  it('should take a set of points with negative z values and output a set of contour polygons', function(done){
     t.load(__dirname+'/testIn/elevation3.geojson', function(err, points){
-      t.contour(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180], false, function(err, contours){
+      t.isobands(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180], function(err, contours){
         if(err) throw err
         //fs.writeFileSync('./testOut/contours4.geojson', JSON.stringify(contours))
         contours.should.be.ok
@@ -50,20 +50,20 @@ describe('isobands', function(){
       })
     })
   })
-  xit('should take a set of points lopsided edges and output a set of contour polygons', function(done){
+  it('should take a set of points lopsided edges and output a set of contour polygons', function(done){
     t.load(__dirname+'/testIn/openContourPoints.geojson', function(err, points){
-      t.contour(points, 'elevation', 15, [5, 15, 40, 80, 90, 110], false, function(err, contours){
+      t.isobands(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180], function(err, contours){
         if(err) throw err
-        //fs.writeFileSync('./testOut/contoursEdges.geojson', JSON.stringify(contours))
+        //fs.writeFileSync(__dirname+'/testOut/contoursEdges.geojson', JSON.stringify(contours))
         contours.should.be.ok
         contours.features.should.be.ok
         done()
       })
     })
   })
-  xit('should take a set of points with internal valleys and output a set of contour polygons', function(done){
+  it('should take a set of points with internal valleys and output a set of contour polygons', function(done){
     t.load(__dirname+'/testIn/holeContourPoints.geojson', function(err, points){
-      t.contour(points, 'elevation', 15, [5, 15, 40, 80, 90, 110], true, function(err, contours){
+      t.isobands(points, 'elevation', 15, [5, 15, 40, 80, 90, 110], function(err, contours){
         if(err) throw err
         //fs.writeFileSync(__dirname+'/testOut/contoursHoles.geojson', JSON.stringify(contours))
         contours.should.be.ok
