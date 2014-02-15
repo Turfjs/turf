be51df91757318fe5119b62854cd1a5b399479b0
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-02-15 01:16:01 -0500

    near test

---
+++ test/near.js
@@ -0,0 +1,21 @@
+var t = require('../index'),
+  should = require('should'),
+  fs = require('fs')
+
+describe('near', function(){
+  it('should take a set of points and return points from a second set that are within a specified distance', function(done){
+
+    var distance = 1,
+        units = 'miles'
+
+    t.load(__dirname+'/testIn/nearIn.geojson', function(err, inPoints){
+      t.load(__dirname+'/testIn/nearOut.geojson', function(err, outPoints){
+        t.near(inPoints, outPoints, distance, unit, function(err, nearByPoints){
+          if(err) throw err
+          nearByPoints.features.should.be.ok
+          done()
+        })
+      })
+    })
+  })
+})
