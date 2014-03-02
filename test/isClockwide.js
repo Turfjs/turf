db133175657275c83da57c6619263573af84b886
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-01 23:54:47 -0500

    clockwise test passing

--- test/isClockwide.js
+++ test/isClockwide.js
@@ -4,10 +4,17 @@ var t = require('../index'),
 
 describe('isClockwise', function(){
   it('should take a ring and return clockwise true', function(done){
-    var ring = [[20, 35], [10, 30], [10, 10], [30, 5], [45, 20], [20, 35]]
+    var ring = [[0,0],[1,1],[1,0],[0,0]]
     //var ring = [20, 35, 10, 30, 10, 10, 30, 5, 45, 20, 20, 35]
     var clockwise = t.isClockwise(ring)
     clockwise.should.equal(true)
     done()
   })
-})
+  it('should take a ring and return clockwise false', function(done){
+    var ring = [[0,0],[0,-1],[-1,-1],[0,0]]
+    //var ring = [20, 35, 10, 30, 10, 10, 30, 5, 45, 20, 20, 35]
+    var clockwise = t.isClockwise(ring)
+    clockwise.should.equal(false)
+    done()
+  })
+})  
