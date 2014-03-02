7df49f204276e5409cb49bee18286c2b25a5d382
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-01 22:49:46 -0500

    added test stub for isClockwise

---
+++ test/isClockwide.js
@@ -0,0 +1,10 @@
+var t = require('../index'),
+  should = require('should'),
+  fs = require('fs')
+
+describe('isClockwise', function(){
+  it('should take a ring and return clockwise true', function(done){
+    
+    done()
+  })
+})
