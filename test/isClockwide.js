64b4cd2763b6e6139cc9f3aa348ee0151af43038
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-01 22:55:09 -0500

    isClockwise added to main index

--- test/isClockwide.js
+++ test/isClockwide.js
@@ -6,7 +6,7 @@ describe('isClockwise', function(){
   it('should take a ring and return clockwise true', function(done){
     var ring = [[20, 35], [10, 30], [10, 10], [30, 5], [45, 20], [20, 35]]
     var clockwise = t.isClockwise(ring)
-    clockwise.should.be.true
+    clockwise.should.equal(true)
     done()
   })
 })
