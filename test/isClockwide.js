0dc18e826d06eceeba927b8def691293b62ffbb2
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-01 23:41:07 -0500

    clockwise first pass

--- test/isClockwide.js
+++ test/isClockwide.js
@@ -5,6 +5,7 @@ var t = require('../index'),
 describe('isClockwise', function(){
   it('should take a ring and return clockwise true', function(done){
     var ring = [[20, 35], [10, 30], [10, 10], [30, 5], [45, 20], [20, 35]]
+    //var ring = [20, 35, 10, 30, 10, 10, 30, 5, 45, 20, 20, 35]
     var clockwise = t.isClockwise(ring)
     clockwise.should.equal(true)
     done()
