11ef221461b6fc266d5dacff0a7ace15cc4b2a37
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-25 17:47:09 -0400

    release

--- turf.js
+++ turf.js
@@ -3479,10 +3479,13 @@ module.exports = function(fc, quantization, minimumArea, done){
 }
 },{"topojson":68}],50:[function(_dereq_,module,exports){
 module.exports = function(bbox, factor, done){
-  var lowX = (((bbox[2] - bbox[0]) / 2) * factor) + bbox[0]
-  var lowY = (((bbox[3] - bbox[1]) / 2) * factor) + bbox[1]
-  var highX = (((bbox[2] - bbox[0]) / 2) * factor) + bbox[2]
-  var highY = (((bbox[3] - bbox[1]) / 2) * factor) + bbox[3]
+  var xDistance = ((bbox[2] - bbox[0]) / 2) * factor
+  var yDistance = ((bbox[3] - bbox[1]) / 2) * factor
+
+  var lowX = bbox[0] - xDistance
+  var lowY = bbox[1] - yDistance
+  var highX = xDistance + bbox[2]
+  var highY = yDistance + bbox[3]
 
   var sized = [lowX, lowY, highX, highY]
   done(null, sized)
