a399f6b93402987ee311078f6c65bd71437057c6
Tom MacWright <tom@macwright.org>
2016-02-27 11:05:36 -0500

    Merge turf-feature into turf-helpers

--- packages/turf-helpers/index.js
+++ packages/turf-helpers/index.js
@@ -205,3 +205,31 @@ module.exports.multiPoint = function (coordinates, properties) {
         'properties': properties || {}
     };
 };
+
+/**
+ * Wraps a GeoJSON {@link Geometry} in a GeoJSON {@link Feature}.
+ *
+ * @module turf/feature
+ * @category helper
+ * @param {Geometry} geometry input geometry
+ * @returns {FeatureCollection} a FeatureCollection of input features
+ * @example
+ * var geometry = {
+ *      "type": "Point",
+ *      "coordinates": [
+ *        67.5,
+ *        32.84267363195431
+ *      ]
+ *    }
+ *
+ * var feature = turf.feature(geometry);
+ *
+ * //=feature
+ */
+module.exports.feature = function (geometry) {
+    return {
+        type: 'Feature',
+        properties: {},
+        geometry: geometry
+    };
+};
--- packages/turf-helpers/test.js
+++ packages/turf-helpers/test.js
@@ -7,6 +7,7 @@ var lineString = geometries.lineString;
 var featureCollection = geometries.featureCollection;
 var multiLineString = geometries.multiLineString;
 var multiPoint = geometries.multiPoint;
+var feature = geometries.feature;
 
 test('point', function(t){
   var ptArray = point([5, 10], {name: 'test point'});
@@ -136,6 +137,78 @@ test('multiPoint', function(t){
   t.throws(function(err){
     multiPoint();
   }, 'throws error with no coordinates');
+
+  t.end();
+});
+
+test('feature', function(t){
+  var pt = {
+      "type": "Point",
+      "coordinates": [
+        67.5,
+        32.84267363195431
+      ]
+    };
+  var line = {
+      "type": "LineString",
+      "coordinates": [
+        [
+          82.96875,
+          58.99531118795094
+        ],
+        [
+          72.7734375,
+          55.57834467218206
+        ],
+        [
+          84.0234375,
+          55.57834467218206
+        ]
+      ]
+    };
+  var polygon = {
+      "type": "Polygon",
+      "coordinates": [
+        [
+          [
+            85.78125,
+            -3.513421045640032
+          ],
+          [
+            85.78125,
+            13.581920900545844
+          ],
+          [
+            92.46093749999999,
+            13.581920900545844
+          ],
+          [
+            92.46093749999999,
+            -3.513421045640032
+          ],
+          [
+            85.78125,
+            -3.513421045640032
+          ]
+        ]
+      ]
+    };
+
+  t.equal(feature(pt).type, 'Feature');
+  t.equal(feature(line).type, 'Feature');
+  t.equal(feature(polygon).type, 'Feature');
+  t.deepEqual(feature(pt),
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          67.5,
+          32.84267363195431
+        ]
+      }
+    });
 
   t.end();
 });
