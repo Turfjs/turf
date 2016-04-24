6df66c6b3580082469242dbba47a4013cf667fa6
Tom MacWright <tmcw@users.noreply.github.com>
2016-04-23 20:42:10 -0400

    Concave use concaveman, fix versions (#361)
    * Concave use concaveman, fix versions
    
    * Version

--- packages/turf-bezier/index.js
+++ packages/turf-bezier/index.js
@@ -47,15 +47,15 @@ module.exports = function (line, resolution, sharpness) {
     var lineOut = linestring([]);
 
     lineOut.properties = line.properties;
-    var pts = line.geometry.coordinates.map(function (pt) {
-        return {x: pt[0], y: pt[1]};
-    });
 
     var spline = new Spline({
-        points: pts,
+        points: line.geometry.coordinates.map(function (pt) {
+            return {x: pt[0], y: pt[1]};
+        }),
         duration: resolution,
         sharpness: sharpness
     });
+
     for (var i = 0; i < spline.duration; i += 10) {
         var pos = spline.pos(i);
         if (Math.floor(i / 100) % 2 === 0) {
