176d1ae2261f1f2da039e382656190a8ce8c1c31
Michele Ferretti <mferretti@Micheles-iMac.local>
2016-04-28 16:56:03 +0100

    fix eslint errors

--- packages/turf-idw/index.js
+++ packages/turf-idw/index.js
@@ -17,38 +17,38 @@ var extent = require('turf-extent');
  * @return {FeatureCollection<Polygon>} grid A grid of polygons where each cell has an IDW value
  */
 module.exports = function (controlPoints, valueField, b, cellWidth, units) {
-  // check if field containing data exists..
-  var filtered = filter(controlPoints, valueField);
-  //alternative method
-  // console.log(controlPoints.features.map(function (feat) { return valueField in feat.properties}));
-  if (filtered.features.length === 0) {
-    // create a sample square grid
-    // compared to a point grid helps visualizing the output (like a raster..)
-    var samplingGrid = squareGrid(extent(controlPoints), cellWidth, units);
-    var N = samplingGrid.features.length;
-    // for every sampling point..
-    for (var i = 0; i < N; i++) {
-      var w;
-      var zw = 0;
-      var sw = 0;
-      // calculate the distance from each control point to cell's centroid
-      controlPoints.features.map(function (point) {
-        var d = distance(centroid(samplingGrid.features[i]), point, units);
-        if (d === 0) {
-          zw = point.properties[valueField];
-          return;
+    // check if field containing data exists..
+    var filtered = filter(controlPoints, valueField);
+    //alternative method
+    // console.log(controlPoints.features.map(function (feat) { return valueField in feat.properties}));
+    if (filtered.features.length === 0) {
+      // create a sample square grid
+      // compared to a point grid helps visualizing the output (like a raster..)
+        var samplingGrid = squareGrid(extent(controlPoints), cellWidth, units);
+        var N = samplingGrid.features.length;
+        // for every sampling point..
+        var myFun = function (point, i, zw, sw) {
+            var d = distance(centroid(samplingGrid.features[i]), point, units);
+            if (d === 0) {
+                zw = point.properties[valueField];
+                return zw;
+            }
+            var w = 1.0 / Math.pow(d, b);
+            sw += w;
+            zw += w * point.properties[valueField];
+        };
+        for (var i = 0; i < N; i++) {
+            var zw = 0;
+            var sw = 0;
+            // calculate the distance from each control point to cell's centroid
+            controlPoints.features.map(myFun);
+            // write IDW value for each grid cell
+            samplingGrid.features[i].properties.z = zw / sw;
         }
-        w = 1.0 / Math.pow(d, b);
-        sw += w;
-        zw += w * point.properties[valueField];
-      });
-      // write IDW value for each grid cell
-      samplingGrid.features[i].properties.z = zw / sw;
-    }
-    return samplingGrid;
+        return samplingGrid;
 
-  } else {
-    console.log('Specified Data Field is Missing');
-  }
+    } else {
+        console.log('Specified Data Field is Missing');
+    }
 
 };
