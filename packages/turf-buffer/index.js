c6ed64a1a94b7bb1037be5e53e34ffbfb50961d4
Tom MacWright <tom@macwright.org>
2016-05-16 15:16:44 -0400

    Buffer units

--- packages/turf-buffer/index.js
+++ packages/turf-buffer/index.js
@@ -2,60 +2,44 @@
 // radians = degrees * (pi/180)
 // https://github.com/bjornharrtell/jsts/blob/master/examples/buffer.html
 
-var featurecollection = require('turf-helpers').featureCollection;
+var helpers = require('turf-helpers');
+var featureCollection = helpers.featureCollection;
 var jsts = require('jsts');
 var normalize = require('geojson-normalize');
 
 /**
-* Calculates a buffer for input features for a given radius. Units supported are miles, kilometers, and degrees.
-*
-* @name buffer
-* @category transformation
-* @param {(Feature|FeatureCollection)} feature input to be buffered
-* @param {Number} distance distance to draw the buffer
-* @param {String} unit 'miles', 'feet', 'kilometers', 'meters', or 'degrees'
-* @return {FeatureCollection<Polygon>|FeatureCollection<MultiPolygon>|Polygon|MultiPolygon} buffered features
-*
-* @example
-* var pt = {
-*   "type": "Feature",
-*   "properties": {},
-*   "geometry": {
-*     "type": "Point",
-*     "coordinates": [-90.548630, 14.616599]
-*   }
-* };
-* var unit = 'miles';
-*
-* var buffered = turf.buffer(pt, 500, unit);
-* var result = turf.featurecollection([buffered, pt]);
-*
-* //=result
-*/
+ * Calculates a buffer for input features for a given radius. Units supported are miles, kilometers, and degrees.
+ *
+ * @name buffer
+ * @category transformation
+ * @param {(Feature|FeatureCollection)} feature input to be buffered
+ * @param {Number} distance distance to draw the buffer
+ * @param {String} unit 'miles', 'feet', 'kilometers', 'meters', or 'degrees'
+ * @return {FeatureCollection<Polygon>|FeatureCollection<MultiPolygon>|Polygon|MultiPolygon} buffered features
+ *
+ * @example
+ * var pt = {
+ *   "type": "Feature",
+ *   "properties": {},
+ *   "geometry": {
+ *     "type": "Point",
+ *     "coordinates": [-90.548630, 14.616599]
+ *   }
+ * };
+ * var unit = 'miles';
+ *
+ * var buffered = turf.buffer(pt, 500, unit);
+ * var result = turf.featurecollection([buffered, pt]);
+ *
+ * //=result
+ */
 
 module.exports = function (feature, radius, units) {
 
-    switch (units) {
-    case 'miles':
-        radius = radius / 69.047;
-        break;
-    case 'feet':
-        radius = radius / 364568.0;
-        break;
-    case 'kilometers':
-        radius = radius / 111.12;
-        break;
-    case 'meters':
-    case 'metres':
-        radius = radius / 111120.0;
-        break;
-    case 'degrees':
-        break;
-    }
-
+    var degrees = helpers.distanceToDegrees(radius, units);
     var fc = normalize(feature);
     var buffered = normalize(featurecollection(fc.features.map(function (f) {
-        return bufferOp(f, radius);
+        return bufferOp(f, degrees);
     })));
 
     if (buffered.features.length > 1) return buffered;
