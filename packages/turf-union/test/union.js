ea4937b25edacf6c13ea45b498362ac386a29c66
Aurélien Defossez <defossez.aurelien@gmail.com>
2016-07-13 17:39:00 +0200

    Fixed the union test
    It was poorly written (my bad), I forgot to change the turf-union call.

--- packages/turf-union/test/union.js
+++ packages/turf-union/test/union.js
@@ -8,7 +8,11 @@ var REGEN = false;
 test('union', function(t){
   glob.sync(__dirname + '/fixtures/in/*.geojson').forEach(function(input) {
       var fcs = JSON.parse(fs.readFileSync(input));
-      var output = union(fcs[0].features[0], fcs[1].features[0]);
+      var args = [];
+      for (var i = 0; i < fcs.length; i++) {
+        args[i] = fcs[i].features[0];
+      }
+      var output = union.apply(this, args);
       if (REGEN) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output));
       t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input);
   });
