0b997ed8c9bacc22a8045aaf4a4924f96ba7ec64
Aurélien Defossez <defossez.aurelien@gmail.com>
2016-07-18 10:46:58 +0200

    Test added for turf-center-of-mass using a FeatureCollection.

--- packages/turf-center-of-mass/test/center-of-mass.js
+++ packages/turf-center-of-mass/test/center-of-mass.js
@@ -5,8 +5,8 @@ var centerOfMass = require('../'),
 
 test('center of mass', function(t){
   glob.sync(__dirname + '/fixtures/in/*.geojson').forEach(function(input) {
-      var fcs = JSON.parse(fs.readFileSync(input));
-      var output = centerOfMass(fcs.features[0]);
+      var jsonData = JSON.parse(fs.readFileSync(input));
+      var output = centerOfMass(jsonData);
       if (process.env.UPDATE) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output));
       t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input);
   });
