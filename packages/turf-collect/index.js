715fe3722c4f5a7f917ebdbed03e47a757bbcd81
Tom MacWright <tom@macwright.org>
2016-08-08 18:36:17 -0700

    Fix lint

--- packages/turf-collect/index.js
+++ packages/turf-collect/index.js
@@ -2,9 +2,9 @@ var inside = require('@turf/inside');
 
 /**
  * Merges a specified property from a FeatureCollection of points into a
- * FeatureCollection of polygons. Given an `inProperty` on points and an `outProperty` 
+ * FeatureCollection of polygons. Given an `inProperty` on points and an `outProperty`
  * for polygons, this finds every point that lies within each polygon, collects the
- * `inProperty` values from those points, and adds them as an array to `outProperty` 
+ * `inProperty` values from those points, and adds them as an array to `outProperty`
  * on the polygon.
  *
  * @name collect
