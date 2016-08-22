cc07bac0ded982928eb340d163e39afc5c90645b
Michele Ferretti <mic.ferretti@gmail.com>
2016-08-22 22:49:56 +0100

    fix regressions introduced by merge #447

--- packages/turf-idw/index.js
+++ packages/turf-idw/index.js
@@ -32,14 +32,14 @@ module.exports = function (controlPoints, valueField, b, cellWidth, units) {
             var zw = 0;
             var sw = 0;
             // calculate the distance from each control point to cell's centroid
-            for (var j = 0; j < controlPoints.length; j++) {
-                var d = distance(centroid(samplingGrid.features[j]), controlPoints[j], units);
+            for (var j = 0; j < controlPoints.features.length; j++) {
+                var d = distance(centroid(samplingGrid.features[j]), controlPoints.features[j], units);
                 if (d === 0) {
-                    zw = controlPoints[j].properties[valueField];
+                    zw = controlPoints.features[j].properties[valueField];
                 }
                 var w = 1.0 / Math.pow(d, b);
                 sw += w;
-                zw += w * controlPoints[j].properties[valueField];
+                zw += w * controlPoints.features[j].properties[valueField];
             }
             // write IDW value for each grid cell
             samplingGrid.features[i].properties.z = zw / sw;
