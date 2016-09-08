a4d11e17dd463f3d759cb3b9dd3d51976a3e956f
Denis <carriere.denis@gmail.com>
2016-09-08 13:14:54 -0400

    Update Geometry Collection JSDocs
    Updated example
    - proper function name `geometryCollection`
    - Added unused variables

--- packages/turf-helpers/index.js
+++ packages/turf-helpers/index.js
@@ -237,7 +237,7 @@ module.exports.multiPolygon = function (coordinates, properties) {
  * @name geometryCollection
  * @param {Array<{Geometry}>} geometries an array of GeoJSON Geometries
  * @param {Object=} properties an Object of key-value pairs to add as properties
- * @returns {Feature<GeometryCollection>} a geometrycollection feature
+ * @returns {Feature<GeometryCollection>} a GeoJSON GeometryCollection Feature
  * @example
  * var pt = {
  *     "type": "Point",
@@ -247,7 +247,7 @@ module.exports.multiPolygon = function (coordinates, properties) {
  *     "type": "LineString",
  *     "coordinates": [ [101, 0], [102, 1] ]
  *   };
- * var collection = turf.geometrycollection([[0,0],[10,10]]);
+ * var collection = turf.geometryCollection([pt, line]);
  *
  * //=collection
  */
