533b4a0a8000b7f0cc4818fdaa3f0815032a027e
Jordan Rousseau <jrousseau@wdtinc.com>
2017-01-18 16:21:10 -0600

    fix example -- featurecollection -> featureCollection

--- packages/turf-buffer/index.js
+++ packages/turf-buffer/index.js
@@ -28,7 +28,7 @@ var normalize = require('@mapbox/geojson-normalize');
  * var unit = 'miles';
  *
  * var buffered = turf.buffer(pt, 500, unit);
- * var result = turf.featurecollection([buffered, pt]);
+ * var result = turf.featureCollection([buffered, pt]);
  *
  * //=result
  */
