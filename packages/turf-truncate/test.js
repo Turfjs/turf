da9dd88af4521fa1d9df07973dc9912f8e2803bb
Denis <carriere.denis@gmail.com>
2017-02-01 09:16:08 -0500

    Drop path.join in tests

--- packages/turf-truncate/test.js
+++ packages/turf-truncate/test.js
@@ -5,16 +5,16 @@ var load = require('load-json-file');
 var write = require('write-json-file');
 
 var directories = {
-    in: path.join(__dirname, 'test', 'in'),
-    out: path.join(__dirname, 'test', 'out')
+    in: path.join(__dirname, 'test', 'in') + path.sep,
+    out: path.join(__dirname, 'test', 'out') + path.sep
 };
 
 test('truncate', (t) => {
-    var point = load.sync(path.join(directories.in, 'point.json'));
-    var points = load.sync(path.join(directories.in, 'points.json'));
-    var pointElevation = load.sync(path.join(directories.in, 'pointElevation.json'));
-    var polygon = load.sync(path.join(directories.in, 'polygon.json'));
-    var polygons = load.sync(path.join(directories.in, 'polygons.json'));
+    var point = load.sync(directories.in + 'point.json');
+    var points = load.sync(directories.in + 'points.json');
+    var pointElevation = load.sync(directories.in + 'pointElevation.json');
+    var polygon = load.sync(directories.in + 'polygon.json');
+    var polygons = load.sync(directories.in + 'polygons.json');
 
     var pointTruncate = truncate(point);
     var pointsTruncate = truncate(points);
@@ -24,19 +24,19 @@ test('truncate', (t) => {
     var polygonsTruncateDecimal3 = truncate(polygons, 3);
 
     if (process.env.REGEN) {
-        write.sync(path.join(directories.out, 'pointTruncate.json'), pointTruncate);
-        write.sync(path.join(directories.out, 'pointsTruncate.json'), pointsTruncate);
-        write.sync(path.join(directories.out, 'pointElevationTruncate.json'), pointElevationTruncate);
-        write.sync(path.join(directories.out, 'polygonTruncate.json'), polygonTruncate);
-        write.sync(path.join(directories.out, 'polygonsTruncate.json'), polygonsTruncate);
-        write.sync(path.join(directories.out, 'polygonsTruncateDecimal3.json'), polygonsTruncateDecimal3);
+        write.sync(directories.out + 'pointTruncate.json', pointTruncate);
+        write.sync(directories.out + 'pointsTruncate.json', pointsTruncate);
+        write.sync(directories.out + 'pointElevationTruncate.json', pointElevationTruncate);
+        write.sync(directories.out + 'polygonTruncate.json', polygonTruncate);
+        write.sync(directories.out + 'polygonsTruncate.json', polygonsTruncate);
+        write.sync(directories.out + 'polygonsTruncateDecimal3.json', polygonsTruncateDecimal3);
     }
 
-    t.deepEqual(pointTruncate, load.sync(path.join(directories.out, 'pointTruncate.json')), 'pointTruncate');
-    t.deepEqual(pointsTruncate, load.sync(path.join(directories.out, 'pointsTruncate.json')), 'pointsTruncate');
-    t.deepEqual(pointElevationTruncate, load.sync(path.join(directories.out, 'pointElevationTruncate.json')), 'pointElevationTruncate');
-    t.deepEqual(polygonTruncate, load.sync(path.join(directories.out, 'polygonTruncate.json')), 'polygonTruncate');
-    t.deepEqual(polygonsTruncate, load.sync(path.join(directories.out, 'polygonsTruncate.json')), 'polygonsTruncate');
-    t.deepEqual(polygonsTruncateDecimal3, load.sync(path.join(directories.out, 'polygonsTruncateDecimal3.json')), 'polygonsTruncateDecimal3');
+    t.deepEqual(pointTruncate, load.sync(directories.out + 'pointTruncate.json'), 'pointTruncate');
+    t.deepEqual(pointsTruncate, load.sync(directories.out + 'pointsTruncate.json'), 'pointsTruncate');
+    t.deepEqual(pointElevationTruncate, load.sync(directories.out + 'pointElevationTruncate.json'), 'pointElevationTruncate');
+    t.deepEqual(polygonTruncate, load.sync(directories.out + 'polygonTruncate.json'), 'polygonTruncate');
+    t.deepEqual(polygonsTruncate, load.sync(directories.out + 'polygonsTruncate.json'), 'polygonsTruncate');
+    t.deepEqual(polygonsTruncateDecimal3, load.sync(directories.out + 'polygonsTruncateDecimal3.json'), 'polygonsTruncateDecimal3');
     t.end();
 });
