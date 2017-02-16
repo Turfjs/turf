45143536ad4b34c7c2f4d07df5d07965f643f5d8
Denis <carriere.denis@gmail.com>
2017-02-16 09:31:18 -0500

    Minor fixes to turf-idw tests - Include test/in & out folders - Change env UPDATE to REGEN - Update Typescript definition

--- packages/turf-idw/test.js
+++ packages/turf-idw/test.js
@@ -3,11 +3,11 @@ var idw = require('./');
 var fs = require('fs');
 
 test('idw', function (t) {
-  var testPoints = JSON.parse(fs.readFileSync('./data/data.geojson'));
+  var testPoints = JSON.parse(fs.readFileSync('./test/in/data.geojson'));
 
-  var idw1 = idw(testPoints,'value' , 0.5 , 1, 'kilometers');
-  var idw2 = idw(testPoints,'value', 0.5 ,0.5, 'kilometers');
-  var idw3 = idw(testPoints,'value', 2 , 1, 'miles');
+  var idw1 = idw(testPoints, 'value', 0.5, 1, 'kilometers');
+  var idw2 = idw(testPoints, 'value', 0.5, 0.5, 'kilometers');
+  var idw3 = idw(testPoints, 'value', 2, 1, 'miles');
   var idw4 = idw(testPoints, 'WRONGDataField', 0.5, 1, 'miles');
 
   t.ok(idw1.features.length);
@@ -15,10 +15,10 @@ test('idw', function (t) {
   t.ok(idw3.features.length);
   t.error(idw4);
 
-  if (process.env.UPDATE) {
-    fs.writeFileSync(__dirname+'/tests/idw1.geojson', JSON.stringify(idw1,null,2));
-    fs.writeFileSync(__dirname+'/tests/idw2.geojson', JSON.stringify(idw2,null,2));
-    fs.writeFileSync(__dirname+'/tests/idw3.geojson', JSON.stringify(idw3,null,2));
+  if (process.env.REGEN) {
+    fs.writeFileSync(__dirname + '/test/out/idw1.geojson', JSON.stringify(idw1, null, 2));
+    fs.writeFileSync(__dirname + '/test/out/idw2.geojson', JSON.stringify(idw2, null, 2));
+    fs.writeFileSync(__dirname + '/test/out/idw3.geojson', JSON.stringify(idw3, null, 2));
   }
 
   t.end();
