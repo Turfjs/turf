868942b75dc86273403af4b0cfc86ce76a819ff2
Denis Carriere <carriere.denis@gmail.com>
2017-02-21 10:18:36 -0500

    Update turf inside benchmark

--- packages/turf-inside/bench.js
+++ packages/turf-inside/bench.js
@@ -1,22 +1,18 @@
 var inside = require('./');
 var Benchmark = require('benchmark');
-var fs = require('fs');
 var point = require('@turf/helpers').point;
 var polygon = require('@turf/helpers').polygon;
 
-var poly = polygon([[[0,0], [0,100], [100,100], [100,0]]]);
-var ptIn = point(50, 50);
-var ptOut = point(140, 150);
+var poly = polygon([[[0, 0], [0, 100], [100, 100], [100, 0], [0, 0]]]);
+var ptIn = point([50, 50]);
 
 var suite = new Benchmark.Suite('turf-inside');
 suite
-  .add('turf-inside',function () {
+  .add('turf-inside', function () {
     inside(ptIn, poly);
   })
   .on('cycle', function (event) {
     console.log(String(event.target));
   })
-  .on('complete', function () {
-    
-  })
+  .on('complete', function () { })
   .run();
