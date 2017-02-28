49f6279c104ee55f2803a24ffe442ad8950d6880
Rowan Winsemius <rowanwins@yahoo.com.au>
2017-03-01 00:18:07 +1100

    Update turf doco for difference to resolve #494 (#581)
    * Update turf doco for difference to ersolve #494
    
    * Shorten JSDocs return

--- packages/turf-difference/index.d.ts
+++ packages/turf-difference/index.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="geojson" />
 
 type Polygon = GeoJSON.Feature<GeoJSON.Polygon>;
+type MultiPolygon = GeoJSON.Feature<GeoJSON.MultiPolygon>;
 
 /**
  * http://turfjs.org/docs/#difference
  */
-declare function difference(poly1: Polygon, poly2: Polygon): Polygon;
+declare function difference(poly1: Polygon, poly2: Polygon): Polygon|MultiPolygon;
 declare namespace difference { }
 export = difference;
