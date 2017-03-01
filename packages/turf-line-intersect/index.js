42b024c102749956c31f7c41c37828a3e9683982
Denis <carriere.denis@gmail.com>
2017-03-01 09:52:55 -0500

    Add dynamic tests

--- packages/turf-line-intersect/index.js
+++ packages/turf-line-intersect/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
+var coordEach = require('@turf/meta').coordEach;
+var rbush = require('rbush');
+
 /**
  * Takes two GeoJSON LineStrings and returns the intersecting point(s).
  *
  * @name lineIntersect
  * @param {Feature<LineString>} line1 GeoJSON LineString Feature
  * @param {Feature<LineString>} line2 GeoJSON LineString Feature
+ * @param {boolean} [debug=false] Debug mode
  * @returns {FeatureCollection<Point>} point(s) that intersect both lines
  * @example
  * var line1 = {
@@ -45,6 +49,22 @@
  * var points = turf.lineIntersect(line1, line2);
  * //= points
  */
-module.exports = function (line1, line2) {
-    return [line1, line2];
+module.exports = function (line1, line2, debug) {
+    var tree1 = lineTree(line1);
+    return line1;
 };
+
+/**
+ * Builds two coordinate line segment RBush index from LineString
+ *
+ * @private
+ * @param {Feature<LineString>} line GeoJSON LineString
+ * @returns {RBush} RBush Tree
+ */
+function lineTree(line) {
+    var tree = rbush();
+    coordEach(line, function (coords) {
+        console.log(coords);
+    });
+    return tree;
+}
--- packages/turf-line-intersect/test.js
+++ packages/turf-line-intersect/test.js
@@ -1,14 +1,41 @@
-var test = require('tape');
-// var path = require('path');
-// var load = require('load-json-file');
-// var write = require('write-json-file');
-// var lineIntersect = require('.');
+const test = require('tape');
+const fs = require('fs');
+const path = require('path');
+const load = require('load-json-file');
+const write = require('write-json-file');
+const mkdirp = require('mkdirp');
+const lineIntersect = require('.');
 
-// var directories = {
-//     in: path.join(__dirname, 'test', 'in') + path.sep,
-//     out: path.join(__dirname, 'test', 'out') + path.sep
-// };
+const directories = {
+    in: path.join(__dirname, 'test', 'in') + path.sep,
+    out: path.join(__dirname, 'test', 'out') + path.sep
+};
 
-test('line-intersect', (t) => {
+const fixtures = fs.readdirSync(directories.in).map(folder => {
+    return {
+        folder,
+        line1: load.sync(path.join(directories.in, folder, 'line1.geojson')),
+        line2: load.sync(path.join(directories.in, folder, 'line2.geojson'))
+    };
+});
+
+test('turf-line-intersect', t => {
+    for (const {folder, line1, line2} of fixtures) {
+        console.log('processing:', folder);
+
+        // Line Intersect
+        const points = lineIntersect(line1, line2);
+        const debug = lineIntersect(line1, line2, true);
+
+        // Save Results
+        mkdirp.sync(path.join(directories.out, folder));
+        if (process.env.REGEN) {
+            write.sync(path.join(directories.out, folder, 'results.geojson'), points);
+            write.sync(path.join(directories.out, folder, 'debug.geojson'), debug);
+        }
+
+        // Tests
+        t.deepEquals(points, load.sync(path.join(directories.out, folder, 'results.geojson')), folder);
+    }
     t.end();
 });
