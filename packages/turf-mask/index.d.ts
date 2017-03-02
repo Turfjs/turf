90c3142e25f991fe05bea6e76034462316749c2d
rowanwins <rowanwins@yahoo.com.au>
2017-03-02 16:31:49 +1100

    fix ts defintion

--- packages/turf-mask/index.d.ts
+++ packages/turf-mask/index.d.ts
@@ -6,7 +6,7 @@ interface Mask {
     /**
      * http://turfjs.org/docs/#mask
      */
-    (poly: Polygon, mask: Polygon): Polygon;
+    (poly: Polygon, mask?: Polygon): Polygon;
 }
 declare const mask: Mask;
 export = mask;
