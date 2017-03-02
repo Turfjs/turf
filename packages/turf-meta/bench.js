50f7f2ee965404f2f608ff0ed5192d9d504ab719
Denis <carriere.denis@gmail.com>
2017-03-01 21:08:22 -0500

    Improve turf.meta benchmark

--- packages/turf-meta/bench.js
+++ packages/turf-meta/bench.js
@@ -13,13 +13,31 @@ const suite = new Benchmark.Suite('turf-meta');
 
 Object.keys(fixtures).forEach(name => {
     const fixture = fixtures[name];
-    suite.add('coordEach#' + name, () => {
-        meta.coordEach(fixture, (coords, index) => { });
+    suite.add('coordEach     - ' + name, () => {
+        meta.coordEach(fixture, (currentCoords, currentIndex) => { });
     });
-    suite.add('coordReduce#' + name, () => {
-        meta.coordReduce(fixture, (previousValue, currentCoords, index) => { });
+    suite.add('coordReduce   - ' + name, () => {
+        meta.coordReduce(fixture, (previousValue, currentCoords, currentIndex) => { });
     });
-    suite.add('coordAll#' + name, () => {
+    suite.add('propEach      - ' + name, () => {
+        meta.propEach(fixture, (currentProperties, currentIndex) => { });
+    });
+    suite.add('propReduce    - ' + name, () => {
+        meta.propReduce(fixture, (previousValue, currentProperties, currentIndex) => { });
+    });
+    suite.add('geomEach      - ' + name, () => {
+        meta.geomEach(fixture, (currentGeometry, currentIndex) => { });
+    });
+    suite.add('geomReduce    - ' + name, () => {
+        meta.geomReduce(fixture, (previousValue, currentGeometry, currentIndex) => { });
+    });
+    suite.add('featureEach   - ' + name, () => {
+        meta.featureEach(fixture, (currentFeature, currentIndex) => { });
+    });
+    suite.add('featureReduce - ' + name, () => {
+        meta.featureReduce(fixture, (previousValue, currentFeature, currentIndex) => { });
+    });
+    suite.add('coordAll      - ' + name, () => {
         meta.coordAll(fixture);
     });
 });
