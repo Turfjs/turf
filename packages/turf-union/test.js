1c7f50f5475dc5c3263ac6eea1744a97135ba709
Denis <carriere.denis@gmail.com>
2017-03-04 12:15:12 -0500

    Add tests for Typescript definition

--- packages/turf-union/test.js
+++ packages/turf-union/test.js
@@ -1,12 +1,12 @@
-var union = require('./'),
-  test = require('tape'),
-  glob = require('glob'),
-  fs = require('fs');
+var union = require('./');
+var test = require('tape');
+var glob = require('glob');
+var fs = require('fs');
 
 var REGEN = false;
 
-test('union', function(t){
-  glob.sync(__dirname + '/test/fixtures/in/*.geojson').forEach(function(input) {
+test('union', function (t) {
+  glob.sync(__dirname + '/test/in/*.geojson').forEach(function (input) {
       var fcs = JSON.parse(fs.readFileSync(input));
       var args = [];
       for (var i = 0; i < fcs.length; i++) {
