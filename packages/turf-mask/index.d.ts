3b5d10ad3733750ef9c04f689f85a517c60f5e84
Denis <carriere.denis@gmail.com>
2017-03-05 01:09:51 -0500

    Add typescript namespace mask

--- packages/turf-mask/index.d.ts
+++ packages/turf-mask/index.d.ts
@@ -6,4 +6,5 @@ type Polygon = GeoJSON.Feature<GeoJSON.Polygon> | GeoJSON.Polygon;
  * http://turfjs.org/docs/#mask
  */
 declare function mask(poly: Polygon, mask?: Polygon): Polygon;
+declare namespace mask {}
 export = mask;
---
+++ packages/turf-mask/test/types.ts
@@ -0,0 +1,4 @@
+import * as mask from '../'
+
+const poly: GeoJSON.Feature<GeoJSON.Polygon> = undefined
+mask(poly, poly)
