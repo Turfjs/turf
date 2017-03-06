50e646fcb42a65000d1023d8b4b524301914f45a
Denis <carriere.denis@gmail.com>
2017-03-05 23:53:42 -0500

    Add benchmark to turf-dissolve

--- packages/turf-dissolve/test.js
+++ packages/turf-dissolve/test.js
@@ -8,28 +8,28 @@ var directories = {
     in: path.join(__dirname, 'test', 'in')
 }
 
-function save (directory, filename, features) {
-  return fs.writeFileSync(path.join(directory, filename), JSON.stringify(features, null, 2))
+function save(directory, filename, features) {
+    return fs.writeFileSync(path.join(directory, filename), JSON.stringify(features, null, 2))
 }
 
-function read (directory, filename) {
-  return JSON.parse(fs.readFileSync(path.join(directory, filename), 'utf8'))
+function read(directory, filename) {
+    return JSON.parse(fs.readFileSync(path.join(directory, filename), 'utf8'))
 }
 
 test('turf-dissolve', function (t) {
-    var polys = read(directories.in, 'polys.json');
+    var polys = read(directories.in, 'polys.geojson');
 
     // With Property
     var polysByProperty = dissolve(polys, 'combine');
-    if (process.env.REGEN) { save(directories.out, 'polysByProperty.json', polysByProperty); }
+    if (process.env.REGEN) { save(directories.out, 'polysByProperty.geojson', polysByProperty); }
     t.equal(polysByProperty.features.length, 3);
-    t.deepEqual(polysByProperty, read(directories.out, 'polysByProperty.json'));
+    t.deepEqual(polysByProperty, read(directories.out, 'polysByProperty.geojson'));
 
     // Without Property
     var polysWithoutProperty = dissolve(polys);
-    if (process.env.REGEN) { save(directories.out, 'polysWithoutProperty.json', polysWithoutProperty); }
+    if (process.env.REGEN) { save(directories.out, 'polysWithoutProperty.geojson', polysWithoutProperty); }
     t.equal(polysWithoutProperty.features.length, 2);
-    t.deepEqual(polysWithoutProperty, read(directories.out, 'polysWithoutProperty.json'));
+    t.deepEqual(polysWithoutProperty, read(directories.out, 'polysWithoutProperty.geojson'));
 
     t.end();
 });
