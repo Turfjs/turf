abe7c552ea5d0835dbdd7d005568c6cc964d6bc1
Denis <carriere.denis@gmail.com>
2017-03-07 08:43:15 -0500

    Fix JSDocs index.js - Fix geometry type lineString == LineString - Shorten coordinates

--- packages/turf-line-chunk/index.js
+++ packages/turf-line-chunk/index.js
@@ -9,34 +9,22 @@ var featureCollection = helpers.featureCollection;
 /**
  * Divides a {@link linestring} into chunks of a specified length.
  * If the line is shorter than the segment length then the orginal line is returned.
+ *
  * @name lineChunk
- * @param {Feature<linestring>} line the line to split
+ * @param {FeatureCollection|Feature<LineString|MultiLineString>} featureIn the lines to split
  * @param {number} segmentLength how long to make each segment
  * @param {string}[units='kilometers'] units can be degrees, radians, miles, or kilometers
- * @return {FeatureCollection<linestring>} collection of line segments
+ * @return {FeatureCollection<LineString>} collection of line segments
  * @example
  * var line = {
- *   'type': 'Feature',
- *   'properties': {},
- *   'geometry': {
- *     'type': 'lineString',
- *     'coordinates': [
- *       [
- *         -86.28524780273438,
- *         40.250184183819854
- *       ],
- *       [
- *         -85.98587036132812,
- *         40.17887331434696
- *       ],
- *       [
- *         -85.97213745117188,
- *         40.08857859823707
- *       ],
- *       [
- *         -85.77987670898438,
- *         40.15578608609647
- *       ]
+ *   "type": "Feature",
+ *   "properties": {},
+ *   "geometry": {
+ *     "type": "LineString",
+ *     "coordinates": [
+ *       [-95, 40],
+ *       [-93, 45],
+ *       [-85, 50]
  *     ]
  *   }
  * };
@@ -44,7 +32,7 @@ var featureCollection = helpers.featureCollection;
  * //=result
  */
 
-module.exports = function (line, segmentLength, units) {
+module.exports = function (featureIn, segmentLength, units) {
     if (line.type === 'LineString') {
         line = lineString(line.coordinates, line.properties);
     }
