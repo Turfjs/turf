cf998e4f441ba07975f0f4c6945136efb57fb3fa
Denis <carriere.denis@gmail.com>
2017-03-08 15:21:36 -0500

    Get mask ready for union - Tests still pass at this point

--- packages/turf-mask/index.js
+++ packages/turf-mask/index.js
@@ -1,5 +1,10 @@
-var helpers = require('@turf/helpers');
 var featureEach = require('@turf/meta').featureEach;
+var rbush = require('rbush');
+var turfBBox = require('@turf/bbox');
+var helpers = require('@turf/helpers');
+var lineString = helpers.lineString;
+var polygon = helpers.polygon;
+var featureCollection = helpers.featureCollection;
 
 /**
  * Takes any type of {@link Polygon|polygon} and an optional mask and returns a {@link Polygon|polygon} exterior ring with holes.
@@ -24,17 +29,18 @@ var featureEach = require('@turf/meta').featureEach;
  */
 module.exports = function (polygon, mask) {
     // Define mask
-    var maskCoordinates = createMask(mask);
-    var maskOuter = maskCoordinates[0];
-    var maskInners = maskCoordinates.slice(1);
+    var maskPolygon = createMask(mask);
 
     // Define polygon
     var separated = separatePolygonToLines(polygon);
     var polygonOuters = separated[0];
     var polygonInners = separated[1];
 
+    // Union Outers & Inners
+    // polygonOuters = unionLines(polygonOuters);
+
     // Create masked area
-    var masked = buildMask(maskOuter, maskInners, polygonOuters, polygonInners);
+    var masked = buildMask(maskPolygon, polygonOuters, polygonInners);
     return masked;
 };
 
@@ -42,18 +48,22 @@ module.exports = function (polygon, mask) {
  * Build Mask
  *
  * @private
- * @param {line} maskOuter Mask Outer
- * @param {Array<line>} maskInners Mask Inners
- * @param {Array<line>} polygonOuters Polygon Outers
- * @param {Array<line>} polygonInners Polygon Inners
+ * @param {Feature<Polygon>} maskPolygon Mask Outer
+ * @param {FeatureCollection<LineString>} polygonOuters Polygon Outers
+ * @param {FeatureCollection<LineString>} polygonInners Polygon Inners
  * @returns {Feature<Polygon>} Feature Polygon
  */
-function buildMask(maskOuter, maskInners, polygonOuters) {
-    var coordinates = [maskOuter];
-    polygonOuters.forEach(function (outer) {
-        coordinates.push(outer);
+function buildMask(maskPolygon, polygonOuters, polygonInners) {
+    var coordinates = maskPolygon.geometry.coordinates;
+
+    featureEach(polygonOuters, function (feature) {
+        coordinates.push(feature.geometry.coordinates);
     });
-    return helpers.polygon(coordinates);
+
+    featureEach(polygonInners, function (feature) {
+        coordinates.push(feature.geometry.coordinates);
+    });
+    return polygon(coordinates);
 }
 
 /**
@@ -61,7 +71,7 @@ function buildMask(maskOuter, maskInners, polygonOuters) {
  *
  * @private
  * @param {FeatureCollection|Feature<Polygon|MultiPolygon>} polygon GeoJSON Feature
- * @returns {Array<line[], line[]>} Outer & Inner lines
+ * @returns {Array<FeatureCollection<LineString>, FeatureCollection<LineString>>} Outer & Inner lines
  */
 function separatePolygonToLines(polygon) {
     var outers = [];
@@ -74,13 +84,13 @@ function separatePolygonToLines(polygon) {
             var coordinates = feature.geometry.coordinates;
             var featureOuter = coordinates[0];
             var featureInner = coordinates.slice(1);
-            outers.push(featureOuter);
+            outers.push(lineString(featureOuter));
             featureInner.forEach(function (inner) {
-                inners.push(inner);
+                inners.push(lineString(inner));
             });
         });
     });
-    return [outers, inners];
+    return [featureCollection(outers), featureCollection(inners)];
 }
 
 /**
@@ -103,10 +113,57 @@ function flattenMultiPolygon(multiPolygon) {
  *
  * @private
  * @param {Feature<Polygon>} [mask] default to world if undefined
- * @returns {coordinates} mask coordinate
+ * @returns {Feature<Polygon>} mask coordinate
  */
 function createMask(mask) {
     var world = [[[180, 90], [-180, 90], [-180, -90], [180, -90], [180, 90]]];
     var coordinates = mask && mask.geometry.coordinates || world;
-    return coordinates;
+    return polygon(coordinates);
+}
+
+/**
+ * Union Lines
+ *
+ * @private
+ * @param {FeatureCollection<LineString>} lines an array of line coordinates
+ * @returns {FeatureCollection<LineString>} lines that have been union if they collid
+ */
+function unionLines(lines) {
+    var tree = createIndex(lines);
+    var results = [];
+    featureEach(lines, function (feature) {
+        var bbox = turfBBox(feature);
+        var search = tree.search({
+            minX: bbox[0],
+            minY: bbox[1],
+            maxX: bbox[2],
+            maxY: bbox[3]
+        });
+        console.log(search);
+    });
+    return featureCollection(results);
+}
+
+/**
+ * Create RBush Tree Index
+ *
+ * @param {FeatureCollection<any>} features GeoJSON FeatureCollection
+ * @returns {RBush} RBush Tree
+ */
+function createIndex(features) {
+    var tree = rbush();
+    var load = [];
+    featureEach(features, function (feature, index) {
+        var bbox = turfBBox(feature);
+        load.push({
+            minX: bbox[0],
+            minY: bbox[1],
+            maxX: bbox[2],
+            maxY: bbox[3],
+            geojson: feature,
+            index: index
+        });
+    });
+    tree.load(load);
+    return tree;
 }
