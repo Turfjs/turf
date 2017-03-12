2441dcdb5a6e48e0127000834b3796e92b2dc50a
Denis <carriere.denis@gmail.com>
2017-03-12 12:18:13 -0400

    Update typescript featureIn param

--- packages/turf-line-segment/index.d.ts
+++ packages/turf-line-segment/index.d.ts
@@ -1,10 +1,8 @@
 import {LineString, MultiLineString, Polygon, MultiPolygon, LineStrings} from '@turf/helpers'
 
-type FeatureIn = LineString | MultiLineString | Polygon | MultiPolygon
-
 /**
  * http://turfjs.org/docs/#linesegment
  */
-declare function lineSegment(featureIn: FeatureIn): LineStrings;
+declare function lineSegment(geojson: LineString | MultiLineString | Polygon | MultiPolygon): LineStrings;
 declare namespace lineSegment {}
 export = lineSegment;
