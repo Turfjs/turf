0af1b0c55a1077377db5d205f0957494011fc72a
Denis <carriere.denis@gmail.com>
2017-03-12 18:06:17 -0400

    turf-point-grid add GeoJSON as valid bbox input

--- packages/turf-point-grid/test/types.ts
+++ packages/turf-point-grid/test/types.ts
@@ -1,10 +1,5 @@
 import {BBox, Points} from '@turf/helpers'
 import * as pointGrid from '../'
 
-const bbox: BBox = [
-    -96.6357421875,
-    31.12819929911196,
-    -84.9462890625,
-    40.58058466412764
-]
+const bbox: BBox = [-95, 30, -85, 40]
 const grid: Points = pointGrid(bbox, 50, 'miles')
