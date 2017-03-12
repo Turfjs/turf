db0d0abc2ebedda38f51b0e6b7ca0eba186f98e7
Denis <carriere.denis@gmail.com>
2017-03-11 20:25:46 -0500

    Improve turf-kinks testing & benchmark

--- packages/turf-kinks/bench.js
+++ packages/turf-kinks/bench.js
@@ -1,87 +1,29 @@
-var kinks = require('./');
-var Benchmark = require('benchmark');
-var fs = require('fs');
+const Benchmark = require('benchmark');
+const load = require('load-json-file');
+const path = require('path');
+const fs = require('fs');
+const kinks = require('./');
 
-var hourglass = {
-    type: "Polygon",
-    coordinates: [
-      [
-        [
-          -12.034835815429688,
-          8.901183448260598
-        ],
-        [
-          -12.060413360595701,
-          8.899826693726117
-        ],
-        [
-          -12.036380767822266,
-          8.873199368734273
-        ],
-        [
-          -12.059383392333983,
-          8.871418491385919
-        ],
-        [
-          -12.034835815429688,
-          8.901183448260598
-        ]
-      ]
-    ]
-  };
+const suite = new Benchmark.Suite('turf-kinks');
 
-var triple = {
-        type: "Polygon",
-        coordinates: [
-          [
-            [
-              -44.384765625,
-              1.0106897682409128
-            ],
-            [
-              -53.4375,
-              0.4833927027896987
-            ],
-            [
-              -43.154296875,
-              -6.402648405963884
-            ],
-            [
-              -53.173828125,
-              -6.708253968671543
-            ],
-            [
-              -43.857421875,
-              -13.752724664396975
-            ],
-            [
-              -54.09667968749999,
-              -14.944784875088372
-            ],
-            [
-              -53.3935546875,
-              -11.867350911459308
-            ],
-            [
-              -44.384765625,
-              1.0106897682409128
-            ]
-          ]
-        ]
-      };
+const directories = {
+    in: path.join(__dirname, 'test', 'in') + path.sep,
+    out: path.join(__dirname, 'test', 'out') + path.sep
+};
 
-var suite = new Benchmark.Suite('turf-kinks');
+const fixtures = fs.readdirSync(directories.in).map(filename => {
+    return {
+        filename,
+        name: path.parse(filename).name,
+        geojson: load.sync(directories.in + filename)
+    };
+});
+
+for (const {name, geojson} of fixtures) {
+    suite.add(name, () => { kinks(geojson); });
+}
+
 suite
-  .add('turf-kinks##hourglass',function () {
-    kinks(hourglass);
-  })
-  .add('turf-kinks##triple',function () {
-    kinks(triple);
-  })
-  .on('cycle', function (event) {
-    console.log(String(event.target));
-  })
-  .on('complete', function () {
-    
-  })
-  .run();
+    .on('cycle', e => console.log(String(e.target)))
+    .on('complete', () => {})
+    .run();
