324c434b4ca0f6068592b5513c9111daca411fd8
Denis <carriere.denis@gmail.com>
2017-03-15 10:27:56 -0400

    Update docs & fix linting

--- packages/turf-area/index.js
+++ packages/turf-area/index.js
@@ -4,6 +4,7 @@ var geomReduce = require('@turf/meta').geomReduce;
 /**
  * Takes one or more features and returns their area in square meters.
  *
+ * @name area
  * @param {FeatureCollection|Feature<any>} geojson input GeoJSON feature(s)
  * @returns {number} area in square meters
  * @addToMap polygon
@@ -35,4 +36,4 @@ module.exports = function (geojson) {
     return geomReduce(geojson, function (value, geometry) {
         return value + area(geometry);
     }, 0);
-}
+};
