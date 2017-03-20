104432498d8d9355f58a3b67e738ac64555e6de5
Denis <carriere.denis@gmail.com>
2017-03-19 22:38:45 -0400

    Refactor turf-square to use west/south syntax

--- packages/turf-square/index.js
+++ packages/turf-square/index.js
@@ -5,8 +5,9 @@ var distance = require('@turf/distance');
  * would contain the input.
  *
  * @name square
- * @param {Array<number>} bbox extent in [minX, minY, maxX, maxY] order
+ * @param {Array<number>} bbox extent in [west, south, east, north] order
  * @returns {Array<number>} a square surrounding `bbox`
+ * @addToMap features
  * @example
  * var bbox = [-20,-20,-15,0];
  *
@@ -23,23 +24,28 @@ var distance = require('@turf/distance');
  * //=features
  */
 module.exports = function (bbox) {
-    var horizontalDistance = distance(bbox.slice(0, 2), [bbox[2], bbox[1]], 'miles');
-    var verticalDistance = distance(bbox.slice(0, 2), [bbox[0], bbox[3]], 'miles');
+    var west = bbox[0];
+    var south = bbox[1];
+    var east = bbox[2];
+    var north = bbox[3];
+
+    var horizontalDistance = distance(bbox.slice(0, 2), [east, south]);
+    var verticalDistance = distance(bbox.slice(0, 2), [west, north]);
     if (horizontalDistance >= verticalDistance) {
-        var verticalMidpoint = (bbox[1] + bbox[3]) / 2;
+        var verticalMidpoint = (south + north) / 2;
         return [
-            bbox[0],
-            verticalMidpoint - ((bbox[2] - bbox[0]) / 2),
-            bbox[2],
-            verticalMidpoint + ((bbox[2] - bbox[0]) / 2)
+            west,
+            verticalMidpoint - ((east - west) / 2),
+            east,
+            verticalMidpoint + ((east - west) / 2)
         ];
     } else {
-        var horizontalMidpoint = (bbox[0] + bbox[2]) / 2;
+        var horizontalMidpoint = (west + east) / 2;
         return [
-            horizontalMidpoint - ((bbox[3] - bbox[1]) / 2),
-            bbox[1],
-            horizontalMidpoint + ((bbox[3] - bbox[1]) / 2),
-            bbox[3]
+            horizontalMidpoint - ((north - south) / 2),
+            south,
+            horizontalMidpoint + ((north - south) / 2),
+            north
         ];
     }
 };
