04a5cd1148d1f81ed017a45f2a310a1785309bf0
Denis <carriere.denis@gmail.com>
2017-03-25 21:48:35 -0400

    Refactor turf-line-intersect (#631)
    * Refactor turf-line-intersect
    
    * Truncate decimals

--- packages/turf-line-intersect/bench.js
+++ packages/turf-line-intersect/bench.js
@@ -2,32 +2,32 @@ const Benchmark = require('benchmark');
 const path = require('path');
 const fs = require('fs');
 const load = require('load-json-file');
-const lineIntersect = require('.');
+const lineIntersect = require('./');
 
-const suite = new Benchmark.Suite('turf-line-intersect');
-
-const directories = {
-    in: path.join(__dirname, 'test', 'in') + path.sep,
-    out: path.join(__dirname, 'test', 'out') + path.sep
-};
-
-let fixtures = fs.readdirSync(directories.in).map(folder => {
+const directory = path.join(__dirname, 'test', 'in') + path.sep;
+const fixtures = fs.readdirSync(directory).map(filename => {
     return {
-        folder,
-        line1: load.sync(path.join(directories.in, folder, 'line1.geojson')),
-        line2: load.sync(path.join(directories.in, folder, 'line2.geojson'))
+        filename,
+        name: path.parse(filename).name,
+        geojson: load.sync(directory + filename)
     };
 });
-// const include = [
-//     'multi-linestring'
-// ];
-// fixtures = fixtures.filter(fixture => include.indexOf(fixture.folder) !== -1);
 
-for (const {folder, line1, line2} of fixtures) {
-    suite.add(folder, () => lineIntersect(line1, line2));
+/**
+ * Benchmark Results
+ *
+ * 2-vertex-segment x 7,177,957 ops/sec ±0.83% (92 runs sampled)
+ * double-intersect x 88,165 ops/sec ±3.02% (78 runs sampled)
+ * multi-linestring x 19,554 ops/sec ±2.09% (77 runs sampled)
+ * multi-polygon x 11,149 ops/sec ±2.05% (74 runs sampled)
+ * same-coordinates x 67,439 ops/sec ±2.01% (73 runs sampled)
+ */
+const suite = new Benchmark.Suite('turf-line-intersect');
+for (const {name, geojson} of fixtures) {
+    suite.add(name, () => lineIntersect(geojson.features[0], geojson.features[1]));
 }
 
 suite
-    .on('cycle', (event) => { console.log(String(event.target)); })
+    .on('cycle', e => { console.log(String(e.target)); })
     .on('complete', () => {})
     .run();
--- packages/turf-line-intersect/test/in/empty-line2/line2.geojson
+++
@@ -1,4 +0,0 @@
-{
-  "type": "FeatureCollection",
-  "features": []
-}
