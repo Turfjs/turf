f8a82086a7e8063be3e6fdc557d07b5a190b8c7b
Denis <carriere.denis@gmail.com>
2017-03-25 23:10:51 -0400

    Add BBox to line-segment

--- packages/turf-line-segment/index.js
+++ packages/turf-line-segment/index.js
@@ -8,7 +8,7 @@ var getCoords = require('@turf/invariant').getCoords;
  * Creates a {@link FeatureCollection} of 2-vertex {@link LineString} segments from a {@link LineString}, {@link MultiLineString}, {@link MultiPolygon} or {@link Polygon}.
  *
  * @name lineSegment
- * @param {Feature<LineString|MultiLineString|MultiPolygon|Polygon>} geojson input GeoJSON Feature
+ * @param {FeatureCollection|Feature<LineString|MultiLineString|MultiPolygon|Polygon>} geojson GeoJSON Polygon or LineString
  * @returns {FeatureCollection<LineString>} 2-vertex line segments
  * @example
  * var polygon = {
@@ -26,22 +26,57 @@ module.exports = function (geojson) {
     var results = [];
     featureEach(geojson, function (multiFeature) {
         featureEach(flatten(multiFeature), function (feature) {
-            var linestrings;
+            var coords = [];
             var type = (feature.geometry) ? feature.geometry.type : feature.type;
             switch (type) {
             case 'Polygon':
-                linestrings = getCoords(feature);
+                coords = getCoords(feature);
                 break;
             case 'LineString':
-                linestrings = [getCoords(feature)];
+                coords = [getCoords(feature)];
             }
-            linestrings.forEach(function (linestring) {
-                linestring.reduce(function (previousCoords, currentCoords) {
-                    results.push(lineString([previousCoords, currentCoords], feature.properties));
-                    return currentCoords;
-                });
+            coords.forEach(function (coord) {
+                var segments = createSegments(coord, feature.properties);
+                segments.forEach(function (segment) { results.push(segment); });
             });
         });
     });
     return featureCollection(results);
 };
+
+/**
+ * Create Segments from LineString coordinates
+ *
+ * @param {LineString} coords LineString coordinates
+ * @param {*} properties GeoJSON properties
+ * @returns {Array<Feature<LineString>>} line segments
+ */
+function createSegments(coords, properties) {
+    var segments = [];
+    coords.reduce(function (previousCoords, currentCoords) {
+        var segment = lineString([previousCoords, currentCoords], properties);
+        segment.bbox = bbox(previousCoords, currentCoords);
+        segments.push(segment);
+        return currentCoords;
+    });
+    return segments;
+}
+
+/**
+ * Create BBox between two coordinates (faster than @turf/bbox)
+ *
+ * @param {[number, number]} coords1 Point coordinate
+ * @param {[number, number]} coords2 Point coordinate
+ * @returns {BBox} [west, south, east, north]
+ */
+function bbox(coords1, coords2) {
+    var x1 = coords1[0];
+    var y1 = coords1[1];
+    var x2 = coords2[0];
+    var y2 = coords2[1];
+    var west = (x1 < x2) ? x1 : x2;
+    var south = (y1 < y2) ? y1 : y2;
+    var east = (x1 > x2) ? x1 : x2;
+    var north = (y1 > y2) ? y1 : y2;
+    return [west, south, east, north];
+}
