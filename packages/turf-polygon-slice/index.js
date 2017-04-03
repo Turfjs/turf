8ef150134b5ec424551fd460a482829c6ba32e97
Denis <carriere.denis@gmail.com>
2017-04-02 21:49:01 -0400

    Update to turf-line-split

--- packages/turf-polygon-slice/index.js
+++ packages/turf-polygon-slice/index.js
@@ -1,22 +1,16 @@
-var rbush = require('geojson-rbush');
-var clone = require('clone');
-var meta = require('@turf/meta');
 var helpers = require('@turf/helpers');
+var lineSplit = require('@turf/line-split');
 var getCoords = require('@turf/invariant').getCoords;
-var lineSegment = require('@turf/line-segment');
-var lineIntersect = require('@turf/line-intersect');
+var featureEach = require('@turf/meta').featureEach;
 var featureCollection = helpers.featureCollection;
 var lineString = helpers.lineString;
-var coordEach = meta.coordEach;
-var featureEach = meta.featureEach;
-var featureReduce = meta.featureReduce;
 
 /**
  * Slices {@link Polygon} using a {@link Linestring}.
  *
  * @name polygonSlice
- * @param {Feature<Polygon>} polygon Polygon to slice
- * @param {Feature<LineString>} linestring LineString used to slice Polygon
+ * @param {Feature<Polygon>} poly Polygon to slice
+ * @param {Feature<LineString>} splitter LineString used to slice Polygon
  * @returns {FeatureCollection<Polygon>} Sliced Polygons
  * @example
  * var polygon = {
@@ -45,83 +39,44 @@ var featureReduce = meta.featureReduce;
  * var sliced = turf.polygonSlice(polygon, linestring);
  * //=sliced
 */
-module.exports = function polygonSlice(polygon, linestring) {
-    linestring = clone(linestring);
-    polygon = clone(polygon);
+module.exports = function polygonSlice(poly, splitter) {
+    var results = [];
+    var coords = getCoords(poly);
+    var outer = lineString(coords[0]);
+    var inners = innerLineStrings(poly);
 
-    var splitLines = lineSplit(linestring, polygon);
-    var splitPolygons = lineSplit(polygon, linestring);
+    // Split outers
+    featureEach(lineSplit(outer, splitter), function (line) {
+        results.push(line);
+    });
 
-    return featureCollection(splitLines.features.concat(splitPolygons.features));
+    // Split inners
+    featureEach(inners, function (inner) {
+        featureEach(lineSplit(inner, splitter), function (line) {
+            results.push(line);
+        });
+    });
+
+    // Split splitter
+    featureEach(lineSplit(splitter, poly), function (line) {
+        results.push(line);
+    });
+
+    return featureCollection(results);
 };
 
 /**
- * Split a LineString at the intersections of another LineString.
+ * Retrieve inner linestrings from polygon
  *
  * @private
- * @param {Feature<LineString>} source Line Feature to split
- * @param {Feature<LineString>} target Line Feature used to find intersections
- * @returns {FeatureCollection<LineString>} Split lines
+ * @param {Feature<Polygon>} poly Feature Polygon
+ * @returns {FeatureCollection<LineString>} inner lines from polygon
  */
-function lineSplit(source, target) {
+function innerLineStrings(poly) {
     var results = [];
-
-    // Spatial Index
-    var tree = rbush();
-    tree.load(lineSegment(target));
-
-    // Lines of 2-vertex
-    var segments = lineSegment(source);
-    var initialValue = segments.features[0];
-
-    var last = featureReduce(segments, function (previous, current, index) {
-        var matched = false;
-        var lastCoord = getCoords(clone(current))[1];
-
-        featureEach(tree.search(current), function (polySegment) {
-            if (!matched) {
-                // Segments match
-                var intersect = lineIntersect(current, polySegment).features[0];
-                if (intersect) {
-                    // Create Segment
-                    var newSegment = clone(previous);
-                    if (index === 0) {
-                        newSegment = lineString([getCoords(current)[0], getCoords(intersect)]);
-                    } else {
-                        newSegment.geometry.coordinates.push(getCoords(intersect));
-                    }
-                    // Push new split lines to results
-                    if (validSegment(newSegment)) {
-                        results.push(newSegment);
-                    }
-                    // Restart previous value to intersection point
-                    previous.geometry.coordinates = [intersect.geometry.coordinates];
-                    matched = true;
-                }
-            }
-        });
-        // Append last coordinate of current segment
-        previous.geometry.coordinates.push(lastCoord);
-        return previous;
-    }, initialValue);
-
-    // Add the end segment to the lineEnd of the line
-    if (validSegment(last)) {
-        results.push(last);
-    }
-    return featureCollection(results);
-}
-
-/**
- * Validate Segment - Must contain more than 1 unique point
- *
- * @param {Feature<LineString>} segment Line Segment
- * @returns {boolean} true if segment is valid
- */
-function validSegment(segment) {
-    var uniques = {};
-    coordEach(segment, function (coord) {
-        uniques[coord.join(',')] = true;
+    var coords = getCoords(poly);
+    coords.slice(1, coords.length).forEach(function (coord) {
+        results.push(lineString(coord));
     });
-    return Object.keys(uniques).length > 1;
+    return featureCollection(results);
 }
