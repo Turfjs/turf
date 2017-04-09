4da5c5a767782be80a78142ad450ff33754be7f5
Denis <carriere.denis@gmail.com>
2017-04-09 14:16:23 -0400

    Update Typescript defintion with distance Units

--- packages/turf-sector/index.d.ts
+++ packages/turf-sector/index.d.ts
@@ -1,12 +1,10 @@
 /// <reference types="geojson" />
 
-type Point = GeoJSON.Feature<GeoJSON.Point>;
-type Polygon = GeoJSON.Feature<GeoJSON.Polygon>;
+import {Point, Polygon, Units} from '@turf/helpers';
 
 /**
  * http://turfjs.org/docs/#sector
  */
-declare function sector(center: Point, radius: number, bearing1: number, bearing2: number, steps?: number, units?: string): Polygon;
-declare namespace sector {
-}
+declare function sector(center: Point, radius: number, bearing1: number, bearing2: number, steps?: number, units?: Units): Polygon;
+declare namespace sector {}
 export = sector;
