2cbeeaea19e1842a61cd4f4156e5f550f36c83f4
Denis <carriere.denis@gmail.com>
2017-04-09 14:00:13 -0400

    Update benchmark results

--- packages/turf-sector/bench.js
+++ packages/turf-sector/bench.js
@@ -15,12 +15,12 @@ const fixtures = fs.readdirSync(directory).map(filename => {
 /**
  * Benchmark Results
  *
- * sector-full-360 x 45,292 ops/sec ±1.33% (89 runs sampled)
- * sector-greater-360 x 379,941 ops/sec ±10.07% (70 runs sampled)
- * sector1 x 291,012 ops/sec ±1.83% (90 runs sampled)
- * sector2 x 110,804 ops/sec ±1.39% (91 runs sampled)
- * sector3 x 57,071 ops/sec ±0.78% (91 runs sampled)
- * sector4 x 39,117 ops/sec ±0.93% (93 runs sampled)
+ * sector-full-360 x 46,260 ops/sec ±0.96% (91 runs sampled)
+ * sector-greater-360 x 516,991 ops/sec ±1.13% (88 runs sampled)
+ * sector1 x 271,179 ops/sec ±5.04% (82 runs sampled)
+ * sector2 x 105,831 ops/sec ±2.57% (86 runs sampled)
+ * sector3 x 56,994 ops/sec ±1.29% (89 runs sampled)
+ * sector4 x 37,871 ops/sec ±3.13% (86 runs sampled)
  */
 const suite = new Benchmark.Suite('turf-sector');
 for (const {name, geojson} of fixtures) {
