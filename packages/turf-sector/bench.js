4a9db06e7a0ccf7d11e03e97e08901a396fe0826
Denis <carriere.denis@gmail.com>
2017-04-09 13:56:35 -0400

    Add benchmark

--- packages/turf-sector/bench.js
+++ packages/turf-sector/bench.js
@@ -1,23 +1,34 @@
-var sector = require('./');
-var Benchmark = require('benchmark');
+const Benchmark = require('benchmark');
+const path = require('path');
+const fs = require('fs');
+const load = require('load-json-file');
+const sector = require('./');
 
-var center = {
-    type: "Feature",
-    geometry: {type: "Point", coordinates: [-75.0, 39.0]}
-};
-var radius = 5;
-var steps = 10;
-var bearing1 = 30;
-var bearing2 = 90;
-var suite = new Benchmark.Suite('turf-sector');
+const directory = path.join(__dirname, 'test', 'in') + path.sep;
+const fixtures = fs.readdirSync(directory).map(filename => {
+    return {
+        name: path.parse(filename).name,
+        geojson: load.sync(directory + filename)
+    };
+});
+
+/**
+ * Benchmark Results
+ *
+ * sector-full-360 x 45,292 ops/sec ±1.33% (89 runs sampled)
+ * sector-greater-360 x 379,941 ops/sec ±10.07% (70 runs sampled)
+ * sector1 x 291,012 ops/sec ±1.83% (90 runs sampled)
+ * sector2 x 110,804 ops/sec ±1.39% (91 runs sampled)
+ * sector3 x 57,071 ops/sec ±0.78% (91 runs sampled)
+ * sector4 x 39,117 ops/sec ±0.93% (93 runs sampled)
+ */
+const suite = new Benchmark.Suite('turf-sector');
+for (const {name, geojson} of fixtures) {
+    const {radius, bearing1, bearing2} = geojson.properties;
+    suite.add(name, () => sector(geojson, radius, bearing1, bearing2));
+}
 
 suite
-    .add('turf-sector', function () {
-        sector(center, radius, bearing1, bearing2, steps, 'kilometers');
-    })
-    .on('cycle', function (event) {
-        console.log(String(event.target));
-    })
-    .on('complete', function () {
-    })
+    .on('cycle', e => console.log(String(e.target)))
+    .on('complete', () => {})
     .run();
