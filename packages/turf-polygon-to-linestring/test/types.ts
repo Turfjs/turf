e091d6d8e45f1b12171840bc2c31c0faa7e74151
Denis <carriere.denis@gmail.com>
2017-04-09 20:50:29 -0400

    Remove extra spaces in types.ts

--- packages/turf-polygon-to-linestring/test/types.ts
+++ packages/turf-polygon-to-linestring/test/types.ts
@@ -1,13 +1,13 @@
 import {Polygon} from '@turf/helpers'
 import * as polygonToLineString from '../'
 
- var poly: Polygon = {
-   'type': 'Feature',
-   'properties': {},
-   'geometry': {
-     'type': 'Polygon',
-     'coordinates': [[[125, -30], [145, -30], [145, -20], [125, -20], [125, -30]]
-     ]
-   }
- }
- var line = polygonToLineString(poly);
+const poly: Polygon = {
+  'type': 'Feature',
+  'properties': {},
+  'geometry': {
+    'type': 'Polygon',
+    'coordinates': [[[125, -30], [145, -30], [145, -20], [125, -20], [125, -30]]
+    ]
+  }
+}
+const line = polygonToLineString(poly);
