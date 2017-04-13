971d1dfe6729952c985554f644f74f4d05ab91f6
stebogit <stefano.borghi@gmail.com>
2017-04-12 23:13:49 -0700

    replaced loops with featureEach; added setColor function in test.js; removed unused dependences; modified test input;

--- packages/turf-interpolate/index.js
+++ packages/turf-interpolate/index.js
@@ -1,9 +1,3 @@
-// var point = helpers.point;
-// var distance = require('@turf/distance');
-// var area = require('@turf/area');
-// var destination = require('@turf/destination');
-// var helpers = require('@turf/helpers');
-// var explode = require('@turf/explode');
 var inside = require('@turf/inside');
 var invariant = require('@turf/invariant');
 var featureEach = require('@turf/meta').featureEach;
@@ -23,17 +17,9 @@ var tin = require('@turf/tin');
  * @param {string} [units=kilometers] miles, kilometers
  * @returns {FeatureCollection<Point>} grid of points with 'property'
  * @example
- * var center = {
- *   "type": "Feature",
- *   "properties": {},
- *   "geometry": {
- *     "type": "Point",
- *     "coordinates": [-75, 40]
- *   }
- * }
- * var radius = 5;
- * var bearing1 = 25;
- * var bearing2 = 47;
+
+
+
  *
  * var arc = turf.lineArc(center, radius, bearing1, bearing2);
  *
@@ -53,33 +39,21 @@ module.exports = function (points, cellSize, property, units) {
     var bboxBBox = bbox(points); // [minX, minY, maxX, maxY]
     var squareBBox = square(bboxBBox);
 
-    // cellSize = cellSize || distance(
-    //         point([squareBBox[0], squareBBox[1]]),
-    //         point([squareBBox[2], squareBBox[1]]),
-    //         units
-    //     ) / resolution;
+    var x = JSON.stringify(tinResult);
 
     var gridResult = grid(squareBBox, cellSize, units, true);
 
     // add property value to each point of the grid
-    for (var i = 0; i < gridResult.features.length; i++) {
-        var pt = gridResult.features[i];
-        for (var j = 0; j < tinResult.features.length; j++) {
-            var triangle = tinResult.features[j];
+    featureEach(gridResult, function (pt) {
+        featureEach(tinResult, function (triangle) {
             if (inside(pt, triangle)) {
                 pt.properties = {};
                 pt.properties[property] = planepoint(pt, triangle);
             } else {
                 pt.properties[property] = 0;
             }
-        }
-    }
-
-    // featureEach(points, function (point) {
-    //     var lat = getLatitude(point);
-    //     if (!unique[lat]) unique[lat] = [];
-    //     unique[lat].push(point);
-    // });
+        });
+    });
 
     return gridResult;
 };
--- packages/turf-interpolate/test.js
+++ packages/turf-interpolate/test.js
@@ -4,6 +4,7 @@ const path = require('path');
 const load = require('load-json-file');
 const write = require('write-json-file');
 const truncate = require('@turf/truncate');
+const featureEach = require('@turf/meta').featureEach;
 const featureCollection = require('@turf/helpers').featureCollection;
 const interpolate = require('./');
 
@@ -23,17 +24,30 @@ const fixtures = fs.readdirSync(directories.in).map(filename => {
 test('turf-interpolate', t => {
     for (const {filename, name, geojson}  of fixtures) {
         const {cellSize} = geojson.properties;
+
+    // const coloredGeojson = setColor(geojson);
+    //     const gjs = JSON.stringify(coloredGeojson);
+
         const grid = truncate(interpolate(geojson, cellSize));
         // const results = featureCollection([geojson, grid]);
-        const g = JSON.stringify(grid);
+        const coloredGrid = setColor(grid);
+        const g = JSON.stringify(coloredGrid);
 
-        // featureEach(points, function (point) {
-            // set color
-            // "marker-color": "#F00" #0000ff -> #00ffff
-        // });
 
-    if (process.env.REGEN) write.sync(directories.out + filename, grid);
-        t.deepEquals(results, load.sync(directories.out + filename), name);
+        if (process.env.REGEN) write.sync(directories.out + filename, grid);
+            t.deepEquals(grid, load.sync(directories.out + filename), name);
     }
     t.end();
 });
+
+
+function setColor(grid) {
+    featureEach(grid, function (pt) {
+        // set color #0000ff -> #00ffff
+        const elev = Math.round(pt.properties.elevation);
+        const shade = elev.toString(16);
+        const color = '#' + shade + shade + shade;
+        pt.properties['marker-color'] = color;
+    });
+    return grid;
+}
