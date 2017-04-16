7727791f04818aa65c8208d93e7991782f9d7c57
Denis <carriere.denis@gmail.com>
2017-04-15 20:00:48 -0400

    Add properties param to turf-circle

--- packages/turf-circle/bench.js
+++ packages/turf-circle/bench.js
@@ -1,21 +1,21 @@
-var circle = require('./');
-var Benchmark = require('benchmark');
+const circle = require('./');
+const Benchmark = require('benchmark');
 
-var center = {
-    type: "Feature",
-    geometry: {type: "Point", coordinates: [-75.0, 39.0]}
-};
-var radius = 5;
-var steps = 10;
+const center = [-75.0, 39.0];
+const radius = 5;
 
-var suite = new Benchmark.Suite('turf-circle');
-
+/**
+ * Benchmark Results
+ *
+ * turf-circle - 16 steps x 140,793 ops/sec ±5.92% (79 runs sampled)
+ * turf-circle - 32 steps x 84,428 ops/sec ±2.28% (86 runs sampled)
+ * turf-circle - 64 steps x 45,202 ops/sec ±1.85% (88 runs sampled)
+ */
+const suite = new Benchmark.Suite('turf-circle');
 suite
-    .add('turf-circle',function () {
-        circle(center, radius, steps, 'kilometers');
-    })
-    .on('cycle', function (event) {
-        console.log(String(event.target));
-    })
-    .on('complete', function () {})
+    .add('turf-circle - 16 steps', () => circle(center, radius, 16))
+    .add('turf-circle - 32 steps', () => circle(center, radius, 32))
+    .add('turf-circle - 64 steps', () => circle(center, radius, 64))
+    .on('cycle', e => console.log(String(e.target)))
+    .on('complete', () => {})
     .run();
