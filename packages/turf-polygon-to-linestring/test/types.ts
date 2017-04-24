f2328d0cbb1f9ee9394634f74534a31e6ad5832c
Denis <carriere.denis@gmail.com>
2017-04-24 00:17:11 -0400

    Change output types @turf/polygon-to-linestring - Change output types - Update Typescript definition - Add properties param to translate properties to GeoJSON - Update fixtures

--- packages/turf-polygon-to-linestring/test/types.ts
+++ packages/turf-polygon-to-linestring/test/types.ts
@@ -1,13 +1,26 @@
-import {Polygon} from '@turf/helpers'
+import {Polygon, MultiPolygon} from '@turf/helpers'
 import * as polygonToLineString from '../'
 
 const poly: Polygon = {
-  'type': 'Feature',
-  'properties': {},
-  'geometry': {
-    'type': 'Polygon',
-    'coordinates': [[[125, -30], [145, -30], [145, -20], [125, -20], [125, -30]]
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [[[125, -30], [145, -30], [145, -20], [125, -20], [125, -30]]]
+  }
+}
+
+const multiPoly: MultiPolygon = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "MultiPolygon",
+    "coordinates": [
+      [[[125, -30], [145, -30], [145, -20], [125, -20], [125, -30]]],
+      [[[135, -40], [155, -40], [155, -30], [135, -30], [135, -40]]]
     ]
   }
 }
-const line = polygonToLineString(poly);
+
+const feature = polygonToLineString(poly);
+const collection = polygonToLineString(multiPoly);
