d10375833b0477ac2b259384306dcdcb56fa520c
Denis <carriere.denis@gmail.com>
2017-04-25 00:00:09 -0400

    Change output types @turf/polygon-to-linestring (#686)
    * Change output types @turf/polygon-to-linestring
    - Change output types
    - Update Typescript definition
    - Add properties param to translate properties to GeoJSON
    - Update fixtures
    
    * Add outer-doughnut fixture

--- packages/turf-polygon-to-linestring/bench.js
+++ packages/turf-polygon-to-linestring/bench.js
@@ -16,11 +16,11 @@ const fixtures = fs.readdirSync(directory).map(filename => {
 /**
  * Benchmark Results
  *
- * geometry-polygon x 8,237,334 ops/sec ±1.12% (92 runs sampled)
- * multi-polygon-with-holes x 2,665,487 ops/sec ±1.62% (88 runs sampled)
- * multi-polygon x 3,109,822 ops/sec ±1.13% (87 runs sampled)
- * polygon-with-hole x 8,212,111 ops/sec ±0.94% (88 runs sampled)
- * polygon x 8,468,172 ops/sec ±0.96% (89 runs sampled)
+ * geometry-polygon x 10,766,300 ops/sec ±1.20% (88 runs sampled)
+ * multi-polygon-with-holes x 2,597,052 ops/sec ±3.29% (81 runs sampled)
+ * multi-polygon x 3,103,830 ops/sec ±2.59% (81 runs sampled)
+ * polygon-with-hole x 10,533,741 ops/sec ±1.15% (88 runs sampled)
+ * polygon x 10,651,632 ops/sec ±0.77% (89 runs sampled)
  */
 const suite = new Benchmark.Suite('turf-polygon-to-linestring');
 for (const {name, geojson} of fixtures) {
