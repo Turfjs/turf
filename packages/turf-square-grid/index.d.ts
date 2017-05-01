ddea1bc8040e2a2121a0bea49c6cdb82c8ca921b
Denis <carriere.denis@gmail.com>
2017-05-01 14:29:09 -0400

    Update dependent types

--- packages/turf-square-grid/index.d.ts
+++ packages/turf-square-grid/index.d.ts
@@ -3,6 +3,6 @@ import {Units, BBox, Polygons, Feature, Features} from '@turf/helpers'
 /**
  * http://turfjs.org/docs/#squaregrid
  */
-declare function squareGrid(bbox: BBox | Feature | Features, cellSize: number, units?: Units, completelyWithin?: boolean): Polygons;
+declare function squareGrid(bbox: BBox | Feature<any> | Features<any>, cellSize: number, units?: Units, completelyWithin?: boolean): Polygons;
 declare namespace squareGrid { }
 export = squareGrid;
