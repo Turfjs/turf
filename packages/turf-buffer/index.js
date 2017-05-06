974fb558f74c00a423a97c0d0f8437e9c3096040
Denis <carriere.denis@gmail.com>
2017-05-06 14:01:26 -0400

    Test fails - input mutation

--- packages/turf-buffer/index.js
+++ packages/turf-buffer/index.js
@@ -1,21 +1,21 @@
-var Offset = require('polygon-offset');
-var helpers = require('@turf/helpers');
-var circle = require('@turf/circle');
-var dissolve = require('@turf/dissolve');
 var meta = require('@turf/meta');
+var Offset = require('polygon-offset');
+var circle = require('@turf/circle');
+var helpers = require('@turf/helpers');
+var dissolve = require('@turf/dissolve');
 var getCoords = require('@turf/invariant').getCoords;
+var point = helpers.point;
+var polygon = helpers.polygon;
 var coordEach = meta.coordEach;
 var featureEach = meta.featureEach;
 var featureCollection = helpers.featureCollection;
 var distanceToDegrees = helpers.distanceToDegrees;
-var polygon = helpers.polygon;
-var point = helpers.point;
 
 /**
  * Calculates a buffer for input features for a given radius. Units supported are miles, kilometers, and degrees.
  *
  * @name buffer
- * @param {FeatureCollection|Feature<any>} feature input to be buffered
+ * @param {FeatureCollection|Geometry|Feature<any>} feature input to be buffered
  * @param {number} radius distance to draw the buffer
  * @param {string} [units=kilometers] any of the options supported by turf units
  * @param {number} [steps=64] number of steps
@@ -64,30 +64,32 @@ module.exports = function (geojson, radius, units, steps) {
  * Buffer single Feature
  *
  * @private
- * @param {Feature<any>} feature input to be buffered
+ * @param {Geometry|Feature<any>} geojson input to be buffered
  * @param {number} radius distance to draw the buffer
  * @param {string} [units='kilometers'] any of the options supported by turf units
  * @param {number} [steps=64] number of steps
  * @returns {Feature<Polygon|MultiPolygon>} buffered feature
  */
-function buffer(feature, radius, units, steps) {
+function buffer(geojson, radius, units, steps) {
     // validation
+    if (!geojson) throw new Error('geojson is required');
+    if (radius === undefined || radius === null) throw new Error('radius is required');
     if (steps <= 0) throw new Error('steps must be greater than 0');
 
     // default params
-    var properties = feature.properties || {};
+    var properties = geojson.properties || {};
     var distance = distanceToDegrees(radius, units);
-    var geometry = (feature.type === 'Feature') ? feature.geometry : feature;
-    var coords = getCoords(feature);
+    var coords = getCoords(geojson);
+    var type = (geojson.geometry) ? geojson.geometry.type : geojson.type;
 
-    switch (geometry.type) {
+    switch (type) {
     case 'Point':
-        var pointBuffer = circle(feature, radius, steps, units);
+        var pointBuffer = circle(coords, radius, steps, units);
         pointBuffer.properties = properties;
         return pointBuffer;
     case 'MultiPoint':
         var polys = [];
-        coordEach(feature, function (coord) {
+        coordEach(geojson, function (coord) {
             var poly = circle(point(coord, properties), radius, steps, units);
             poly.properties = properties;
             polys.push(poly);
@@ -104,7 +106,5 @@ function buffer(feature, radius, units, steps) {
         var polyOffset = new Offset(coords, (steps > 32) ? 32 : steps);
         var polyBuffer = polyOffset.margin(distance);
         return polygon(polyBuffer, properties);
-    default:
-        throw new Error('geometry type ' + geometry.type + ' not supported');
     }
 }
--- packages/turf-buffer/test.js
+++ packages/turf-buffer/test.js
@@ -1,11 +1,11 @@
-const test = require('tape');
 const fs = require('fs');
+const test = require('tape');
 const path = require('path');
 const load = require('load-json-file');
 const write = require('write-json-file');
 const truncate = require('@turf/truncate');
-const featureEach = require('@turf/meta').featureEach;
-const featureCollection = require('@turf/helpers').featureCollection;
+const {featureEach} = require('@turf/meta');
+const {featureCollection, point, polygon} = require('@turf/helpers');
 const buffer = require('./');
 
 const directories = {
@@ -21,7 +21,7 @@ const fixtures = fs.readdirSync(directories.in).map(filename => {
     };
 });
 
-test('turf-buffer', function (t) {
+test('turf-buffer', t => {
     for (const {filename, name, geojson} of fixtures) {
         let {radius, units, padding} = geojson.properties || {};
         radius = radius || 50;
@@ -37,5 +37,33 @@ test('turf-buffer', function (t) {
         if (process.env.REGEN) write.sync(directories.out + filename, results);
         t.deepEqual(results, load.sync(directories.out + filename), name);
     }
+    t.end();
+});
+
+test('turf-buffer - Support Geometry Objects', t => {
+    const pt = point([61, 5]);
+    const poly = polygon([[[11, 0], [22, 4], [31, 0], [31, 11], [21, 15], [11, 11], [11, 0]]]);
+
+    t.assert(buffer(pt.geometry, 10));
+    t.assert(buffer(poly.geometry, 10));
+    t.end();
+});
+
+test('turf-buffer - Prevent Input Mutation', t => {
+    const pt = point([61, 5]);
+    const poly = polygon([[[11, 0], [22, 4], [31, 0], [31, 11], [21, 15], [11, 11], [11, 0]]]);
+    const collection = featureCollection([pt, poly]);
+
+    const beforePt = JSON.parse(JSON.stringify(pt));
+    const beforePoly = JSON.parse(JSON.stringify(poly));
+    const beforeCollection = JSON.parse(JSON.stringify(collection));
+
+    buffer(pt, 10);
+    buffer(poly, 10);
+    buffer(collection, 10);
+
+    t.deepEqual(pt, beforePt, 'pt should not mutate');
+    t.deepEqual(poly, beforePoly, 'poly should not mutate');
+    t.deepEqual(collection, beforeCollection, 'collection should not mutate');
     t.end();
 });
