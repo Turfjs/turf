d94a78ebf13d5721faf30cc00203071c9c6e4c98
Denis <carriere.denis@gmail.com>
2017-05-07 23:56:54 -0400

    Add test true/false fixtures boolean-contains

--- packages/turf-boolean-contains/bench.js
+++ packages/turf-boolean-contains/bench.js
@@ -1,31 +1,30 @@
+const path = require('path');
+const glob = require('glob');
 const Benchmark = require('benchmark');
-const {point, multiPoint, lineString, polygon} = require('@turf/helpers');
+const load = require('load-json-file');
+const bbox = require('@turf/bbox');
 const contains = require('./');
-
-// Fixtures
-const pt = point([1, 1]);
-const mp = multiPoint([[1, 1], [12, 12]]);
-const line = lineString([[1, 1], [1, 2], [1, 3], [1, 4]]);
-const poly = polygon([[[1, 1], [1, 10], [10, 10], [10, 1], [1, 1]]]);
 
 /**
  * Benchmark Results
  *
- * point - point x 25,229,201 ops/sec ±2.10% (90 runs sampled)
- * point - multipoint:
- * point - line:
- * point - polygon:
- * multipoint - point x 865,610 ops/sec ±1.71% (86 runs sampled)
- * polygon - line x 991,549 ops/sec ±1.71% (90 runs sampled)
+ * false-polygon-overlap x 110,681 ops/sec ±1.35% (89 runs sampled)
+ * false-polygons1 x 5,008,431 ops/sec ±5.25% (83 runs sampled)
+ * false-polygons2 x 4,958,640 ops/sec ±5.64% (82 runs sampled)
+ * true-polygons1 x 235,353 ops/sec ±1.27% (90 runs sampled)
+ * true-polygons2 x 170,512 ops/sec ±1.24% (91 runs sampled)
  */
 const suite = new Benchmark.Suite('turf-boolean-contains');
+glob.sync(path.join(__dirname, 'test', '**', '*.geojson')).forEach(filepath => {
+    const {name} = path.parse(filepath);
+    const geojson = load.sync(filepath);
+    const [feature1, feature2] = geojson.features;
+    feature1.bbox = bbox(feature1);
+    feature2.bbox = bbox(feature2);
+    suite.add(name, () => contains(feature1, feature2));
+});
+
 suite
-    .add('point - point', () => contains(pt, pt))
-    .add('point - multipoint', () => contains(pt, mp))
-    .add('point - line', () => contains(pt, line))
-    .add('point - polygon', () => contains(pt, polygon))
-    .add('multipoint - point', () => contains(mp, pt))
-    .add('polygon - line', () => contains(poly, line))
     .on('cycle', e => console.log(String(e.target)))
     .on('complete', () => {})
     .run();
--- packages/turf-boolean-contains/test.js
+++ packages/turf-boolean-contains/test.js
@@ -1,6 +1,27 @@
+const glob = require('glob');
+const path = require('path');
 const test = require('tape');
+const load = require('load-json-file');
 const {point, multiPoint, lineString, polygon} = require('@turf/helpers');
 const contains = require('./');
+
+test('turf-boolean-contains#fixtures', t => {
+    // True Fixtures
+    glob.sync(path.join(__dirname, 'test', 'true', '*.geojson')).forEach(filepath => {
+        const {name} = path.parse(filepath);
+        const geojson = load.sync(filepath);
+        const [feature1, feature2] = geojson.features;
+        t.true(contains(feature1, feature2), '[true] ' + name);
+    });
+    // False Fixtures
+    glob.sync(path.join(__dirname, 'test', 'false', '*.geojson')).forEach(filepath => {
+        const {name} = path.parse(filepath);
+        const geojson = load.sync(filepath);
+        const [feature1, feature2] = geojson.features;
+        t.false(contains(feature1, feature2), '[false] ' + name);
+    });
+    t.end();
+});
 
 test('turf-boolean-contains', t => {
     const pt1 = point([1, 1]);
