0211f3ce9627290838e2177ba13bd01491148891
stebogit <stefano.borghi@gmail.com>
2017-05-22 00:45:46 -0700

    updated bench.js

--- packages/turf-transform-translate/bench.js
+++ packages/turf-transform-translate/bench.js
@@ -2,63 +2,37 @@ const path = require('path');
 const Benchmark = require('benchmark');
 const load = require('load-json-file');
 const fs = require('fs');
-const matrixToGrid = require('matrix-to-grid');
-const isobands = require('./');
+const translate = require('./');
 
-// Define Fixtures
 const directory = path.join(__dirname, 'test', 'in') + path.sep;
 const fixtures = fs.readdirSync(directory).map(filename => {
     return {
-        filename,
         name: path.parse(filename).name,
-        jsondata: load.sync(directory + filename)
+        geojson: load.sync(directory + filename)
     };
 });
+
 
 /**
  * Benchmark Results
  *
- * bigMatrix x 73.43 ops/sec ±2.12% (62 runs sampled)
- * matrix1 x 5,205 ops/sec ±3.13% (78 runs sampled)
- * matrix2 x 2,333 ops/sec ±9.38% (71 runs sampled)
- * pointGrid x 3,201 ops/sec ±1.81% (78 runs sampled)
+ * line x 48,600 ops/sec ±2.90% (74 runs sampled)
+ * multiLine x 86,384 ops/sec ±2.33% (77 runs sampled)
+ * multiPoint x 113,514 ops/sec ±3.20% (74 runs sampled)
+ * multiPolygon x 6,712 ops/sec ±2.15% (77 runs sampled)
+ * no-motion x 24,466 ops/sec ±2.86% (78 runs sampled)
+ * point x 245,205 ops/sec ±2.00% (81 runs sampled)
+ * polygon-with-hole x 17,681 ops/sec ±2.07% (78 runs sampled)
+ * polygon x 117,984 ops/sec ±2.56% (76 runs sampled)
+ * z-translation x 83,989 ops/sec ±3.72% (77 runs sampled)
  */
-const suite = new Benchmark.Suite('turf-isobands');
-for (const {name, jsondata, filename} of fixtures) {
-
-    let breaks, points, zProperty, isobandProperties, commonProperties;
-    // allow geojson featureCollection...
-    if (filename.includes('geojson')) {
-        breaks = jsondata.properties.breaks;
-        zProperty = jsondata.properties.zProperty;
-        commonProperties = jsondata.properties.commonProperties;
-        isobandProperties = jsondata.properties.isobandProperties;
-        points = jsondata;
-    } else {
-        // ...or matrix input
-        const matrix = jsondata.matrix;
-        const cellSize = jsondata.cellSize;
-        const origin = jsondata.origin;
-        breaks = jsondata.breaks;
-        zProperty = jsondata.zProperty;
-        points = matrixToGrid(matrix, origin, cellSize, {zProperty, units: jsondata.units});
-        commonProperties = jsondata.commonProperties;
-        isobandProperties = jsondata.isobandProperties;
-    }
-
-    isobands(points, breaks, zProperty, {
-        commonProperties,
-        isobandProperties
-    });
-
-    // isobands(geojson, 'elevation', [5, 45, 55, 65, 85,  95, 105, 120, 180]);
-    suite.add(name, () => isobands(points, breaks, zProperty, {
-            commonProperties,
-            isobandProperties
-        })
-    );
+const suite = new Benchmark.Suite('turf-transform-translate');
+for (const {name, geojson} of fixtures) {
+    let {distance, direction, units, zTranslation} = geojson.properties || {};
+    suite.add(name, () => translate(geojson, distance, direction, units, zTranslation));
 }
+
 suite
-  .on('cycle', e => console.log(String(e.target)))
-  .on('complete', () => {})
-  .run();
+    .on('cycle', e => console.log(String(e.target)))
+    .on('complete', () => {})
+    .run();
