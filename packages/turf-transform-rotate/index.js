c2d2617d5a924e849175043bb29c6382bcbd7f88
stebogit <stefano.borghi@gmail.com>
2017-05-24 23:26:41 -0700

    refactored using coordEach

--- packages/turf-transform-rotate/index.js
+++ packages/turf-transform-rotate/index.js
@@ -1,18 +1,16 @@
 var helpers = require('@turf/helpers');
+var meta = require('@turf/meta');
 var centroid = require('@turf/centroid');
 var invariant = require('@turf/invariant');
 var rhumbBearing = require('@turf/rhumb-bearing');
 var rhumbDistance = require('@turf/rhumb-distance');
 var rhumbDestination = require('@turf/rhumb-destination');
+var clone = require('clone');
+var coordEach = meta.coordEach;
 var getCoord = invariant.getCoord;
 var getCoords = invariant.getCoords;
 var point = helpers.point;
 var feature = helpers.feature;
-var polygon = helpers.polygon;
-var multiPoint = helpers.multiPoint;
-var lineString = helpers.lineString;
-var multiPolygon = helpers.multiPolygon;
-var multiLineString = helpers.multiLineString;
 
 /**
  * Rotates any geojson Feature or Geometry of a specified angle, around its `centroid` or a given `pivot` point;
@@ -51,65 +49,18 @@ module.exports = function (geojson, angle, pivot, xRotation, yRotation) {
     if (pivot) pivot = point(getCoord(pivot));
     else pivot = centroid(geojson);
 
-    // copy properties to avoid reference issues
-    var properties = {};
-    Object.keys(geojson.properties).forEach(function (key) {
-        properties[key] = geojson.properties[key];
-    });
+    // clone geojson to avoid side effects
+    var newGeojson = clone(geojson);
 
-    var rotatedCoords;
-
-    var type = geojson.geometry.type;
-    switch (type) {
-    case 'Point':
-        if (!pivot) return geojson;
-        rotatedCoords = rotate([getCoords(geojson)], angle, pivot);
-        return point(rotatedCoords[0], properties);
-    case 'MultiPoint':
-        rotatedCoords = rotate(getCoords(geojson), angle, pivot);
-        return multiPoint(rotatedCoords, properties);
-    case 'LineString':
-        rotatedCoords = rotate(getCoords(geojson), angle, pivot);
-        return lineString(rotatedCoords, properties);
-    case 'MultiLineString':
-        rotatedCoords = getCoords(geojson).map(function (lineCoords) {
-            return rotate(lineCoords, angle, pivot);
-        });
-        return multiLineString(rotatedCoords, properties);
-    case 'Polygon':
-        rotatedCoords = getCoords(geojson).map(function (ringCoords) {
-            return rotate(ringCoords, angle, pivot);
-        });
-        return polygon(rotatedCoords, properties);
-    case 'MultiPolygon':
-        rotatedCoords = getCoords(geojson).map(function (polyCoords) {
-            return polyCoords.map(function (ringCoords) {
-                return rotate(ringCoords, angle, pivot);
-            });
-        });
-        return multiPolygon(rotatedCoords, properties);
-    case 'GeometryCollection':
-        throw new Error('GeometryCollection is not supported');
-    }
-};
-
-/**
- * Perform rotation to each coordinate of the input array using Rhumb modules
- *
- * @private
- * @param {Array<Array<number>>} featureCoords coordinates of the points of a feature
- * @param {number} rotationAngle of rotation on the plain
- * @param {Feature<Point>} pivot defining the origin of the rotation
- * @returns {Array<Array<number>>} rotated coordinates
- */
-function rotate(featureCoords, rotationAngle, pivot) {
-    return featureCoords.map(function (pointCoords) {
+    coordEach(newGeojson, function (pointCoords) {
         var initialAngle = rhumbBearing(pivot, pointCoords);
-        var finalAngle = initialAngle + rotationAngle;
+        var finalAngle = initialAngle + angle;
         var distance = rhumbDistance(pivot, pointCoords);
-
-        var rotatedCoords = getCoords(rhumbDestination(pivot, distance, finalAngle));
-        return rotatedCoords;
+        var newCoords = getCoords(rhumbDestination(pivot, distance, finalAngle));
+        pointCoords[0] = newCoords[0];
+        pointCoords[1] = newCoords[1];
     });
-}
+
+    return newGeojson;
+};
 
