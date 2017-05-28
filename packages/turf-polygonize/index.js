92ffec8dc3023aa3031d19fc073ecb513a01c649
Nicolas Cisco <ncis20@gmail.com>
2017-05-27 23:19:11 -0300

    Lint: Fixed index.js

--- packages/turf-polygonize/index.js
+++ packages/turf-polygonize/index.js
@@ -1,6 +1,6 @@
 const Graph = require('./src/Graph'),
-  EdgeRing = require('./src/EdgeRing'),
-  { featureCollection } = require('@turf/helpers');
+    EdgeRing = require('./src/EdgeRing'),
+    {featureCollection} = require('@turf/helpers');
 
 /** Implementation of GEOSPolygonize function (`geos::operation::polygonize::Polygonizer`).
  *
@@ -12,39 +12,39 @@ const Graph = require('./src/Graph'),
  *
  * - Dangles: edges which have one or both ends which are not incident on another edge endpoint.
  * - Cut Edges (bridges): edges that are connected at both ends but which do not form part
- *   of a polygon.
+ *     of a polygon.
  *
  * @param {FeatureCollection<LineString>} geoJson - Lines in order to polygonize
- * @returns {FeatureCollection<Polygon>}
+ * @returns {FeatureCollection<Polygon>} - Polygons created
  */
 module.exports = function polygonize(geoJson) {
-  const graph = Graph.fromGeoJson(geoJson);
+    const graph = Graph.fromGeoJson(geoJson);
 
-  // 1. Remove dangle node
-  graph.deleteDangles();
+    // 1. Remove dangle node
+    graph.deleteDangles();
 
-  // 2. Remove cut-edges (bridge edges)
-  graph.deleteCutEdges();
+    // 2. Remove cut-edges (bridge edges)
+    graph.deleteCutEdges();
 
-  // 3. Get all holes and shells
-  const holes = [],
-    shells = [];
+    // 3. Get all holes and shells
+    const holes = [],
+        shells = [];
 
-  graph.getEdgeRings()
-    .filter(edgeRing => edgeRing.isValid())
-    .forEach(edgeRing => {
-    if (edgeRing.isHole())
-      holes.push(edgeRing);
-    else
-      shells.push(edgeRing);
-  });
+    graph.getEdgeRings()
+        .filter(edgeRing => edgeRing.isValid())
+        .forEach(edgeRing => {
+            if (edgeRing.isHole())
+                holes.push(edgeRing);
+            else
+                shells.push(edgeRing);
+        });
 
-  // 4. Assign Holes to Shells
-  holes.forEach(hole => {
-    if (EdgeRing.findEdgeRingContaining(hole, shells))
-      shells.push(hole);
-  });
+    // 4. Assign Holes to Shells
+    holes.forEach(hole => {
+        if (EdgeRing.findEdgeRingContaining(hole, shells))
+            shells.push(hole);
+    });
 
-  // 5. EdgeRings to Polygons
-  return featureCollection(shells.map(shell => shell.toPolygon()));
+    // 5. EdgeRings to Polygons
+    return featureCollection(shells.map(shell => shell.toPolygon()));
 };
