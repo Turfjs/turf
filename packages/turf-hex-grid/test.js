04ed7accd38146d19a9d9416ceafeb7ac1c882fc
Denis <carriere.denis@gmail.com>
2017-05-27 23:32:15 -0400

    Center x & y Fraction using mid point

--- packages/turf-hex-grid/test.js
+++ packages/turf-hex-grid/test.js
@@ -14,17 +14,20 @@ const bbox1 = require(directories.in + 'bbox1.json');
 const bbox2 = require(directories.in + 'bbox2.json');
 const bbox3 = require(directories.in + 'bbox3.json');
 const bbox4 = require(directories.in + 'bbox4.json');
+const bbox5 = require(directories.in + 'bbox5.json');
 
 test('hex-grid', t => {
     const grid1 = truncate(grid(bbox1, 50, 'miles'));
     const grid2 = truncate(grid(bbox2, 5, 'miles'));
     const grid3 = truncate(grid(bbox3, 2, 'miles'));
     const grid4 = truncate(grid(bbox4, 50, 'kilometers'));
+    const grid5 = truncate(grid(bbox5, 500, 'kilometers'));
 
     t.ok(grid1.features.length, '50mi grid');
     t.ok(grid2.features.length, '5mi grid');
     t.ok(grid3.features.length, '2mi grid');
     t.ok(grid4.features.length, '50km grid');
+    t.ok(grid5.features.length, '500km grid');
 
     t.equal(grid(bbox1, 100, 'miles').features.length, 85);
 
@@ -33,11 +36,13 @@ test('hex-grid', t => {
         write.sync(directories.out + 'grid2.geojson', grid2);
         write.sync(directories.out + 'grid3.geojson', grid3);
         write.sync(directories.out + 'grid4.geojson', grid4);
+        write.sync(directories.out + 'grid5.geojson', grid5);
     }
     t.deepEqual(load.sync(directories.out + 'grid1.geojson'), grid1, 'grid is correct');
     t.deepEqual(load.sync(directories.out + 'grid2.geojson'), grid2, 'grid is correct');
     t.deepEqual(load.sync(directories.out + 'grid3.geojson'), grid3, 'grid is correct');
     t.deepEqual(load.sync(directories.out + 'grid4.geojson'), grid4, 'grid is correct');
+    t.deepEqual(load.sync(directories.out + 'grid5.geojson'), grid5, 'grid is correct');
 
     t.end();
 });
@@ -47,11 +52,13 @@ test('hex-tri-grid', t => {
     const grid2 = truncate(grid(bbox2, 5, 'miles', true));
     const grid3 = truncate(grid(bbox3, 2, 'miles', true));
     const grid4 = truncate(grid(bbox4, 50, 'kilometers', true));
+    const grid5 = truncate(grid(bbox5, 500, 'kilometers', true));
 
     t.ok(grid1.features.length, '50mi grid');
     t.ok(grid2.features.length, '5mi grid');
     t.ok(grid3.features.length, '2mi grid');
     t.ok(grid4.features.length, '50km grid');
+    t.ok(grid5.features.length, '500km grid');
 
     t.equal(grid(bbox1, 100, 'miles').features.length, 85);
 
@@ -60,20 +67,20 @@ test('hex-tri-grid', t => {
         write.sync(directories.out + 'trigrid2.geojson', grid2);
         write.sync(directories.out + 'trigrid3.geojson', grid3);
         write.sync(directories.out + 'trigrid4.geojson', grid4);
+        write.sync(directories.out + 'trigrid5.geojson', grid5);
     }
     t.deepEqual(load.sync(directories.out + 'trigrid1.geojson'), grid1, 'grid is correct');
     t.deepEqual(load.sync(directories.out + 'trigrid2.geojson'), grid2, 'grid is correct');
     t.deepEqual(load.sync(directories.out + 'trigrid3.geojson'), grid3, 'grid is correct');
     t.deepEqual(load.sync(directories.out + 'trigrid4.geojson'), grid4, 'grid is correct');
+    t.deepEqual(load.sync(directories.out + 'trigrid5.geojson'), grid5, 'grid is correct');
 
     t.end();
 });
 
 test('longitude (13141439571036224) issue #758', t => {
     const bbox = [-179, -90, 179, 90];
-    const cellSize = 1;
-    const units = 'kilometers';
-    const hexgrid = grid(bbox, cellSize, units);
+    const hexgrid = grid(bbox, 500, 'kilometers');
 
     const coords = [];
     hexgrid.features.forEach(feature => feature.geometry.coordinates[0].forEach(coord => coords.push(coord)));
