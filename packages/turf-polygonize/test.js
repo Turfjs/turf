35c16eb0d92216470e151eb88e0b14006d131570
Nicolas Cisco <ncis20@gmail.com>
2017-05-28 19:32:05 -0300

    Fixing tests

--- packages/turf-polygonize/test.js
+++ packages/turf-polygonize/test.js
@@ -2,50 +2,29 @@ const test = require('tape'),
   { featureCollection, lineString, polygon } = require('@turf/helpers'),
   polygonize = require('./'),
   fs = require('fs'),
-  path = require('path');
+  path = require('path'),
+  load = require('load-json-file'),
+  write = require('write-json-file');
 
 const directories = {
   in: path.join(__dirname, 'test', 'in') + path.sep,
   out: path.join(__dirname, 'test', 'out') + path.sep,
 };
 
-function getFullPath(filename, type='in') {
-  return path.join(directories[type], filename);
-}
+const fixtures = fs.readdirSync(directories.in).map(filename => {
+  return {
+    filename,
+    name: path.parse(filename).name,
+    geojson: load.sync(directories.in + filename)
+  };
+});
 
-function readJsonFile(filename, type='in') {
-  try {
-    return JSON.parse(fs.readFileSync(getFullPath(filename, type)));
-  } catch(e) {
-    if (e.code !== 'ENOENT')
-      throw e;
-    return undefined;
+test('turf-polygonize', t => {
+  for (const {filename, name, geojson}  of fixtures) {
+    const results = polygonize(geojson);
+
+    if (process.env.REGEN) write.sync(directories.out + filename, results);
+    t.deepEquals(results, load.sync(directories.out + filename), name);
   }
-}
-
-function writeJsonFile(filename, data, type='out') {
-  fs.writeFileSync(getFullPath(filename, type), JSON.stringify(data));
-}
-
-fs.readdirSync(directories.in)
-  .filter(filename => !filename.startsWith('.'))
-  .map(filename => {
-    return {
-      filename,
-      name: path.parse(filename).name,
-      input: readJsonFile(filename, 'in'),
-      output: readJsonFile(filename, 'out'),
-    }
-  })
-  .forEach(({filename, name, input, output}) => {
-    test(`turf-polygonize :: ${name}`, t => {
-      const result = polygonize(input);
-      if (output) {
-        t.deepEqual(result, output);
-      } else {
-        t.skip(`${name} not found :: writing`);
-        writeJsonFile(filename, result);
-      }
-      t.end();
-    });
-  });
+  t.end();
+});
