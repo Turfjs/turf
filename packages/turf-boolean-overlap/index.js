78a0af82734995bce9d721927f481bd04c540365
Rowan Winsemius <rowanwins@yahoo.com.au>
2017-05-30 23:02:31 +1000

    boolean-overlap improvements

--- packages/turf-boolean-overlap/index.js
+++ packages/turf-boolean-overlap/index.js
@@ -1,18 +1,20 @@
 var inside = require('@turf/inside');
 var helpers = require('@turf/helpers');
 var lineOverlap = require('@turf/line-overlap');
+var polyToLinestring = require('@turf/polygon-to-linestring');
+var lineIntersect = require('@turf/line-intersect');
 var deepEqual = require('deep-equal');
 
 /**
  * Overlap compares two geometries of the same dimension and returns (TRUE) if their
- * intersectionset resultsin a geometry different from both but of the same dimension.
+ * intersection set results in a geometry different from both but of the same dimension.
  *
  * @name overlap
  * @param {feature1} feature1 GeoJSON Feature
  * @param {feature2} feature2 GeoJSON Feature
  * @returns {Boolean} Features overlap
  * @example
- * turf.overlap(feature1, feature2);
+ * turf.booleanOverlap(feature1, feature2);
  * //=true/false
  */
 module.exports = function (feature1, feature2) {
@@ -27,10 +29,13 @@ module.exports = function (feature1, feature2) {
     }
 };
 
-function doMultiPointsOverlap(MultiPoint1, MultiPoint2) {
-    for (var i = 0; i < MultiPoint2.geometry.coordinates.length; i++) {
-        for (var i2 = 0; i2 < MultiPoint1.geometry.coordinates.length; i2++) {
-            if (deepEqual(MultiPoint2.geometry.coordinates[i], MultiPoint1.geometry.coordinates[i2])) {
+function doMultiPointsOverlap(multipoint1, multipoint2) {
+    if (deepEqual(multipoint1, multipoint2)) {
+        return false;
+    }
+    for (var i = 0; i < multipoint2.geometry.coordinates.length; i++) {
+        for (var i2 = 0; i2 < multipoint1.geometry.coordinates.length; i2++) {
+            if (deepEqual(multipoint2.geometry.coordinates[i], multipoint1.geometry.coordinates[i2])) {
                 return true;
             }
         }
@@ -38,27 +43,66 @@ function doMultiPointsOverlap(MultiPoint1, MultiPoint2) {
     return false;
 }
 
-// TO DO - Work out how to check if line is in line (can potentially use line overlap module)
-// Also need to make sure lines are exactly the same, eg the second must be smaller than the first
-function isLineOnLine(LineString1, LineString2) {
-    var overlappingLine = lineOverlap(LineString1, LineString2);
+function isLineOnLine(lineString1, lineString2) {
+    if (deepEqual(lineString1, lineString2)) {
+        return false;
+    }
+    var overlappingLine = lineOverlap(lineString1, lineString2);
     if (overlappingLine.features.length !== 0) {
+        for (var i = 0; i < lineString1.geometry.coordinates.length; i++) {
+            if (!ispointOnLine(lineString2.geometry, lineString1.geometry.coordinates[i])) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function ispointInPoly(Polygon, point) {
+    return inside(point, Polygon);
+}
+
+function isPolyInPoly(polygon1, polygon2) {
+    for (var i = 0; i < polygon2.geometry.coordinates[0].length; i++) {
+        if (ispointInPoly(polygon1, helpers.point(polygon2.geometry.coordinates[0][i]), true)) {
+            return true;
+        }
+    }
+    if (doLineStringAndPolygonCross(polygon1, polygon2)) {
         return true;
     }
     return false;
 }
 
-function isPointInPoly(Polygon, Point) {
-    return inside(Point, Polygon);
+function doLineStringAndPolygonCross(polygon1, polygon2) {
+    var doLinesIntersect = lineIntersect(polyToLinestring(polygon1), polyToLinestring(polygon2));
+    if (doLinesIntersect.features.length > 0) {
+        return true;
+    }
+    return false;
 }
 
-// TO DO HANDLE IF THERE ARE NO POINTS INSIDE THE OTHER POLY BY LINES OVERLAP
-function isPolyInPoly(Polygon1, Polygon2) {
-    for (var i = 0; i < Polygon2.geometry.coordinates[0].length; i++) {
-        if (isPointInPoly(Polygon1, helpers.point(Polygon2.geometry.coordinates[0][i]), true)) {
+function ispointOnLine(lineString, pointCoords) {
+    for (var i = 0; i < lineString.coordinates.length - 1; i++) {
+        if (ispointOnLineSegment(lineString.coordinates[i], lineString.coordinates[i + 1], pointCoords)) {
             return true;
         }
     }
+    return false;
+}
 
-    return true;
+function ispointOnLineSegment(LineSegmentStart, LineSegmentEnd, point) {
+    var dxc = point[0] - LineSegmentStart[0];
+    var dyc = point[1] - LineSegmentStart[1];
+    var dxl = LineSegmentEnd[0] - LineSegmentStart[0];
+    var dyl = LineSegmentEnd[1] - LineSegmentStart[1];
+    var cross = dxc * dyl - dyc * dxl;
+    if (cross !== 0) {
+        return false;
+    }
+    if (Math.abs(dxl) >= Math.abs(dyl)) {
+        return dxl > 0 ? LineSegmentStart[0] <= point[0] && point[0] <= LineSegmentEnd[0] : LineSegmentEnd[0] <= point[0] && point[0] <= LineSegmentStart[0];
+    } else {
+        return dyl > 0 ? LineSegmentStart[1] <= point[1] && point[1] <= LineSegmentEnd[1] : LineSegmentEnd[1] <= point[1] && point[1] <= LineSegmentStart[1];
+    }
 }
--- packages/turf-boolean-overlap/test.js
+++ packages/turf-boolean-overlap/test.js
@@ -1,8 +1,35 @@
 const test = require('tape');
-const {point, lineString, polygon, multiPoint, multiLineString, multiPolygon} = require('@turf/helpers');
-const equal = require('./');
+const {lineString, polygon, multiPoint} = require('@turf/helpers');
+const overlap = require('./');
 
 test('turf-boolean-overlap', t => {
-    t.skip('boolean-overlap');
+    const mp1 = multiPoint([[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]);
+    const mp2 = multiPoint([[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]);
+    const mp3 = multiPoint([[10, 0], [10, 10], [10, 10], [10, 0], [0, 0]]);
+    const mp4 = multiPoint([[100, 100], [20, 20]]);
+
+    t.equal(overlap(mp1, mp2), false, 'MultiPoints are equal so overlap is false');
+    t.equal(overlap(mp1, mp3), true, 'MultiPoints are not equal so overlap is true');
+    t.equal(overlap(mp1, mp4), false, 'MultiPoints are have no overlaps so overlap is false');
+
+    const line1 = lineString([[1, 1], [1, 2], [1, 3], [1, 4]]);
+    const line2 = lineString([[1, 1], [1, 2], [1, 3], [1, 4]]);
+    const line3 = lineString([[2, 1], [1, 2], [1, 3], [2, 4]]);
+    const line4 = lineString([[1, 2], [1, 3]]);
+
+    t.equal(overlap(line1, line2), false, 'Lines are exactly equal so overlap is false');
+    t.equal(overlap(line1, line3), true, 'Lines overlaps partially so overlap is true');
+    t.equal(overlap(line4, line1), false, 'Lines is contained within other line so overlap is false');
+
+    const poly1 = polygon([[[-1, 2], [3, 2], [3, 3], [-1, 3], [-1, 2]]]);
+    const poly2 = polygon([[[0, 1], [3, 2], [3, 3], [-1, 3], [0, 1]]]);
+    const poly3 = polygon([[[-10, 20], [30, 20], [30, 30], [-10, 30], [-10, 20]]]);
+    const poly4 = polygon([[[1, 0], [4, 0], [4, 4], [1, 4], [1, 0]]]);
+
+
+    t.equal(overlap(poly1, poly2), true, 'Polys have points inside so overlap is true');
+    t.equal(overlap(poly1, poly3), false, 'Polys do not overlap so overlap is false');
+    t.equal(overlap(poly1, poly4), true, 'Polys overlap via lines so overlap is true');
+
     t.end();
 });
