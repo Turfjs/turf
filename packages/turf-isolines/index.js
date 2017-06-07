487784201913f2de9a4eaa7b3f8d2f92e266068d
stebogit <stefano.borghi@gmail.com>
2017-06-07 01:07:26 -0700

    replaced conrec with marchingsquare

--- packages/turf-isolines/index.js
+++ packages/turf-isolines/index.js
@@ -1,17 +1,19 @@
-//https://github.com/jasondavies/conrec.js
-//http://stackoverflow.com/questions/263305/drawing-a-topographical-map
-var tin = require('@turf/tin');
+// var tin = require('@turf/tin');
 var bbox = require('@turf/bbox');
-var grid = require('@turf/point-grid');
-var inside = require('@turf/inside');
+// var grid = require('@turf/point-grid');
+// var inside = require('@turf/inside');
+// var square = require('@turf/square');
+var invariant = require('@turf/invariant');
 var square = require('@turf/square');
 var helpers = require('@turf/helpers');
-var distance = require('@turf/distance');
-var planepoint = require('@turf/planepoint');
-var featurecollection = helpers.featureCollection;
-var linestring = helpers.lineString;
-var point = helpers.point;
-var Conrec = require('./conrec');
+// var distance = require('@turf/distance');
+// var planepoint = require('@turf/planepoint');
+var gridToMatrix = require('grid-to-matrix');
+var marchingsquares = require('marchingsquares');
+// var point = helpers.point;
+var multiLineString = helpers.multiLineString;
+var collectionOf = invariant.collectionOf;
+var featureCollection = helpers.featureCollection;
 
 /**
  * Takes {@link Point|points} with z-values and an array of
@@ -19,10 +21,13 @@ var Conrec = require('./conrec');
  *
  * @name isolines
  * @param {FeatureCollection<Point>} points input points
- * @param {string} z the property name in `points` from which z-values will be pulled
- * @param {number} resolution resolution of the underlying grid
- * @param {Array<number>} breaks where to draw contours
- * @returns {FeatureCollection<LineString>} isolines
+ * @param {Array<number>} breaks where to draw isolines
+ * @param {string} [zProperty='elevation'] the property name in `points` from which z-values will be pulled
+ * @param {Object} [options={}] options on output
+ * @param {Array<Object>} [options.isolineProperties=[]] GeoJSON properties passed, in order, to the correspondent
+ * isoline (order defined by breaks)
+ * @param {Object} [options.commonProperties={}] GeoJSON properties passed to ALL isolines
+ * @returns {FeatureCollection<MultiLineString>} a FeatureCollection of {@link MultiLineString} features representing isolines
  * @example
  * // create random points with random
  * // z-values in their properties
@@ -34,67 +39,120 @@ var Conrec = require('./conrec');
  * }
  * var breaks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
  * var isolined = turf.isolines(points, 'z', 15, breaks);
- * //=isolined
+ *
+ * //addToMap
+ * var addToMap = [isolined];
  */
-module.exports = function (points, z, resolution, breaks) {
-    var tinResult = tin(points, z);
-    var bboxBBox = bbox(points);
-    var squareBBox = square(bboxBBox);
-    var sizeCellGrid = distance(point([squareBBox[0], squareBBox[1]]), point([squareBBox[2], squareBBox[1]]), 'kilometers') / resolution;
-    var gridResult = grid(squareBBox, sizeCellGrid, 'kilometers');
-    var data = [];
-
-    for (var i = 0; i < gridResult.features.length; i++) {
-        var pt = gridResult.features[i];
-        for (var j = 0; j < tinResult.features.length; j++) {
-            var triangle = tinResult.features[j];
-            if (inside(pt, triangle)) {
-                pt.properties = {};
-                pt.properties[z] = planepoint(pt, triangle);
-            }
-        }
+module.exports = function (points, breaks, zProperty, options) {
+    // Input validation
+    var isObject = function (input) {
+        return (!!input) && (input.constructor === Object);
+    };
+    collectionOf(points, 'Point', 'Input must contain Points');
+    if (!breaks || !Array.isArray(breaks)) throw new Error('breaks is required');
+    if (options.commonProperties && !isObject(options.commonProperties)) {
+        throw new Error('commonProperties is not an Object');
+    }
+    if (options.isolineProperties && !Array.isArray(options.isolineProperties)) {
+        throw new Error('isolineProperties is not an Array');
+    }
+    if (zProperty && typeof zProperty !== 'string') {
+        throw new Error('zProperty is not a string');
     }
 
-    var depth = Math.sqrt(gridResult.features.length);
-    for (var x = 0; x < depth; x++) {
-        var xGroup = gridResult.features.slice(x * depth, (x + 1) * depth);
-        var xFlat = [];
+    zProperty = zProperty || 'elevation';
+    options = options || {};
+    var commonProperties = options.commonProperties || {};
+    var isolineProperties = options.isolineProperties || [];
 
-        for (var g = 0; g < xGroup.length; g++) {
-            if (xGroup[g].properties) {
-                xFlat.push(xGroup[g].properties[z]);
-            } else {
-                xFlat.push(0);
-            }
+    // Isolined methods
+    var matrix = gridToMatrix(points, zProperty, true);
+    var isolines = createIsoLines(matrix, breaks, zProperty);
+    var scaledIsolines = rescaleIsolines(isolines, matrix, points);
+
+    var multilines = scaledIsolines.map(function (isolines, index) {
+        if (isolineProperties[index] && !isObject(isolineProperties[index])) {
+            throw new Error('Each mappedProperty is required to be an Object');
         }
-        data.push(xFlat);
-    }
-    var interval = (squareBBox[2] - squareBBox[0]) / depth;
-    var xCoordinates = [];
-    var yCoordinates = [];
-    for (var d = 0; d < depth; d++) {
-        xCoordinates.push(d * interval + squareBBox[0]);
-        yCoordinates.push(d * interval + squareBBox[1]);
-    }
-
-    var c = new Conrec();
-    c.contour(data, 0, resolution, 0, resolution, xCoordinates, yCoordinates, breaks.length, breaks);
-    var contourList = c.contourList();
-
-    var fc = featurecollection([]);
-    contourList.forEach(function (c) {
-        if (c.length > 2) {
-            var polyCoordinates = [];
-            c.forEach(function (coord) {
-                polyCoordinates.push([coord.x, coord.y]);
-            });
-            var poly = linestring(polyCoordinates);
-            poly.properties = {};
-            poly.properties[z] = c.level;
-
-            fc.features.push(poly);
-        }
+        // collect all properties
+        var contourProperties = Object.assign(
+            {},
+            commonProperties,
+            isolineProperties[index]
+        );
+        contourProperties[zProperty] = isolines[zProperty];
+        var multiL = multiLineString(isolines.lines, contourProperties);
+        return multiL;
     });
 
-    return fc;
+    return featureCollection(multilines);
 };
+
+/**
+ * Creates the isolines lines (featuresCollection of polygon features) from the 2D data grid
+ *
+ * Marchingsquares process the grid data as a 3D representation of a function on a 2D plane, therefore it
+ * assumes the points (x-y coordinates) are one 'unit' distance. The result of the isolines function needs to be
+ * rescaled, with turfjs, to the original area and proportions on the map
+ *
+ * @private
+ * @param {Array<Array<number>>} matrix Grid Data
+ * @param {Array<number>} breaks Breaks
+ * @param {string} [property='elevation'] Property
+ * @returns {Array<any>} isolines
+ */
+function createIsoLines(matrix, breaks, property) {
+    var isolines = [];
+    for (var i = 1; i < breaks.length; i++) {
+        var threshold = +breaks[i]; // make sure it's a number
+
+        var isolinesCoords = marchingsquares.isoContours(matrix, threshold);
+        var obj = {};
+        obj['lines'] = isolinesCoords;
+        obj[property] = threshold;
+        isolines.push(obj);
+    }
+    return isolines;
+}
+
+/**
+ * Transform isolines of 2D grid to polygons for the map
+ *
+ * @private
+ * @param {Array<any>} isolines Contours
+ * @param {Array<Array<number>>} matrix Grid Data
+ * @param {Object} points Points by Latitude
+ * @returns {Array<any>} isolines
+ */
+function rescaleIsolines(isolines, matrix, points) {
+
+    // get dimensions (on the map) of the original grid
+    var gridBbox = bbox(points); // [ minX, minY, maxX, maxY ]
+    var originalWidth = gridBbox[2] - gridBbox[0];
+    var originalHeigth = gridBbox[3] - gridBbox[1];
+
+    // get origin, which is the first point of the last row on the rectangular data on the map
+    var x0 = gridBbox[0];
+    var y0 = gridBbox[1];
+    // get number of cells per side
+    var matrixWidth = matrix[0].length - 1;
+    var matrixHeight = matrix.length - 1;
+    // calculate the scaling factor between matrix and rectangular grid on the map
+    var scaleX = originalWidth / matrixWidth;
+    var scaleY = originalHeigth / matrixHeight;
+
+    var resize = function (point) {
+        point[0] = point[0] * scaleX + x0;
+        point[1] = point[1] * scaleY + y0;
+    };
+
+    // resize and shift each point/line of the isolines
+    isolines.forEach(function (contour) {
+        contour.lines.forEach(function (lineRingSet) {
+            lineRingSet.forEach(function (lineRing) {
+                lineRing.forEach(resize);
+            });
+        });
+    });
+    return isolines;
+}
--- packages/turf-isolines/test.js
+++ packages/turf-isolines/test.js
@@ -1,16 +1,76 @@
-var test = require('tape');
-var fs = require('fs');
-var path = require('path');
-var isolines = require('./');
+const test = require('tape');
+const path = require('path');
+const fs = require('fs');
+const load = require('load-json-file');
+const write = require('write-json-file');
+const random = require('@turf/random');
+const envelope = require('@turf/envelope');
+const helpers = require('@turf/helpers');
+const lineString = helpers.lineString;
+const getCoords = require('@turf/invariant').getCoords;
+const matrixToGrid = require('matrix-to-grid');
+const pointGrid = require('@turf/point-grid');
+const isolines = require('./');
 
-test('isolines', function (t) {
-  var points = JSON.parse(fs.readFileSync(path.join(__dirname, 'test/Points.geojson')));
+const directories = {
+    in: path.join(__dirname, 'test', 'in') + path.sep,
+    out: path.join(__dirname, 'test', 'out') + path.sep
+};
 
-  var isolined = isolines(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180]);
+const fixtures = fs.readdirSync(directories.in).map(filename => {
+    return {
+        filename,
+        name: path.parse(filename).name,
+        jsondata: load.sync(directories.in + filename)
+    };
+});
 
-  t.ok(isolined.features, 'should take a set of points with z values and output a set of contour lines');
-  t.equal(isolined.features[0].geometry.type, 'LineString');
+test('isolines', t => {
+    fixtures.forEach(({name, jsondata, filename}) => {
 
-  fs.writeFileSync(path.join(__dirname, 'test/isolines.geojson'), JSON.stringify(isolined));
-  t.end();
+        let breaks, points, zProperty, isobandProperties, commonProperties;
+        // allow geojson featureCollection...
+        if (filename.includes('geojson')) {
+            breaks = jsondata.properties.breaks;
+            zProperty = jsondata.properties.zProperty;
+            commonProperties = jsondata.properties.commonProperties;
+            isobandProperties = jsondata.properties.isobandProperties;
+            points = jsondata;
+        } else {
+            // ...or matrix input
+            const matrix = jsondata.matrix;
+            const cellSize = jsondata.cellSize;
+            const origin = jsondata.origin;
+            breaks = jsondata.breaks;
+            zProperty = jsondata.zProperty;
+            points = matrixToGrid(matrix, origin, cellSize, { zProperty, units: jsondata.units });
+            commonProperties = jsondata.commonProperties;
+            isobandProperties = jsondata.isobandProperties;
+        }
+
+        const results = isolines(points, breaks, zProperty, {
+            commonProperties,
+            isobandProperties
+        });
+
+        const box = lineString(getCoords(envelope(points))[0]);
+        box.properties['stroke'] = '#F00';
+        box.properties['stroke-width'] = 1;
+        results.features.push(box);
+
+        if (process.env.REGEN) write.sync(directories.out + filename, results);
+        t.deepEqual(results, load.sync(directories.out + filename), name);
+    });
+
+    t.end();
+});
+
+test('isolines -- throws', t => {
+    const points = pointGrid([-70.823364, -33.553984, -70.473175, -33.302986], 5);
+
+    t.throws(() => isolines(random('polygon'), [1, 2, 3]), 'invalid points');
+    t.throws(() => isolines(points, ''), 'invalid breaks');
+    t.throws(() => isolines(points, [1, 2, 3], 'temp', { isobandProperties: 'hello' }), 'invalid options');
+
+    t.end();
 });
