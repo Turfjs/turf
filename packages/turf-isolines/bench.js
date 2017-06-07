6456b87a6e33fcd78c5a04c486a1e2a7ba8720a8
stebogit <stefano.borghi@gmail.com>
2017-06-07 08:51:56 -0700

    updated bench

--- packages/turf-isolines/bench.js
+++ packages/turf-isolines/bench.js
@@ -1,18 +1,65 @@
-var isolines = require('./');
-var Benchmark = require('benchmark');
-var fs = require('fs');
+const path = require('path');
+const Benchmark = require('benchmark');
+const load = require('load-json-file');
+const fs = require('fs');
+const matrixToGrid = require('matrix-to-grid');
+const isolines = require('./');
 
-var points = JSON.parse(fs.readFileSync(__dirname+'/test/Points.geojson'));
+// Define Fixtures
+const directory = path.join(__dirname, 'test', 'in') + path.sep;
+const fixtures = fs.readdirSync(directory).map(filename => {
+    return {
+        filename,
+        name: path.parse(filename).name,
+        jsondata: load.sync(directory + filename)
+    };
+});
 
-var suite = new Benchmark.Suite('turf-isolines');
+/**
+ * Benchmark Results
+ *
+ * bigMatrix x 168 ops/sec ±2.79% (72 runs sampled)
+ * matrix1 x 17,145 ops/sec ±4.73% (68 runs sampled)
+ * matrix2 x 11,004 ops/sec ±2.56% (79 runs sampled)
+ * pointGrid x 12,109 ops/sec ±2.01% (77 runs sampled)
+ */
+const suite = new Benchmark.Suite('turf-isolines');
+for (const {name, jsondata, filename} of fixtures) {
+
+    let breaks, points, zProperty, isolineProperties, commonProperties;
+    // allow geojson featureCollection...
+    if (filename.includes('geojson')) {
+        breaks = jsondata.properties.breaks;
+        zProperty = jsondata.properties.zProperty;
+        commonProperties = jsondata.properties.commonProperties;
+        isolineProperties = jsondata.properties.isolineProperties;
+        points = jsondata;
+    } else {
+        // ...or matrix input
+        const matrix = jsondata.matrix;
+        const cellSize = jsondata.cellSize;
+        const origin = jsondata.origin;
+        breaks = jsondata.breaks;
+        zProperty = jsondata.zProperty;
+        points = matrixToGrid(matrix, origin, cellSize, {zProperty, units: jsondata.units});
+        commonProperties = jsondata.commonProperties;
+        isolineProperties = jsondata.isolineProperties;
+    }
+
+    isolines(points, breaks, zProperty, {
+        commonProperties,
+        isolineProperties
+    });
+
+    // isolines(geojson, 'elevation', [5, 45, 55, 65, 85,  95, 105, 120, 180]);
+    suite.add(name, () => isolines(points, breaks, zProperty, {
+            commonProperties,
+            isolineProperties
+        })
+    );
+}
 suite
-  .add('turf-isolines',function () {
-    isolines(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180]);
-  })
-  .on('cycle', function (event) {
-    console.log(String(event.target));
-  })
-  .on('complete', function () {
-    
-  })
+  .on('cycle', e => console.log(String(e.target)))
+  .on('complete', () => {})
   .run();
+
