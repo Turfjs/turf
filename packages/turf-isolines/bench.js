321d0df5c77a4005e4a6f28947fe11d463d53bab
Stefano Borghi <stebogit@users.noreply.github.com>
2017-06-11 10:25:11 -0700

    New @turf/isolines based on MarchingSquares.js (#781)
    * replaced conrec with marchingsquare
    
    * fixed script and tests
    
    * updated bench
    
    * updated readme
    
    * updated package.json
    
    * updated index.d.ts
    
    * introduced suggested changes
    
    * added zProperty to index.d.ts
    
    * minor doc change
    
    * Updates to Isolines
    - Divide properties={} to two params
    - Simplify Catch error logic (index.js)
    - Update Typescript definition
    - Add types.ts to test Typescript definition
    - Add single task to benchmark
    - Update Yarn.lock
    - Simplify bench & test logic
    - Save fixtures out to GeoJSON
    CC: @stebogit
    
    * perIsoline overlaps properties from toAllIsolines
    
    * Update Readme

--- packages/turf-isolines/bench.js
+++ packages/turf-isolines/bench.js
@@ -1,18 +1,56 @@
-var isolines = require('./');
-var Benchmark = require('benchmark');
-var fs = require('fs');
+const fs = require('fs');
+const path = require('path');
+const load = require('load-json-file');
+const Benchmark = require('benchmark');
+const matrixToGrid = require('matrix-to-grid');
+const isolines = require('./');
 
-var points = JSON.parse(fs.readFileSync(__dirname+'/test/Points.geojson'));
+// Define Fixtures
+const directory = path.join(__dirname, 'test', 'in') + path.sep;
+const fixtures = fs.readdirSync(directory).map(filename => {
+    return {
+        filename,
+        name: path.parse(filename).name,
+        jsondata: load.sync(directory + filename)
+    };
+});
 
-var suite = new Benchmark.Suite('turf-isolines');
+/**
+ * Benchmark Results
+ *
+ * bigMatrix: 21.457ms
+ * matrix1: 0.741ms
+ * matrix2: 0.547ms
+ * pointGrid: 1.373ms
+ * bigMatrix x 168 ops/sec ±2.79% (72 runs sampled)
+ * matrix1 x 17,145 ops/sec ±4.73% (68 runs sampled)
+ * matrix2 x 11,004 ops/sec ±2.56% (79 runs sampled)
+ * pointGrid x 12,109 ops/sec ±2.01% (77 runs sampled)
+ */
+const suite = new Benchmark.Suite('turf-isolines');
+for (const {name, jsondata, filename} of fixtures) {
+    const {
+        breaks,
+        zProperty,
+        propertiesPerIsoline,
+        propertiesToAllIsolines,
+        matrix,
+        cellSize,
+        units,
+        origin} = jsondata.properties || jsondata;
+
+    // allow GeoJSON FeatureCollection or Matrix
+    let points;
+    if (filename.includes('geojson')) points = jsondata;
+    else points = matrixToGrid(matrix, origin, cellSize, {zProperty, units});
+
+    console.time(name);
+    isolines(points, breaks, zProperty, propertiesToAllIsolines, propertiesPerIsoline);
+    console.timeEnd(name);
+    suite.add(name, () => isolines(points, breaks, zProperty, propertiesToAllIsolines, propertiesPerIsoline));
+}
 suite
-  .add('turf-isolines',function () {
-    isolines(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180]);
-  })
-  .on('cycle', function (event) {
-    console.log(String(event.target));
-  })
-  .on('complete', function () {
-    
-  })
+  .on('cycle', e => console.log(String(e.target)))
+  .on('complete', () => {})
   .run();
+
--- packages/turf-isolines/test.js
+++ packages/turf-isolines/test.js
@@ -1,16 +1,80 @@
-var test = require('tape');
-var fs = require('fs');
-var path = require('path');
-var isolines = require('./');
+const fs = require('fs');
+const test = require('tape');
+const path = require('path');
+const load = require('load-json-file');
+const write = require('write-json-file');
+const random = require('@turf/random');
+const envelope = require('@turf/envelope');
+const pointGrid = require('@turf/point-grid');
+const {getCoords} = require('@turf/invariant');
+const matrixToGrid = require('matrix-to-grid');
+const {lineString} = require('@turf/helpers');
+const isolines = require('./');
 
-test('isolines', function (t) {
-  var points = JSON.parse(fs.readFileSync(path.join(__dirname, 'test/Points.geojson')));
+const directories = {
+    in: path.join(__dirname, 'test', 'in') + path.sep,
+    out: path.join(__dirname, 'test', 'out') + path.sep
+};
 
-  var isolined = isolines(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180]);
+const fixtures = fs.readdirSync(directories.in).map(filename => {
+    return {
+        filename,
+        name: path.parse(filename).name,
+        jsondata: load.sync(directories.in + filename)
+    };
+});
 
-  t.ok(isolined.features, 'should take a set of points with z values and output a set of contour lines');
-  t.equal(isolined.features[0].geometry.type, 'LineString');
+test('isolines', t => {
+    fixtures.forEach(({name, jsondata, filename}) => {
+        const {
+            breaks,
+            zProperty,
+            propertiesPerIsoline,
+            propertiesToAllIsolines,
+            matrix,
+            cellSize,
+            units,
+            origin} = jsondata.properties || jsondata;
 
-  fs.writeFileSync(path.join(__dirname, 'test/isolines.geojson'), JSON.stringify(isolined));
-  t.end();
+        // allow GeoJSON FeatureCollection or Matrix
+        let points;
+        if (filename.includes('geojson')) points = jsondata;
+        else points = matrixToGrid(matrix, origin, cellSize, {zProperty, units});
+
+        const results = isolines(points, breaks, zProperty, propertiesToAllIsolines, propertiesPerIsoline);
+
+        const box = lineString(getCoords(envelope(points))[0]);
+        box.properties['stroke'] = '#F00';
+        box.properties['stroke-width'] = 1;
+        results.features.push(box);
+
+        if (process.env.REGEN) write.sync(directories.out + name + '.geojson', results);
+        t.deepEqual(results, load.sync(directories.out + name + '.geojson'), name);
+    });
+
+    t.end();
+});
+
+test('isolines -- throws', t => {
+    const points = pointGrid([-70.823364, -33.553984, -70.473175, -33.302986], 5);
+
+    t.throws(() => isolines(random('polygon'), [1, 2, 3]), 'invalid points');
+    t.throws(() => isolines(points), /breaks is required/);
+    t.throws(() => isolines(points, 'string'), /breaks must be an Array/);
+    t.throws(() => isolines(points, [1, 2, 3], 5), /zProperty must be a string/);
+    t.throws(() => isolines(points, [1, 2, 3], 'temp', 'string'), /propertiesToAllIsolines must be an Object/);
+    t.throws(() => isolines(points, [1, 2, 3], 'temp', {}, 'string'), /propertiesPerIsoline must be an Array/);
+
+    t.end();
+});
+
+test('isolines -- handling properties', t => {
+    const points = pointGrid([-70.823364, -33.553984, -70.473175, -33.302986], 5);
+    const propertiesToAllIsolines = {name: 'unknown', source: 'foobar'};
+    const propertiesPerIsoline = [{name: 'break1'}, {name: 'break2'}, {name: 'break3'}];
+
+    const lines = isolines(points, [1, 2, 3], 'z', propertiesToAllIsolines, propertiesPerIsoline);
+    t.equal(lines.features[0].properties.name, 'break2');
+    t.equal(lines.features[0].properties.source, 'foobar');
+    t.end();
 });
