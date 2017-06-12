91e559f1332ecc10ac5b484aafd03898446ab3eb
Denis <carriere.denis@gmail.com>
2017-06-12 08:50:24 -0400

    Update benchmark results

--- packages/turf-buffer/bench.js
+++ packages/turf-buffer/bench.js
@@ -16,12 +16,25 @@ const fixtures = fs.readdirSync(directory).map(filename => {
 /**
  * Benchmark Results
  *
- * feature-collection-points x 1,205 ops/sec ±11.92% (72 runs sampled)
- * geometry-collection-points x 2,102 ops/sec ±4.49% (78 runs sampled)
- * linestring x 3,434 ops/sec ±3.17% (80 runs sampled)
- * multi-linestring x 675 ops/sec ±2.89% (85 runs sampled)
- * multi-point x 2,077 ops/sec ±5.69% (73 runs sampled)
- * multi-polygon x 1,120 ops/sec ±5.97% (80 runs sampled)
+ * feature-collection-points: 139.101ms
+ * geometry-collection-points: 20.334ms
+ * issue-#783: 33.209ms
+ * linestring: 6.371ms
+ * multi-linestring: 48.786ms
+ * multi-point: 7.627ms
+ * multi-polygon: 38.921ms
+ * negative-buffer: 5.621ms
+ * north-latitude-points: 71.144ms
+ * northern-polygon: 2.644ms
+ * point: 8.155ms
+ * polygon-with-holes: 6.965ms
+ * feature-collection-points x 722 ops/sec ±14.28% (65 runs sampled)
+ * geometry-collection-points x 1,314 ops/sec ±7.87% (66 runs sampled)
+ * issue-#783 x 1,404 ops/sec ±6.81% (64 runs sampled)
+ * linestring x 2,936 ops/sec ±6.94% (72 runs sampled)
+ * multi-linestring x 623 ops/sec ±4.35% (79 runs sampled)
+ * multi-point x 1,735 ops/sec ±8.60% (65 runs sampled)
+ * multi-polygon x 1,125 ops/sec ±3.93% (80 runs sampled)
  * north-latitude-points x 1,649 ops/sec ±2.09% (86 runs sampled)
  * northern-polygon x 4,658 ops/sec ±3.08% (78 runs sampled)
  * point x 65,020 ops/sec ±1.29% (85 runs sampled)
@@ -29,6 +42,9 @@ const fixtures = fs.readdirSync(directory).map(filename => {
  */
 const suite = new Benchmark.Suite('turf-buffer');
 for (const {name, geojson} of fixtures) {
+    console.time(name);
+    buffer(geojson, 50, 'miles');
+    console.timeEnd(name);
     suite.add(name, () => buffer(geojson, 50, 'miles'));
 }
 
