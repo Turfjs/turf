0fb04667d992424de8d8a0d1ab0f67b249bafa50
Denis <carriere.denis@gmail.com>
2017-06-18 11:47:41 -0400

    Dynamic test fixtures - Update Typescript URL - Add validation - Add dynamic tests fixtures

--- packages/turf-idw/index.d.ts
+++ packages/turf-idw/index.d.ts
@@ -5,8 +5,8 @@ type Points = GeoJSON.FeatureCollection<GeoJSON.Point>;
 type Polygons = GeoJSON.FeatureCollection<GeoJSON.Polygon>;
 
 /**
- * http://turfjs.org/docs/
+ * http://turfjs.org/docs/#idw
  */
-declare function idw(controlPoints: Points, valueField: string, b: number, cellWidth: number, units?: Units): Polygons;
+declare function idw(controlPoints: Points, valueField: string, weight: number, cellWidth: number, units?: Units): Polygons;
 declare namespace idw { }
 export = idw;
