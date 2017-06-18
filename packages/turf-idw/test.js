c12e8bb3de28ca85444887fd03c3d5fe4f274788
Denis <carriere.denis@gmail.com>
2017-06-18 13:06:54 -0400

    truncate fixtures

--- packages/turf-idw/test.js
+++ packages/turf-idw/test.js
@@ -3,6 +3,7 @@ const path = require('path');
 const test = require('tape');
 const write = require('write-json-file');
 const load = require('load-json-file');
+const truncate = require('@turf/truncate');
 const {point, featureCollection} = require('@turf/helpers');
 const idw = require('./');
 
@@ -22,7 +23,7 @@ let fixtures = fs.readdirSync(directories.in).map(filename => {
 test('idw', t => {
     for (const {name, geojson, filename} of fixtures) {
         const {valueField, weight, cellWidth, units} = geojson.properties || {};
-        const result = idw(geojson, valueField, weight, cellWidth, units);
+        const result = truncate(idw(geojson, valueField, weight, cellWidth, units));
 
         if (process.env.REGEN) write.sync(directories.out + filename, result);
 
