20fc6c0f1881dc221ce8fa9281aa358e5ba73c21
Denis <carriere.denis@gmail.com>
2017-06-22 20:39:25 -0400

    Update index.js

--- packages/turf-linestring-to-polygon/index.js
+++ packages/turf-linestring-to-polygon/index.js
@@ -8,7 +8,7 @@ var lineString = helpers.lineString;
 /**
  * Converts (Multi)LineString(s) to Polygon(s).
  *
- * @name linestringToPolygon
+ * @name lineStringToPolygon
  * @param {FeatureCollection|Feature<LineString|MultiLineString>} lines Features to convert
  * @param {Object} [properties] translates GeoJSON properties to Feature
  * @param {boolean} [autoComplete=true] auto complete linestrings (matches first & last coordinates)
@@ -23,7 +23,7 @@ var lineString = helpers.lineString;
  *     'coordinates': [[125, -30], [145, -30], [145, -20], [125, -20], [125, -30]]
  *   }
  * }
- * var polygon = turf.linestringToPolygon(line);
+ * var polygon = turf.lineStringToPolygon(line);
  *
  * //addToMap
  * var addToMap = [polygon];
