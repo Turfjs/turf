0195c9f8e606a2184f6cc53058b168b240eb5eb1
Denis <carriere.denis@gmail.com>
2017-07-09 21:54:56 -0400

    Remove un-used types.ts file

--- packages/turf-interpolate/types.ts
+++ packages/turf-interpolate/types.ts
@@ -1,15 +1,20 @@
 import * as interpolate from './';
-import {point, featureCollection} from '@turf/helpers';
+import {point, featureCollection, Points} from '@turf/helpers';
 
 const cellSize = 1;
 const property = 'pressure';
 const weight = 0.5;
 const units = 'miles';
-const points = featureCollection([
+const points: Points = featureCollection([
   point([1, 2]),
   point([12, 13]),
   point([23, 22]),
-])
+]);
 
-const grid = interpolate(points, cellSize, property, units, weight)
-grid.features[0].properties.pressure
+const grid = interpolate(points, cellSize, property, units, weight);
+grid.features[0].properties.pressure;
+
+// Optional properties
+interpolate(points, cellSize, property, units);
+interpolate(points, cellSize, property);
+interpolate(points, cellSize);
