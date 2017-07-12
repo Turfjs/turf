cb8dc639a167d1c2be9bdcb88b9a2eb2fcac2fea
Denis <carriere.denis@gmail.com>
2017-07-12 11:58:30 -0400

    Update Typescript definition

--- packages/turf-point-grid/index.d.ts
+++ packages/turf-point-grid/index.d.ts
@@ -1,8 +1,22 @@
-import {BBox, Points, Units, Feature, Features} from '@turf/helpers';
+/// <reference types="geojson" />
+
+import * as helpers from '@turf/helpers';
 
 /**
  * http://turfjs.org/docs/#pointgrid
  */
-declare function pointGrid(bbox: BBox | Feature<any> | Array<number>, cellSide: number, units?: Units, centered?: boolean, bboxIsMask?: boolean): Points;
-declare namespace pointGrid { }
+declare function pointGrid(
+    bbox: pointGrid.BBox | pointGrid.Feature | pointGrid.FeatureCollection,
+    cellSide: number,
+    units?: pointGrid.Units,
+    centered?: boolean,
+    bboxIsMask?: boolean): pointGrid.Points;
+
+declare namespace pointGrid {
+    type Feature = GeoJSON.Feature<any>;
+    type FeatureCollection = GeoJSON.FeatureCollection<any>;
+    type Units = helpers.Units;
+    type Points = GeoJSON.FeatureCollection<GeoJSON.Point>;
+    type BBox = helpers.BBox;
+}
 export = pointGrid;
