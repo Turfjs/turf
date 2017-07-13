08e5451d191e5f37907d3a713faf81e41666d79d
stebogit <stefano.borghi@gmail.com>
2017-07-12 23:51:35 -0700

    replaced coordAll with flattenEach; added multiLine3 test;

--- packages/turf-point-on-line/index.js
+++ packages/turf-point-on-line/index.js
@@ -2,11 +2,13 @@ var meta = require('@turf/meta');
 var helpers = require('@turf/helpers');
 var bearing = require('@turf/bearing');
 var distance = require('@turf/distance');
+var invariant = require('@turf/invariant');
 var destination = require('@turf/destination');
 var lineIntersects = require('@turf/line-intersect');
 var point = helpers.point;
-var coordAll = meta.coordAll;
+var getCoords = invariant.getCoords;
 var lineString = helpers.lineString;
+var flattenEach = meta.flattenEach;
 
 /**
  * Takes a {@link Point} and a {@link LineString} and calculates the closest Point on the (Multi)LineString.
@@ -57,49 +59,52 @@ module.exports = function (lines, pt, units) {
     var closestPt = point([Infinity, Infinity], {
         dist: Infinity
     });
+
     var length = 0.0;
+    flattenEach(lines, function (line) {
+        var coords = getCoords(line);
 
-    var allCoords = coordAll(lines);
+        for (var i = 0; i < coords.length - 1; i++) {
+            //start
+            var start = point(coords[i]);
+            start.properties.dist = distance(pt, start, units);
+            //stop
+            var stop = point(coords[i + 1]);
+            stop.properties.dist = distance(pt, stop, units);
+            // sectionLength
+            var sectionLength = distance(start, stop, units);
+            //perpendicular
+            var heightDistance = Math.max(start.properties.dist, stop.properties.dist);
+            var direction = bearing(start, stop);
+            var perpendicularPt1 = destination(pt, heightDistance, direction + 90, units);
+            var perpendicularPt2 = destination(pt, heightDistance, direction - 90, units);
+            var intersect = lineIntersects(lineString([perpendicularPt1.geometry.coordinates, perpendicularPt2.geometry.coordinates]), lineString([start.geometry.coordinates, stop.geometry.coordinates]));
+            var intersectPt = null;
+            if (intersect.features.length > 0) {
+                intersectPt = intersect.features[0];
+                intersectPt.properties.dist = distance(pt, intersectPt, units);
+                intersectPt.properties.location = length + distance(start, intersectPt, units);
+            }
 
-    for (var i = 0; i < allCoords.length - 1; i++) {
-        var start = point(allCoords[i]);
-        var stop = point(allCoords[i + 1]);
-        //start
-        start.properties.dist = distance(pt, start, units);
-        //stop
-        stop.properties.dist = distance(pt, stop, units);
-        // sectionLength
-        var sectionLength = distance(start, stop, units);
-        //perpendicular
-        var heightDistance = Math.max(start.properties.dist, stop.properties.dist);
-        var direction = bearing(start, stop);
-        var perpendicularPt1 = destination(pt, heightDistance, direction + 90, units);
-        var perpendicularPt2 = destination(pt, heightDistance, direction - 90, units);
-        var intersect = lineIntersects(lineString([perpendicularPt1.geometry.coordinates, perpendicularPt2.geometry.coordinates]), lineString([start.geometry.coordinates, stop.geometry.coordinates]));
-        var intersectPt = null;
-        if (intersect.features.length > 0) {
-            intersectPt = intersect.features[0];
-            intersectPt.properties.dist = distance(pt, intersectPt, units);
-            intersectPt.properties.location = length + distance(start, intersectPt, units);
+            if (start.properties.dist < closestPt.properties.dist) {
+                closestPt = start;
+                closestPt.properties.index = i;
+                closestPt.properties.location = length;
+            }
+            if (stop.properties.dist < closestPt.properties.dist) {
+                closestPt = stop;
+                closestPt.properties.index = i + 1;
+                closestPt.properties.location = length + sectionLength;
+            }
+            if (intersectPt && intersectPt.properties.dist < closestPt.properties.dist) {
+                closestPt = intersectPt;
+                closestPt.properties.index = i;
+            }
+            // update length
+            length += sectionLength;
         }
 
-        if (start.properties.dist < closestPt.properties.dist) {
-            closestPt = start;
-            closestPt.properties.index = i;
-            closestPt.properties.location = length;
-        }
-        if (stop.properties.dist < closestPt.properties.dist) {
-            closestPt = stop;
-            closestPt.properties.index = i + 1;
-            closestPt.properties.location = length + sectionLength;
-        }
-        if (intersectPt && intersectPt.properties.dist < closestPt.properties.dist) {
-            closestPt = intersectPt;
-            closestPt.properties.index = i;
-        }
-        // update length
-        length += sectionLength;
-    }
+    });
 
     return closestPt;
 };
