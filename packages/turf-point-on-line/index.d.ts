906ce11d99454576a73254e54c781d3d8541d411
Stefano Borghi <stebogit@users.noreply.github.com>
2017-07-13 12:07:11 -0700

    Added MultiLineString support for @turf/point-on-line (#838)
    * added MultiLineString as valid input;
    added multiLineString test;
    updated relevant files;
    
    * Improved Typescript definition
    - Improved Typescript Definition with additional properties
    - Added Typescript tets
    - Added Geometry tests
    
    * Add 2nd MultiLine tests
    
    * replaced coordAll with flattenEach;
    added multiLine3 test;

--- packages/turf-point-on-line/index.d.ts
+++ packages/turf-point-on-line/index.d.ts
@@ -1,11 +1,28 @@
 /// <reference types="geojson" />
 
-type Point = GeoJSON.Feature<GeoJSON.Point>;
-type LineString = GeoJSON.Feature<GeoJSON.LineString>;
+import {Units} from '@turf/helpers'
+
+type Point = GeoJSON.Feature<GeoJSON.Point> | GeoJSON.Point | number[];
+type LineString = GeoJSON.LineString;
+type MultiLineString = GeoJSON.MultiLineString;
+type Line = GeoJSON.Feature<LineString | MultiLineString> | LineString | MultiLineString;
+
+interface PointOnLine extends GeoJSON.Feature<GeoJSON.Point> {
+    properties: {
+        index?: number
+        dist?: number
+        location?: number
+        [key: string]: any
+    }
+}
 
 /**
  * http://turfjs.org/docs/#pointonline
  */
-declare function pointOnLine(line: LineString, point: Point, units?: string): Point;
+declare function pointOnLine(
+    line: Line,
+    point: Point,
+    units?: Units): PointOnLine;
+
 declare namespace pointOnLine { }
 export = pointOnLine;
