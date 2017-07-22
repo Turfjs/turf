f1cad4dde0621231822a7821fb5273ffc8e9f86d
stebogit <stefano.borghi@gmail.com>
2017-07-22 01:53:22 -0700

    refactored tests; updated bench;

--- packages/turf-boolean-overlaps/bench.js
+++ packages/turf-boolean-overlaps/bench.js
@@ -1,20 +1,37 @@
-var overlaps = require('./');
-var Benchmark = require('benchmark');
-var fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const load = require('load-json-file');
+const overlaps = require('./');
+const Benchmark = require('benchmark');
 
-var poly0 = JSON.parse(fs.readFileSync(__dirname+'/test/in/polygon0.geojson'));
-var poly1 = JSON.parse(fs.readFileSync(__dirname+'/test/in/polygon1.geojson'));
-var poly2 = JSON.parse(fs.readFileSync(__dirname+'/test/in/polygon2.geojson'));
-var suite = new Benchmark.Suite('turf-overlaps');
+/**
+ * Benchmark Results
+ *
+ * lines: 0.822ms
+ * polygons: 0.354ms
+ * lines: 0.599ms
+ * lines2: 0.029ms
+ * polygons: 0.062ms
+ * polygons2: 0.037ms
+ * lines x 159,745 ops/sec ±2.78% (78 runs sampled)
+ * polygons x 129,087 ops/sec ±2.40% (78 runs sampled)
+ * lines x 245,938 ops/sec ±2.26% (78 runs sampled)
+ * lines2 x 684,221 ops/sec ±2.07% (79 runs sampled)
+ * polygons x 242,569 ops/sec ±2.53% (77 runs sampled)
+ * polygons2 x 364,308 ops/sec ±4.93% (68 runs sampled)
+ */
+const suite = new Benchmark.Suite('turf-boolean-disjoint');
+glob.sync(path.join(__dirname, 'test', '**', '*.geojson')).forEach(filepath => {
+    const {name} = path.parse(filepath);
+    const geojson = load.sync(filepath);
+    const [feature1, feature2] = geojson.features;
+    console.time(name);
+    overlaps(feature1, feature2);
+    console.timeEnd(name);
+    suite.add(name, () => overlaps(feature1, feature2));
+});
+
 suite
-  .add('turf-overlaps#polygons-overlap',function () {
-    overlaps(poly0, poly1);
-  })
-  .add('turf-overlaps#polygons-no-overlap',function () {
-    overlaps(poly0, poly2);
-  })
-  .on('cycle', function (event) {
-    console.log(String(event.target));
-  })
-  .on('complete', function () {})
-  .run();
+    .on('cycle', e => console.log(String(e.target)))
+    .on('complete', () => {})
+    .run();
--- packages/turf-boolean-overlaps/test.js
+++ packages/turf-boolean-overlaps/test.js
@@ -1,30 +1,44 @@
-const fs = require('fs');
 const glob = require('glob');
 const path = require('path');
 const test = require('tape');
 const load = require('load-json-file');
+const {point, lineString} = require('@turf/helpers');
 const overlaps = require('./');
 
+test('turf-boolean-overlaps', t => {
+    // True Fixtures
+    glob.sync(path.join(__dirname, 'test', 'true', '**', '*.geojson')).forEach(filepath => {
+        const {name} = path.parse(filepath);
+        const geojson = load.sync(filepath);
+        const [feature1, feature2] = geojson.features;
+        const result = overlaps(feature1, feature2);
 
-test('overlaps -- polygon', t => {
-    poly1 = JSON.parse(fs.readFileSync(__dirname + '/test/in/polygon0.geojson'));
-    poly2 = JSON.parse(fs.readFileSync(__dirname + '/test/in/polygon1.geojson'));
-    poly3 = JSON.parse(fs.readFileSync(__dirname + '/test/in/polygon2.geojson'));
+        if (process.env.SHAPELY) shapely.contains(feature1, feature2).then(result => t.true(result, '[true] shapely - ' + name));
+        t.true(result, '[true] ' + name);
+    });
+    // False Fixtures
+    glob.sync(path.join(__dirname, 'test', 'false', '**', '*.geojson')).forEach(filepath => {
+        const {name} = path.parse(filepath);
+        const geojson = load.sync(filepath);
+        const [feature1, feature2] = geojson.features;
+        const result = overlaps(feature1, feature2);
 
-    t.true(overlaps(poly1, poly2), "Polygons 1 and 2 overlap");
-    t.true(overlaps(poly2, poly3), "Polygons 2 and 3 overlap");
-    t.false(overlaps(poly1, poly3), "Polygons 1 and 3 do not overlap");
+        if (process.env.SHAPELY) shapely.contains(feature1, feature2).then(result => t.false(result, '[false] shapely - ' + name));
+        t.false(result, '[false] ' + name);
+    });
     t.end();
 });
 
-test('overlaps -- linestring', t => {
-    line1 = JSON.parse(fs.readFileSync(__dirname + '/test/in/linestring0.geojson'));
-    line2 = JSON.parse(fs.readFileSync(__dirname + '/test/in/linestring1.geojson'));
-    line3 = JSON.parse(fs.readFileSync(__dirname + '/test/in/linestring2.geojson'));
+test('turf-boolean-overlaps -- throws', t => {
+    const pt = point([9, 50]);
+    const line = lineString([[7, 50], [8, 50], [9, 50]]);
 
-    t.true(overlaps(line1, line2), "LineStrings 1 and 2 overlap");
-    t.true(overlaps(line2, line3), "LineStrings 2 and 3 overlap");
-    t.false(overlaps(line1, line3), "LineStrings 1 and 3 do not overlap");
+    t.throws(() => overlaps(null, line), '<geojson> is required');
+    t.throws(() => overlaps(line, null), '<geojson> is required');
+    t.throws(() => overlaps(pt, line), 'feature1 Point geometry not supported');
+    t.throws(() => overlaps(line, pt), 'feature2 Point geometry not supported');
+    t.throws(() => overlaps({"feature": "non GeoJSON"}, line), '<geojson> must be a Feature or Geometry Object');
+    t.throws(() => overlaps(line, {"feature": "non GeoJSON"}), '<geojson> must be a Feature or Geometry Object');
+
     t.end();
 });
-
