fcdda7bba592fe0753fed524455652bf59e3400a
stebogit <stefano.borghi@gmail.com>
2017-07-29 20:57:19 -0700

    first implementation

--- packages/turf-clean-coords/index.js
+++ packages/turf-clean-coords/index.js
@@ -1,3 +1,9 @@
+var meta = require('@turf/meta');
+var clone = require('@turf/clone');
+var coordAll = meta.coordAll;
+var invariant = require('@turf/invariant');
+var getCoords = invariant.getCoords;
+
 /**
  * Removes redundant coordinates from a (Multi)LineString or (Multi)Polygon; ignores (Multi)Point.
  *
@@ -12,25 +18,59 @@
  * //= [[0, 0], [0, 10]]
  */
 module.exports = function (geojson, mutate) {
+    if (!geojson) throw new Error('geojson is required');
 
+    var points = getCoords(geojson);
 
-    var prevPoint = points[0],
-        newPoints = [prevPoint],
-        point;
+    var prevPoint, point, nextPoint;
+    var newPoints = [];
+    var secondToLast = points.length - 1;
 
-    for (var i = 1, len = points.length; i < len; i++) {
+    newPoints.push(points[0]);
+    for (var i = 1; i < secondToLast; i++) {
+        prevPoint = points[i - 1];
         point = points[i];
+        nextPoint = points[i + 1];
 
-        if (getSqDist(point, prevPoint) > sqTolerance) {
+        if (!isPointOnLineSegment(prevPoint, nextPoint, point)) {
             newPoints.push(point);
-            prevPoint = point;
         }
     }
+    if (nextPoint[0] !== point[0] || nextPoint[1] !== point[1]) newPoints.push(nextPoint);
 
-    if (prevPoint !== point) newPoints.push(point);
+    var output = (mutate === true)
+        ? geojson
+        : clone(geojson, true);
+    if (output.coordinates)
+        output.coordinates = newPoints;
+    else
+        output.geometry.coordinates = newPoints;
 
-    return newPoints;
-
-
-    return cleaned;
+    return output;
 };
+
+/**
+ * Returns if `point` is on the segment between `start` and `end`.
+ * Borrowed from `@turf/boolean-point-on-line` to speed up the evaluation
+ *
+ * @private
+ * @param {Array<number>} start coord pair of start of line
+ * @param {Array<number>} end coord pair of end of line
+ * @param {Array<number>} point coord pair of point to check
+ * @returns {boolean} true/false
+ */
+function isPointOnLineSegment(start, end, point) {
+    var x = point[0], y = point[1];
+    var startX = start[0], startY = start[1];
+    var endX = end[0], endY = end[1];
+
+    var dxc = x - startX;
+    var dyc = y - startY;
+    var dxl = endX - startX;
+    var dyl = endY - startY;
+    var cross = dxc * dyl - dyc * dxl;
+
+    if (cross !== 0) return false;
+    else if (Math.abs(dxl) >= Math.abs(dyl)) return dxl > 0 ? startX <= x && x <= endX : endX <= x && x <= startX;
+    else return dyl > 0 ? startY <= y && y <= endY : endY <= y && y <= startY;
+}
--- packages/turf-clean-coords/test.js
+++ packages/turf-clean-coords/test.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const test = require('tape');
 const path = require('path');
 const load = require('load-json-file');
+const helpers = require('@turf/helpers');
+const lineString = helpers.lineString;
 const write = require('write-json-file');
 const cleanCoords = require('./');
 
@@ -20,8 +22,8 @@ const fixtures = fs.readdirSync(directories.in).map(filename => {
 
 test('turf-clean-coords', t => {
     for (const {filename, name, geojson}  of fixtures) {
-        const [feature1, feature2] = geojson.features;
-        const results = cleanCoords(feature1, feature2);
+        const {mutate} = geojson.properties;
+        const results = cleanCoords(geojson, mutate);
 
         if (process.env.REGEN) write.sync(directories.out + filename, results);
         t.deepEqual(results, load.sync(directories.out + filename), name);
@@ -30,9 +32,15 @@ test('turf-clean-coords', t => {
 });
 
 test('turf-clean-coords -- throws', t => {
-    const line = lineString([[10, 10], [12, 15]]);
+    t.throws(() => cleanCoords(null), /geojson is required/, 'missing geojson');
+    t.end();
+});
 
-    t.throws(() => cleanCoords(null), /geojson required/);
+const line = lineString([[0, 0], [1, 1], [2, 2]], {foo: 'bar'});
 
+test('turf-clean-coords -- prevent input mutation', t => {
+    const before = JSON.parse(JSON.stringify(line));
+    cleanCoords(line);
+    t.deepEqual(before, line);
     t.end();
 });
