c014749f45ab904a5348e273e97ce4f701eff597
Rowan Winsemius <rowanwins@yahoo.com.au>
2017-08-08 21:10:48 +1000

    working commit

--- packages/turf-buffer/index.js
+++ packages/turf-buffer/index.js
@@ -1,22 +1,26 @@
+var center = require('@turf/center');
 var getCoords = require('@turf/invariant').getCoords;
 var coordEach = require('@turf/meta').coordEach;
 var helpers = require('@turf/helpers');
 var bearingToAngle = helpers.bearingToAngle;
 var polygon = helpers.polygon;
 var point = helpers.point;
+var distanceToRadians = helpers.distanceToRadians;
 var distanceToDegrees = helpers.distanceToDegrees;
+var radiansToDegrees = helpers.radians2degrees;
+var radiansToDistance = helpers.radiansToDistance;
 var bearing = require('@turf/bearing');
 var lineArc = require('@turf/line-arc');
+var circle = require('@turf/circle');
 
 /**
- * Takes a {@link LineString|line} and returns a {@link LineString|line} at offset by the specified distance.
+ * Takes a {@link Feature and returns a {@link Feature} at offset by the specified distance.
  *
- * @name lineOffset
- * @param {Geometry|Feature<LineString>} feature input line
- * @param {number} distance distance to offset the line (can be of negative value)
+ * @name buffer
+ * @param {Geometry|Feature<LineString>} feature input
+ * @param {number} distance distance to buffer the feature (can be of negative value)
  * @param {string} [units=kilometers] can be degrees, radians, miles, kilometers, inches, yards, meters
- * @param {number} steps number of steps on a rounded corner
- * @returns {Feature<LineString>} Line offset from the input line
+ * @returns {Feature} Feature buffered from the input feature
  * @example
  * var line = {
  *   "type": "Feature",
@@ -27,119 +31,162 @@ var lineArc = require('@turf/line-arc');
  *   }
  * };
  *
- * var offsetLine = turf.buffer(line, 2, 'miles');
+ * var buffered = turf.buffer(line, 2, 'miles');
  *
  * //addToMap
- * var addToMap = [offsetLine, line]
+ * var addToMap = [buffered, line]
  */
-module.exports = function (feature, distance, units, steps) {
-    if (!feature) throw new Error('line is required');
+module.exports = function (geojson, distance, units, steps) {
+    if (!geojson) throw new Error('feature is required');
     if (distance === undefined || distance === null || isNaN(distance)) throw new Error('distance is required');
     var numSteps = steps ? steps : 64;
 
-    var offsetDegrees = distanceToDegrees(distance, units);
-    switch (feature.geometry.type) {
-    case 'Polygon':
-        return bufferPolygon(feature, offsetDegrees, numSteps);
-    case 'LineString':
-        return bufferLineString(feature, offsetDegrees, numSteps);
+    var properties = geojson.properties || {};
+
+    var geometry = (geojson.type === 'Feature') ? geojson.geometry : geojson;
+    var distance = distanceToDegrees(distance, units);
+
+    var buffered = null
+    switch (geometry.type) {
+        case 'Polygon':
+            buffered = bufferPolygon(geometry, distance, numSteps);
+            break;
+        case 'LineString':
+            buffered = bufferLineString(geometry, distance, numSteps);
+            break;
+        case 'Point':
+            buffered = bufferPoint(geometry, distance, numSteps);
     }
+
+    buffered.properties = properties
+    return buffered;
 };
 
-function bufferPolygon(feature, offsetDegrees, steps) {
+function bufferPoint (geometry, distance, steps) {
+    return circle(geometry, distance, steps, 'degrees');
+}
+
+function bufferPolygon (feature, distance, steps) {
     var coords = getCoords(feature);
     var finalCoords = [];
-    var prevCoords = coords[0][coords[0].length - 2];
+    
+    var prevCoords = coords[0][coords[0].length - 2]
     var nextCoords = null;
 
     coordEach(feature, function (currentCoords, index) {
         if (index !== 0) {
-            prevCoords = coords[0][index - 1];
+            prevCoords = coords[0][index - 1]
         }
         if (index === coords[0].length - 1) {
-            nextCoords = coords[0][1];
+            nextCoords = coords[0][1]
         } else {
-            nextCoords = coords[0][index + 1];
+            nextCoords = coords[0][index + 1]
         }
         var bearingPrevCoords = bearing(point(currentCoords), point(prevCoords));
         var bearingNextCoords = bearing(point(currentCoords), point(nextCoords));
         var angleInDegs = bearingToAngle(bearingToAngle(bearingNextCoords) - bearingToAngle(bearingPrevCoords));
 
         if (index > 0) {
-            var segment = processSegment(currentCoords, nextCoords, offsetDegrees);
-            var prevSegment = processSegment(prevCoords, currentCoords, offsetDegrees);
+            var segment = processSegment(currentCoords, nextCoords, distance);
+            var prevSegment = processSegment(prevCoords, currentCoords, distance);
             if (angleInDegs < 180) {
-                var outsector = createRounded(currentCoords, offsetDegrees, bearingNextCoords, bearingPrevCoords, steps);
-                finalCoords = finalCoords.concat(outsector.geometry.coordinates.reverse());
+                var outsector = getJoin('round', currentCoords, distance, bearingNextCoords, bearingPrevCoords, steps)
+                finalCoords = finalCoords.concat(outsector)                   
             } else {
                 var intersects = checkLineIntersection(segment[0][0], segment[0][1], segment[1][0], segment[1][1], prevSegment[0][0], prevSegment[0][1], prevSegment[1][0], prevSegment[1][1]);
                 finalCoords.push([intersects.x, intersects.y]);
             }
         }
     });
-    finalCoords.push(finalCoords[0]);
+    finalCoords.push(finalCoords[0])
     return polygon([finalCoords], polygon.properties);
 }
 
-function bufferLineString(feature, offsetDegrees, steps) {
+function bufferLineString (feature, distance, steps, endType, joinType) {
     var coords = getCoords(feature);
     var finalCoords = [];
     var otherSideCoords = [];
-    var prevCoords = coords[coords.length - 2];
+    var prevCoords = coords[coords.length - 2]
     var nextCoords = null;
-    var finalRounded = null;
+    var finalRounded = null
     coordEach(feature, function (currentCoords, index) {
         if (index === 0) {
-            nextCoords = coords[index + 1];
+            nextCoords = coords[index + 1]
             var bearingNextCoords = bearing(point(currentCoords), point(nextCoords));
-            var outsector = createRounded(currentCoords, offsetDegrees, bearingNextCoords, bearingNextCoords, steps);
-            finalCoords = finalCoords.concat(outsector.geometry.coordinates.reverse());
+            var startEndcap = getEnd('square', currentCoords, distance, bearingNextCoords, bearingNextCoords, steps)
+            finalCoords = finalCoords.concat(startEndcap)
         } else if (index === coords.length - 1) {
-            prevCoords = coords[index - 1];
+            prevCoords = coords[index - 1]
             var bearingPrevCoords = bearing(point(currentCoords), point(prevCoords));
-            var outsector = createRounded(currentCoords, offsetDegrees, bearingPrevCoords, bearingPrevCoords, steps);
-            finalRounded = outsector.geometry.coordinates.reverse();
-        } else {
-            prevCoords = coords[index - 1];
-            nextCoords = coords[index + 1];
+            var endEndcap = getEnd('square', currentCoords, distance, bearingPrevCoords, bearingPrevCoords, steps)
+            finalRounded = endEndcap
+        } 
+        else {
+            prevCoords = coords[index - 1]
+            nextCoords = coords[index + 1]
             var bearingPrevCoords = bearing(point(currentCoords), point(prevCoords));
             var bearingNextCoords = bearing(point(currentCoords), point(nextCoords));
             var angleInDegs = bearingToAngle(bearingToAngle(bearingNextCoords) - bearingToAngle(bearingPrevCoords));
 
-            var segment = processSegment(currentCoords, nextCoords, offsetDegrees);
-            var prevSegment = processSegment(prevCoords, currentCoords, offsetDegrees);
+            var segment = processSegment(currentCoords, nextCoords, distance);
+            var prevSegment = processSegment(prevCoords, currentCoords, distance);
 
             if (angleInDegs < 180) {
-                var outsector = createRounded(currentCoords, offsetDegrees, bearingNextCoords, bearingPrevCoords, steps);
-                finalCoords = finalCoords.concat(outsector.geometry.coordinates.reverse());
-                var segmentRev = processSegment(currentCoords, nextCoords, -Math.abs(offsetDegrees));
-                var prevSegmentRev = processSegment(prevCoords, currentCoords, -Math.abs(offsetDegrees));
+                var outsector = getJoin('round', currentCoords, distance, bearingNextCoords, bearingPrevCoords, steps)
+                finalCoords = finalCoords.concat(outsector.geometry.coordinates.reverse())
+                var segmentRev = processSegment(currentCoords, nextCoords, -Math.abs(distance));
+                var prevSegmentRev = processSegment(prevCoords, currentCoords, -Math.abs(distance));  
                 var intersects = checkLineIntersection(segmentRev[0][0], segmentRev[0][1], segmentRev[1][0], segmentRev[1][1], prevSegmentRev[0][0], prevSegmentRev[0][1], prevSegmentRev[1][0], prevSegmentRev[1][1]);
-                otherSideCoords.push([intersects.x, intersects.y]);
+                otherSideCoords.push([intersects.x, intersects.y]);             
             } else {
                 var intersects = checkLineIntersection(segment[0][0], segment[0][1], segment[1][0], segment[1][1], prevSegment[0][0], prevSegment[0][1], prevSegment[1][0], prevSegment[1][1]);
                 finalCoords.push([intersects.x, intersects.y]);
-                var outsector = createRounded(currentCoords, offsetDegrees, bearingPrevCoords, bearingNextCoords, steps);
-                otherSideCoords = otherSideCoords.concat(outsector.geometry.coordinates);
+                var outsector = getJoin('round', currentCoords, distance, bearingPrevCoords, bearingNextCoords, steps)
+                otherSideCoords = otherSideCoords.concat(outsector.reverse())
             }
         }
     });
-    finalCoords = finalCoords.concat(finalRounded);
-    finalCoords = finalCoords.concat(otherSideCoords.reverse());
-    finalCoords.push(finalCoords[0]);
+    finalCoords = finalCoords.concat(finalRounded)
+    finalCoords = finalCoords.concat(otherSideCoords.reverse())
+    finalCoords.push(finalCoords[0])
     return polygon([finalCoords], feature.properties);
 }
 
-
-function createRounded(coords, offsetDegrees, bearingNextCoords, bearingPrevCoords, numSteps) {
-    return lineArc(point(coords), offsetDegrees, bearingNextCoords + 90, bearingPrevCoords - 90, numSteps, 'degrees');
+function getJoin (joinType, coords, distance, bearingNextCoords, bearingPrevCoords, numSteps) {
+    switch (joinType) {
+        case 'round':
+            return createRounded(coords, distance, bearingNextCoords, bearingPrevCoords, numSteps);
+        case 'bevel':
+            return createBevel(feature, distance, numSteps);
+        case 'mitre':
+            return createMitre(feature, distance, numSteps);
+    }
 }
 
-function createBevel() {
-
+function getEnd (endType, coords, distance, bearingNextCoords, bearingPrevCoords, numSteps) {
+    switch (endType) {
+        case 'round':
+            return createRounded(coords, distance, bearingNextCoords, bearingPrevCoords, numSteps);
+        case 'square':
+            return createSquare();
+        case 'flat':
+            return createFlat(coords);
+    }
 }
 
-function createMitre() {
+function createRounded (coords, distance, bearingNextCoords, bearingPrevCoords, numSteps) {
+    var arc = lineArc(point(coords), distance, bearingNextCoords + 90, bearingPrevCoords - 90, numSteps, 'degrees');
+    return arc.geometry.coordinates.reverse()
+}
+
+function createSquare () {
+    
+
+
+    return []
+}
+
+function createFlat (coords) {
 
 }
 
@@ -173,13 +220,13 @@ function processSegment(point1, point2, offset) {
  */
 function checkLineIntersection(line1StartX, line1StartY, line1EndX, line1EndY, line2StartX, line2StartY, line2EndX, line2EndY) {
     var denominator, a, b, numerator1, numerator2, result = {
-            x: null,
-            y: null,
-            onLine1: false,
-            onLine2: false
-        };
+        x: null,
+        y: null,
+        onLine1: false,
+        onLine2: false
+    };
     denominator = ((line2EndY - line2StartY) * (line1EndX - line1StartX)) - ((line2EndX - line2StartX) * (line1EndY - line1StartY));
-    if (denominator === 0) {
+    if (denominator == 0) {
         return result;
     }
     a = line1StartY - line2StartY;
@@ -195,5 +242,5 @@ function checkLineIntersection(line1StartX, line1StartY, line1EndX, line1EndY, l
     if (a > 0 && a < 1) {
         result.onLine1 = true;
     }
-    return result;
+    return result
 }
