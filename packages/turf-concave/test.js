02e95304cec3fd72a2488686f13bcfa7a86970be
stebogit <stefano.borghi@gmail.com>
2017-08-13 21:37:03 -0700

    refactored concave: small refactoring in index.js; replaced JSON.parse/JSON.stringify combo with turf-clone; refactored test and bench to current standard; added issue#333 test case; updated README;

--- packages/turf-concave/test.js
+++ packages/turf-concave/test.js
@@ -1,66 +1,54 @@
-var concave = require('./');
-var test = require('tape');
-var fs = require('fs');
-var featureCollection = require('@turf/helpers').featureCollection;
-var point = require('@turf/helpers').point;
+const fs = require('fs');
+const test = require('tape');
+const path = require('path');
+const load = require('load-json-file');
+const write = require('write-json-file');
+const {point, featureCollection} = require('@turf/helpers');
+const {featureEach} = require('@turf/meta');
+const concave = require('./');
 
-var pts1 = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/pts1.geojson'));
-var pts2 = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/pts2.geojson'));
-var pts3 = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/issue-333.geojson'));
+const directories = {
+    in: path.join(__dirname, 'test', 'in') + path.sep,
+    out: path.join(__dirname, 'test', 'out') + path.sep
+};
 
-// test('concave', function(t){
-//
-//   var ptsOnePoint = featureCollection([point([0, 0])]);
-//   var ptsOnePointHull = null;
-//   t.throws(function(){
-//       ptsOnePointHull = concave(ptsOnePoint, 5.5, 'miles');
-//   }, Error, 'fails with too few points');
-//   t.notOk(ptsOnePointHull, 'hull not computed with too few points');
-//
-//   var ptsNoPointHull = null;
-//   t.throws(function(){
-//       ptsNoPointHull = concave(pts1, 0, 'miles');
-//   }, Error, 'fails with small maxEdge');
-//   t.notOk(ptsNoPointHull, 'hull not computed with small maxEdge');
-//
-//   t.end();
-// });
+const fixtures = fs.readdirSync(directories.in).map(filename => {
+    return {
+        filename,
+        name: path.parse(filename).name,
+        geojson: load.sync(directories.in + filename)
+    };
+});
 
-test('concave', function(t){
-  // var pts1HullMiles = concave(pts1, 5.5, 'miles');
-  // var pts1HullKilometers = concave(pts1, 5.5 * 1.60934, 'kilometers');
-  // t.ok(pts1HullMiles, 'computes hull');
-  // t.equal(pts1HullMiles.type, 'Feature');
-  // t.deepEqual(pts1HullMiles, pts1HullKilometers, 'miles and km should return the same result');
-  //
-  // var pts2HullMiles = concave(pts2, 2, 'miles');
-  // var pts2HullKilometers = concave(pts2, 2 * 1.60934, 'kilometers');
-  // t.ok(pts2HullMiles, 'computes hull');
-  // t.equal(pts2HullMiles.type, 'Feature');
-  // t.deepEqual(pts2HullMiles, pts2HullKilometers, 'miles and km should return the same result');
+test('turf-line-split', t => {
+    for(const {filename, name, geojson}  of fixtures) {
+        const {maxEdge, units} = geojson.properties || {maxEdge: 1};
+        const hull = concave(geojson, maxEdge, units);
+        featureEach(geojson, stylePt);
+        const results = featureCollection([...geojson.features, hull]);
 
-  var pts3HullMiles = concave(pts3, 2, 'miles');
-  var pts3HullKilometers = concave(pts3, 2 * 1.60934, 'kilometers');
-  t.ok(pts3HullMiles, 'computes hull');
-  t.equal(pts3HullMiles.type, 'Feature');
-  t.deepEqual(pts3HullMiles, pts3HullKilometers, 'miles and km should return the same result');
+        if (process.env.REGEN) write.sync(directories.out + filename, results);
+        t.deepEquals(results, load.sync(directories.out + filename), name);
+    }
+    t.end();
+});
 
-  // output results
-  // pts1.features = pts1.features.map(stylePt);
-  // pts1.features.push(pts1HullMiles);
-  // pts2.features = pts2.features.map(stylePt);
-  // pts2.features.push(pts2HullMiles);
-  pts3.features = pts3.features.map(stylePt);
-  pts3.features.push(pts3HullMiles);
-  // fs.writeFileSync(__dirname+'/test/fixtures/out/pts1_out.geojson', JSON.stringify(pts1,null,2));
-  // fs.writeFileSync(__dirname+'/test/fixtures/out/pts2_out.geojson', JSON.stringify(pts2,null,2));
-  fs.writeFileSync(__dirname+'/test/fixtures/out/issue-333_out.geojson', JSON.stringify(pts3,null,2));
 
-  t.end();
+const points = featureCollection([point([0, 0]), point([1, 1]), point([1, 0])]);
+const onePoint = featureCollection([point([0, 0])]);
+
+test('concave', t => {
+    t.throws(() => concave(onePoint, 5.5, 'miles'), /too few polygons found to compute concave hull/, 'too few points');
+    t.throws(() => concave(onePoint, 0), /too few polygons found to compute concave hull/, 'maxEdge too small');
+
+    t.throws(() => concave(null, 0), /points is required/, 'no points');
+    t.throws(() => concave(points, null), /maxEdge is required/, 'no maxEdge');
+    t.throws(() => concave(points, 1, 'foo'), /units is invalid/, 'invalid units');
+
+    t.end();
 });
 
 function stylePt(pt){
-  pt.properties['marker-color'] = '#f0f';
-  pt.properties['marker-size'] = 'small';
-  return pt;
+    pt.properties['marker-color'] = '#f0f';
+    pt.properties['marker-size'] = 'small';
 }
