8a9b4d06ce67891821a37b20f6dbc38e31a14b1f
stebogit <stefano.borghi@gmail.com>
2017-08-27 02:05:03 -0700

    changed calculation logic, results ok in city scale

--- packages/turf-point-to-line-distance/index.js
+++ packages/turf-point-to-line-distance/index.js
@@ -3,9 +3,9 @@ var featureOf = invariant.featureOf;
 var helpers = require('@turf/helpers');
 var meta = require('@turf/meta');
 var distance = require('@turf/distance');
+var rhumbDistance = require('@turf/rhumb-distance');
 var bearing = require('@turf/bearing');
 var turfPoint = helpers.point;
-var radiansToDistance = helpers.radiansToDistance;
 var degrees2radians = helpers.degrees2radians;
 var bearingToAngle = helpers.bearingToAngle;
 var segmentEach = meta.segmentEach;
@@ -21,6 +21,7 @@ var turfLine = helpers.lineString;
  * @param {Feature<LineString>|Array<<Array<number>>} line GeoJSON Feature or Geometry
  * @param {string} [units=kilometers] can be degrees, radians, miles, or kilometers
  * @returns {number} distance between point and line
+ * @param {boolean} [rhumb=false] if distances should be calculated with Rhumb formulae
  * @example
  * var pt = turf.point([0, 0]);
  * var line = turf.lineString([[1, 1],[-1, 1]);
@@ -28,7 +29,7 @@ var turfLine = helpers.lineString;
  * var d = pointToLineDistance(point, line, 'degrees');
  * //=1
  */
-module.exports = function (point, line, units) {
+module.exports = function (point, line, units, rhumb) {
     // validation
     if (!point) throw new Error('point is required');
     if (Array.isArray(point)) point = turfPoint(point);
@@ -40,114 +41,67 @@ module.exports = function (point, line, units) {
     var distance = Infinity;
     var pt = point.geometry.coordinates;
     segmentEach(line, function (segment) {
-        var d = distanceToSegment(pt, segment.geometry.coordinates, units);
+        var d = distanceToSegment(pt, segment.geometry.coordinates, units, rhumb);
         if (distance > d) distance = d;
     });
     return distance;
 };
 
 
-function distanceToSegment(p, segment, units) {
-    var R = {
-        miles: 3960,
-        nauticalmiles: 3441.145,
-        degrees: 57.2957795,
-        radians: 1,
-        inches: 250905600,
-        yards: 6969600,
-        meters: 6373000,
-        metres: 6373000,
-        centimeters: 6.373e+8,
-        centimetres: 6.373e+8,
-        kilometers: 6373,
-        kilometres: 6373,
-        feet: 20908792.65
-    };
-
+function distanceToSegment(p, segment, units, rhumb) {
     var a = segment[0];
     var b = segment[1];
 
-    var distanceAP = distance(a, p, units);
-    var bearingAP = bearing(a, p);
-    var bearingAB = bearing(a, b);
-    var relativeBearing = bearingAP - bearingAB;
-    // if relative bearing is obtuse its projection on the line extending the segment falls outside the segment
-    // thus return distance to start point
-    if (Math.abs(relativeBearing) > 90) return distanceAP;
+    var distanceAP = (rhumb !== true) ? distance(a, p, units) : rhumbDistance(a, p, units);
+    var angleAP = bearingToAngle(bearing(a, p));
+    var angleAB = bearingToAngle(bearing(a, b));
+    var angleA = Math.abs(angleAP - angleAB);
+    // if the angle PAB is obtuse its projection on the line extending the segment falls outside the segment
+    // thus return distance between P and the start point A
+    /*
+        P__
+        |\ \____
+        | \     \____
+        |  \         \____
+        |   \             \____
+        |    \                 \____
+        |     \                     \____
+        |      \_________________________\
+        H      A                          B
+     */
+    if (angleA > 90) return distanceAP;
 
-    var segmentLength = distance(a, b, units);
-    // calculate distance between p and its projection along the line extending the segment
-    var projection = Math.asin(Math.sin(distanceAP / R[units]) * Math.sin(bearingAP - bearingAB)) * R[units];
-    // calculate distance between start point to projected point on line
-    var distanceAprogection = Math.acos(Math.cos(distanceAP / R[units]) / Math.cos(projection / R[units])) * R[units];
-    // if relative bearing is acute and the projection falls outside the segment
-    // return the distance between p and the end point
-    if (distanceAprogection > segmentLength) return distance(p, b, units);
-    // else the projection falls inside the segment
-    // so return the distance between p and the segment
-    return Math.abs(projection);
+    var angleBA = (angleAB + 180) % 360;
+    var angleBP = bearingToAngle(bearing(b, p));
+    var angleB = Math.abs(angleBP - angleBA);
+    // also if the angle ABP is acute the projection of P falls outside the segment, on the other side
+    // so return the distance between P and the end point B
+    /*
+                                        __P
+                                   ____/ /|
+                              ____/     / |
+                         ____/         /  |
+                    ____/             /   |
+               ____/                 /    |
+          ____/                     /     |
+         /_________________________/      |
+        A                          B      H
+    */
+    if (angleB > 90) return (rhumb !== true) ? distance(p, b, units) : rhumbDistance(p, b, units);
+    // finally if the projection falls inside the segment
+    // return the distance between P and the segment
+    /*
+                            P
+                         __/|\
+                      __/   | \
+                   __/      |  \
+                __/         |   \
+             __/            |    \
+          __/               |     \
+         /__________________|______\
+        A                   H       B
+    */
+    // var distancePH = projectionHeight(distanceAP, angleB);
+    return distanceAP * Math.sin(degrees2radians(angleA));
+
 }
-
-// /**
-//  * (from: https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment/1501725#1501725)
-//  *
-//  */
-// function distanceToSegment(pt, segment, units) {
-//     var start = {
-//         x: segment[0][0],
-//         y: segment[0][1]
-//     };
-//     var end = {
-//         x: segment[1][0],
-//         y: segment[1][1]
-//     };
-//
-//     var length = distanceSquared(start, end);
-//     if (length === 0) throw new Error('invalid segment in line');
-//
-//     var point = {
-//         x: pt[0],
-//         y: pt[1]
-//     };
-//
-//     // Consider the line extending the segment, parametrized as
-//     //   start + t * (end - start).
-//     // The projection of p onto the line falls where
-//     //   t = [(p - start) * (end - start)] / |end - start|^2
-//     // We clamp t from [0,1] to handle points outside the segment.
-//     var t = (
-//         (point.x - start.x) * (end.x - start.x) +
-//         (point.y - start.y) * (end.y - start.y)
-//     ) / length;
-//     t = Math.max(0, Math.min(1, t));
-//
-//     var d = distanceSquared(point, {
-//         x: start.x + t * (end.x - start.x),
-//         y: start.y + t * (end.y - start.y)
-//     });
-//     // convert degrees to units
-//     return radiansToDistance(degrees2radians(Math.sqrt(d)), units);
-// }
-//
-// /**
-//  * Returns the squared distance between points
-//  *
-//  * @private
-//  * @param {object} p1 point
-//  * @param {object} p2 point
-//  * @returns {number} squared distance
-//  */
-// function distanceSquared(p1, p2) {
-//     return sqr(p1.x - p2.x) + sqr(p1.y - p2.y);
-// }
-//
-// /**
-//  * Returns the square of a number, to avoid Math.pow for performance improvement
-//  *
-//  * @private
-//  * @param {number} n number
-//  * @returns {number} square
-//  */
-// function sqr(n) {
-//     return n * n;
-// }
--- packages/turf-point-to-line-distance/test.js
+++ packages/turf-point-to-line-distance/test.js
@@ -5,6 +5,9 @@ const load = require('load-json-file');
 const write = require('write-json-file');
 const pointToLineDistance = require('./');
 
+const circle = require('@turf/circle');
+const greatCircle = require('@turf/great-circle');
+
 const directories = {
     in: path.join(__dirname, 'test', 'in') + path.sep,
     out: path.join(__dirname, 'test', 'out') + path.sep
@@ -21,9 +24,14 @@ const fixtures = fs.readdirSync(directories.in).map(filename => {
 test('turf-point-to-line-distance', t => {
     for (const {filename, name, geojson}  of fixtures) {
         const [point, line] = geojson.features;
-        let {units} = geojson.properties || {};
+        let {units, rhumb} = geojson.properties || {};
         if (!units) units = 'kilometers';
-        const distance = pointToLineDistance(point, line, units);
+        const distance = pointToLineDistance(point, line, units, rhumb);
+
+    var gc = greatCircle(line.geometry.coordinates[0], line.geometry.coordinates[1]);
+    var c = circle(point, distance, 200, units);
+    geojson.features.push(c);
+    geojson.features.push(gc);
 
         if (process.env.REGEN) write.sync(directories.out + filename, results);
         const expected = load.sync(directories.out + 'distances.json');
