2a5e73a84cecee23c7d351a55665d710c56cd385
Rowan Winsemius <rowanwins@yahoo.com.au>
2017-08-30 19:20:32 +1000

    Fix jsdoc name

--- packages/turf-boolean-within/index.js
+++ packages/turf-boolean-within/index.js
@@ -11,7 +11,7 @@ var getGeomType = invariant.getGeomType;
  * must not intersect the exterior of the secondary (geometry b).
  * Boolean-within returns the exact opposite result of the `@turf/boolean-contains`.
  *
- * @name booleanWith
+ * @name booleanWithin
  * @param {Geometry|Feature<any>} feature1 GeoJSON Feature or Geometry
  * @param {Geometry|Feature<any>} feature2 GeoJSON Feature or Geometry
  * @returns {boolean} true/false
