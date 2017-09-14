e04889d747636bc239a72a7e2bb80c4cef052ae3
stebogit <stefano.borghi@gmail.com>
2017-09-13 23:06:15 -0700

    adds tests

--- packages/turf-boolean-parallel/index.js
+++ packages/turf-boolean-parallel/index.js
@@ -1,8 +1,7 @@
 var cleanCoords = require('@turf/clean-coords');
-var bearing = require('@turf/bearing');
 var lineSegment = require('@turf/line-segment');
-var helpers = require('@turf/helpers');
-var bearingToAngle = helpers.bearingToAngle;
+var rhumbBearing = require('@turf/rhumb-bearing');
+var bearingToAngle = require('@turf/helpers').bearingToAngle;
 
 
 /**
@@ -24,33 +23,34 @@ module.exports = function (line1, line2) {
     if (!line1) throw new Error('line1 is required');
     if (!line2) throw new Error('line2 is required');
     var type1 = getType(line1, 'line1');
-    if (type1.indexOf('LineString') < 0) throw new Error('line1 must be a LineString or MultiLineString');
+    if (type1 !== 'LineString') throw new Error('line1 must be a LineString');
     var type2 = getType(line2, 'line2');
-    if (type2.indexOf('LineString') < 0) throw new Error('line2 must be a LineString or MultiLineString');
+    if (type2 !== 'LineString') throw new Error('line2 must be a LineString');
 
-    var segments1 = lineSegment(cleanCoords(line1));
-    var segments2 = lineSegment(cleanCoords(line2));
+    var segments1 = lineSegment(cleanCoords(line1)).features;
+    var segments2 = lineSegment(cleanCoords(line2)).features;
 
     for (var i = 0; i < segments1.length; i++) {
-        var segment1 = segments1[i];
-        var segment2 = segments2[i];
-        if (!segment2) break;
+        var segment1 = segments1[i].geometry.coordinates;
+        if (!segments2[i]) break;
+        var segment2 = segments2[i].geometry.coordinates;
         if (!isParallel(segment1, segment2)) return false;
     }
     return true;
 };
 
+
 /**
- * Compares slopes
+ * Compares slopes and return result
  *
  * @private
  * @param {Geometry|Feature<LineString>} segment1 Geometry or Feature
  * @param {Geometry|Feature<LineString>} segment2 Geometry or Feature
- * @returns {boolean} if segments are parallel
+ * @returns {boolean} if slopes are equal
  */
 function isParallel(segment1, segment2) {
-    var slope1 = bearingToAngle(bearing(segment1[0], segment1[1]));
-    var slope2 = bearingToAngle(bearing(segment2[0], segment2[1]));
+    var slope1 = bearingToAngle(rhumbBearing(segment1[0], segment1[1]));
+    var slope2 = bearingToAngle(rhumbBearing(segment2[0], segment2[1]));
     return slope1 === slope2;
 }
 
@@ -68,44 +68,3 @@ function getType(geojson, name) {
     if (geojson.type) return geojson.type; // if GeoJSON geometry
     throw new Error('Invalid GeoJSON type for ' + name);
 }
-
-
-// /**
-//  * Is Parallel
-//  *
-//  * @private
-//  * @param {Array<number>} a coordinates [x, y]
-//  * @param {Array<number>} b coordinates [x, y]
-//  * @returns {boolean} true if a and b are parallel (or co-linear)
-//  */
-// function isParallel(a, b) {
-//     var r = ab(a);
-//     var s = ab(b);
-//     return (crossProduct(r, s) === 0);
-// }
-//
-// /**
-//  * AB
-//  *
-//  * @private
-//  * @param {Array<Array<number>>} segment - 2 vertex line segment
-//  * @returns {Array<number>} coordinates [x, y]
-//  */
-// function ab(segment) {
-//     var start = segment[0];
-//     var end = segment[1];
-//     return [end[0] - start[0], end[1] - start[1]];
-// }
-//
-// /**
-//  * Cross Product
-//  *
-//  * @private
-//  * @param {Array<number>} v1 coordinates [x, y]
-//  * @param {Array<number>} v2 coordinates [x, y]
-//  * @returns {Array<number>} Cross Product
-//  */
-// function crossProduct(v1, v2) {
-//     return (v1[0] * v2[1]) - (v2[0] * v1[1]);
-// }
-
--- packages/turf-boolean-parallel/test.js
+++ packages/turf-boolean-parallel/test.js
@@ -2,7 +2,7 @@ const glob = require('glob');
 const path = require('path');
 const test = require('tape');
 const load = require('load-json-file');
-const {point, lineString, polygon} = require('@turf/helpers');
+const {lineString, polygon} = require('@turf/helpers');
 const parallel = require('./');
 
 test('turf-boolean-parallel', t => {
@@ -16,25 +16,30 @@ test('turf-boolean-parallel', t => {
         if (process.env.SHAPELY) shapely.contains(line1, line2).then(result => t.true(result, '[true] shapely - ' + name));
         t.true(result, '[true] ' + name);
     });
-    // // False Fixtures
-    // glob.sync(path.join(__dirname, 'test', 'false', '**', '*.geojson')).forEach(filepath => {
-    //     const {name} = path.parse(filepath);
-    //     const geojson = load.sync(filepath);
-    //     const [line1, line2] = geojson.features;
-    //     const result = parallel(line1, line2);
-    //
-    //     if (process.env.SHAPELY) shapely.contains(line1, line2).then(result => t.false(result, '[false] shapely - ' + name));
-    //     t.false(result, '[false] ' + name);
-    // });
+    // False Fixtures
+    glob.sync(path.join(__dirname, 'test', 'false', '**', '*.geojson')).forEach(filepath => {
+        const {name} = path.parse(filepath);
+        const geojson = load.sync(filepath);
+        const [line1, line2] = geojson.features;
+        const result = parallel(line1, line2);
+
+        if (process.env.SHAPELY) shapely.contains(line1, line2).then(result => t.false(result, '[false] shapely - ' + name));
+        t.false(result, '[false] ' + name);
+    });
     t.end();
 });
 
 
-// test('turf-boolean-parallel -- throws', t => {
-//     t.throws(() => overlap(null, line1), /feature1 is required/, 'missing feature1');
-//     t.throws(() => overlap(line1, null), /feature2 is required/, 'missing feature2');
-//     t.throws(() => overlap(pt, line1), /features must be of the same type/, 'different types');
-//     t.throws(() => overlap(pt, pt), /Point geometry not supported/, 'geometry not supported');
-//
-//     t.end();
-// });
+test('turf-boolean-parallel -- throws', t => {
+    const line = lineString([[0, 0], [0, 1]]);
+    const poly = polygon([[[0, 0], [0, 1], [1, 1], [0, 0]]]);
+
+    t.throws(() => parallel(null, line), /line1 is required/, 'missing line1');
+    t.throws(() => parallel(line, null), /line2 is required/, 'missing line2');
+    t.throws(() => parallel(poly, line), /line1 must be a LineString/, 'different types');
+    t.throws(() => parallel(line, poly), /line2 must be a LineString/, 'different types');
+    t.throws(() => parallel({}, line), /Invalid GeoJSON type for line1/, 'invalid types');
+    t.throws(() => parallel(line, {}), /Invalid GeoJSON type for line2/, 'invalid types');
+
+    t.end();
+});
