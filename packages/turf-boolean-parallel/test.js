43eb1b1b9b601246a1f0825956275c163e332d31
stebogit <stefano.borghi@gmail.com>
2017-09-13 23:10:46 -0700

    updates bench

--- packages/turf-boolean-parallel/test.js
+++ packages/turf-boolean-parallel/test.js
@@ -3,7 +3,7 @@ const path = require('path');
 const test = require('tape');
 const load = require('load-json-file');
 const {lineString, polygon} = require('@turf/helpers');
-const parallel = require('./');
+const booleanParallel = require('./');
 
 test('turf-boolean-parallel', t => {
     // True Fixtures
@@ -11,7 +11,7 @@ test('turf-boolean-parallel', t => {
         const {name} = path.parse(filepath);
         const geojson = load.sync(filepath);
         const [line1, line2] = geojson.features;
-        const result = parallel(line1, line2);
+        const result = booleanParallel(line1, line2);
 
         if (process.env.SHAPELY) shapely.contains(line1, line2).then(result => t.true(result, '[true] shapely - ' + name));
         t.true(result, '[true] ' + name);
@@ -21,7 +21,7 @@ test('turf-boolean-parallel', t => {
         const {name} = path.parse(filepath);
         const geojson = load.sync(filepath);
         const [line1, line2] = geojson.features;
-        const result = parallel(line1, line2);
+        const result = booleanParallel(line1, line2);
 
         if (process.env.SHAPELY) shapely.contains(line1, line2).then(result => t.false(result, '[false] shapely - ' + name));
         t.false(result, '[false] ' + name);
@@ -34,12 +34,12 @@ test('turf-boolean-parallel -- throws', t => {
     const line = lineString([[0, 0], [0, 1]]);
     const poly = polygon([[[0, 0], [0, 1], [1, 1], [0, 0]]]);
 
-    t.throws(() => parallel(null, line), /line1 is required/, 'missing line1');
-    t.throws(() => parallel(line, null), /line2 is required/, 'missing line2');
-    t.throws(() => parallel(poly, line), /line1 must be a LineString/, 'different types');
-    t.throws(() => parallel(line, poly), /line2 must be a LineString/, 'different types');
-    t.throws(() => parallel({}, line), /Invalid GeoJSON type for line1/, 'invalid types');
-    t.throws(() => parallel(line, {}), /Invalid GeoJSON type for line2/, 'invalid types');
+    t.throws(() => booleanParallel(null, line), /line1 is required/, 'missing line1');
+    t.throws(() => booleanParallel(line, null), /line2 is required/, 'missing line2');
+    t.throws(() => booleanParallel(poly, line), /line1 must be a LineString/, 'different types');
+    t.throws(() => booleanParallel(line, poly), /line2 must be a LineString/, 'different types');
+    t.throws(() => booleanParallel({}, line), /Invalid GeoJSON type for line1/, 'invalid types');
+    t.throws(() => booleanParallel(line, {}), /Invalid GeoJSON type for line2/, 'invalid types');
 
     t.end();
 });
