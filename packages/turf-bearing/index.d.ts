e284e113d6015c05e8fc0d9e81bddc2842989936
Denis <carriere.denis@gmail.com>
2017-09-25 12:33:32 -0400

    Add boolean-point-on-line

--- packages/turf-bearing/index.d.ts
+++ packages/turf-bearing/index.d.ts
@@ -2,7 +2,11 @@
 
 export type Point = GeoJSON.Feature<GeoJSON.Point> | GeoJSON.Point | number[];
 
+interface Options {
+    final?: boolean
+}
+
 /**
  * http://turfjs.org/docs/#bearing
  */
-export default function bearing(start: Point, end: Point, final?: boolean): number;
+export default function bearing(start: Point, end: Point, options?: Options): number;
