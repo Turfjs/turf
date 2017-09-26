93cf14f088611b1bb0365f138865db15eff53698
Denis <carriere.denis@gmail.com>
2017-09-26 16:00:42 -0400

    Update packages

--- scripts/update-to-es6-modules.js
+++ scripts/update-to-es6-modules.js
@@ -5,7 +5,7 @@ const load = require('load-json-file');
 const write = require('write-json-file');
 const path = require('path');
 const glob = require('glob');
-const {dependencies} = require('../packages/turf/package.json');
+// const {dependencies} = require('../packages/turf/package.json');
 
 function entries(obj) {
     return Object.keys(obj || {}).map(key => [key, obj[key]]);
@@ -34,6 +34,7 @@ function updateDevDependencies(pckg) {
     new Map(entries(pckg.devDependencies))
         .set('rollup', '*')
         .set('tape', '*')
+        .set('@std/esm', '*')
         .set('benchmark', '*').forEach((version, name) => {
             // Update dependencies to v5.0.0-alpha
             switch (name) {
@@ -51,31 +52,30 @@ function updateDevDependencies(pckg) {
 }
 
 // Update package.json
-Object.keys(dependencies).forEach(name => {
-    const basename = name.replace('@turf/', '');
-    const packagePath = path.join(__dirname, '..', 'packages', 'turf-' + basename, 'package.json');
+glob.sync(path.join(__dirname, '..', 'packages', 'turf-*', 'package.json')).forEach(packagePath => {
     const pckg = load.sync(packagePath);
     const files = new Set(pckg.files);
-    files.add('index.es5.js');
+    files.add('dist');
     files.add('index.js');
-    files.delete('dist');
+    files.add('index.d.ts');
+    files.delete('index.es5.js');
+    files.delete('index.mjs');
     files.delete('index.cjs.js');
     files.delete('index.cjs');
-    files.delete('index.mjs');
 
     const newPckg = {
         name: pckg.name,
         version: pckg.version,
         description: pckg.description,
-        main: 'index.es5.js',
-        module: 'index.js',
-        'jsnext:main': 'index.js',
-        types: pckg.types,
+        main: 'dist/index',
+        module: 'index',
+        'jsnext:main': 'index',
+        types: 'index.d.ts',
         files: [...files],
         scripts: {
             'pretest': 'rollup -c ../../rollup.config.js',
-            'test': 'node test.es5.js',
-            'bench': 'node bench.js'
+            'test': 'node -r @std/esm test.js',
+            'bench': 'node -r @std/esm bench.js'
         },
         repository: {
             type: 'git',
@@ -90,15 +90,30 @@ Object.keys(dependencies).forEach(name => {
         },
         homepage: 'https://github.com/Turfjs/turf',
         devDependencies: updateDevDependencies(pckg),
-        dependencies: updateDependencies(pckg)
+        dependencies: updateDependencies(pckg),
+        '@std/esm': {
+            esm: 'js',
+            cjs: true
+        }
     };
     write.sync(packagePath, newPckg, {indent: 2});
 });
 
-// Convert ".mjs" files to ".js"
-glob.sync(path.join(__dirname, '..', 'packages', 'turf*', '*.mjs')).forEach(filepath => {
-    var index = fs.readFileSync(filepath, 'utf8');
-    index = index.replace(/'.\/'/g, '\'.\'');
-    fs.writeFileSync(filepath.replace('.mjs', '.js'), index);
-    fs.removeSync(filepath);
-});
+// // Convert ".js" files => ".mjs"
+// glob.sync(path.join(__dirname, '..', 'packages', 'turf*', 'index.js')).forEach(filepath => {
+//     const outpath = filepath.replace('.js', '.mjs');
+//     if (fs.existsSync(outpath)) return;
+
+//     var index = fs.readFileSync(filepath, 'utf8');
+//     index = index.replace(/'\.\/'/g, '\'.\'');
+//     fs.writeFileSync(outpath, index);
+//     fs.removeSync(filepath);
+// });
+
+// // Convert ES Module index import
+// glob.sync(path.join(__dirname, '..', 'packages', 'turf*', 'test.js')).forEach(filepath => {
+
+//     var index = fs.readFileSync(filepath, 'utf8');
+//     index = index.replace(/'.'/g, '\'./index\'');
+//     fs.writeFileSync(filepath, index);
+// });
