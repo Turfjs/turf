fd281bae5b1e5a388fb6f1bf798f1b37bdbe9dfb
Denis <carriere.denis@gmail.com>
2017-10-02 15:33:24 -0400

    Update typescript tests

--- packages/turf-great-circle/types.ts
+++ packages/turf-great-circle/types.ts
@@ -1,4 +1,4 @@
-import {point} from '@turf/helpers'
+import { point } from '@turf/helpers'
 import greatCircle from './'
 
 const pt1 = point([0, 0])
@@ -6,7 +6,7 @@ const pt2 = point([60, 0])
 greatCircle(pt1, pt2)
 greatCircle(pt1.geometry, pt2.geometry)
 greatCircle(pt1.geometry.coordinates, pt2.geometry.coordinates)
-greatCircle(pt1, pt2, {'name': 'Seattle to DC'})
-greatCircle(pt1, pt2, {}, 100)
-greatCircle(pt1, pt2, {}, 100, 10)
+greatCircle(pt1, pt2, {properties: {'name': 'Seattle to DC'}})
+greatCircle(pt1, pt2, {npoints: 10})
+greatCircle(pt1, pt2, {offset: 100, npoints: 10})
 
