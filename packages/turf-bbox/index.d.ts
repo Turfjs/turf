5c354fb4cfe13102d24ec9aea98386783cb53130
Denis <carriere.denis@gmail.com>
2017-10-01 23:41:44 -0400

    Publish linestring-to-polygon & bbox

--- packages/turf-bbox/index.d.ts
+++ packages/turf-bbox/index.d.ts
@@ -1,10 +1,8 @@
-/// <reference types="geojson" />
-
-export type Feature = GeoJSON.Feature<any>;
-export type Features = GeoJSON.FeatureCollection<any>;
-export type BBox = [number, number, number, number];
+import { Feature, FeatureCollection, BBox } from '@turf/helpers'
 
 /**
  * http://turfjs.org/docs/#bbox
  */
-export default function bbox(features: Feature | Features): BBox;
+export default function bbox(
+    features: Feature<any> | FeatureCollection<any>
+): BBox;
