5ce402f759bb77e170d2452a6b8d616238da1658
Denis <carriere.denis@gmail.com>
2017-10-20 15:32:14 -0600

    Add ES6 rollup config

--- rollup.config.es6.js
+++ rollup.config.es6.js
@@ -1,12 +1,12 @@
 import typescriptExport from './rollup-plugins/typescript-export';
-import validES5 from './rollup-plugins/valid-es5';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
-export default {
+export default [{
     input: 'index.js',
     output: {
         extend: true,
         file: 'main.js',
         format: 'cjs'
     },
-    plugins: [typescriptExport(), validES5()]
-};
+    plugins: [typescriptExport(), nodeResolve()]
+}];
