05891349bf7cff4f6f74eb17a614c9d0f6e227e4
Denis <carriere.denis@gmail.com>
2017-10-20 20:31:10 -0600

    Update all Yarn lock & package.json

--- scripts/update-to-es-modules.js
+++ scripts/update-to-es-modules.js
@@ -5,77 +5,6 @@ const write = require('write-json-file');
 const path = require('path');
 const glob = require('glob');
 
-function entries(obj) {
-    return Object.keys(obj || {}).map(key => [key, obj[key]]);
-}
-
-function updateDependencies(pckg) {
-    const dependencies = {};
-    new Map(entries(pckg.dependencies))
-        .forEach((version, name) => {
-            // Update dependencies to v5.0.0
-            switch (name) {
-            case '@turf/point-on-line':
-            case '@turf/circle':
-            case '@turf/bbox':
-            case '@turf/linestring-to-polygon':
-            case '@turf/polygon-to-linestring':
-            case '@turf/point-to-line-distance':
-            case '@turf/rhumb-bearing':
-            case '@turf/rhumb-destination':
-            case '@turf/rhumb-distance':
-            case '@turf/bearing':
-            case '@turf/destination':
-            case '@turf/distance':
-            case '@turf/line-intersect':
-            case '@turf/line-segment':
-            case '@turf/helpers':
-            case '@turf/invariant':
-            case '@turf/bbox-polygon':
-            case '@turf/envelope':
-            case '@turf/inside':
-            case '@turf/polygonize':
-            case '@turf/meta':
-            case '@turf/line-overlap':
-                dependencies[name] = '^5.0.0';
-                break;
-            case 'jsts':
-                dependencies[name] = '1.4.0';
-                break;
-            case 'geojson-rbush':
-                dependencies[name] = '2.1.0';
-                break;
-            default:
-                dependencies[name] = version;
-            }
-        });
-    return dependencies;
-}
-
-function updateDevDependencies(pckg) {
-    const devDependencies = {};
-    const dev = new Map(entries(pckg.devDependencies));
-    dev.delete('rollup-plugin-uglify', '*');
-    dev.delete('uglify-js', '*');
-    dev.set('rollup', '*')
-        .set('tape', '*')
-        .set('@std/esm', '*')
-        .set('benchmark', '*').forEach((version, name) => {
-            // Update dependencies to v5.0.0-alpha
-            switch (name) {
-            case '@turf/helpers':
-            case '@turf/invariant':
-            case '@turf/meta':
-                devDependencies[name] = '*';
-                break;
-            // Change all other devDependencies to *
-            default:
-                devDependencies[name] = '*';
-            }
-        });
-    return devDependencies;
-}
-
 // Update package.json
 glob.sync(path.join(__dirname, '..', 'packages', 'turf-*', 'package.json')).forEach(packagePath => {
     const pckg = load.sync(packagePath);
@@ -117,8 +46,8 @@ glob.sync(path.join(__dirname, '..', 'packages', 'turf-*', 'package.json')).forE
             url: 'https://github.com/Turfjs/turf/issues'
         },
         homepage: 'https://github.com/Turfjs/turf',
-        devDependencies: updateDevDependencies(pckg),
-        dependencies: updateDependencies(pckg),
+        devDependencies: pckg.devDependencies,
+        dependencies: pckg.dependencies,
         '@std/esm': {
             esm: 'js',
             cjs: true
