fb112c3c666c86bdeb1baa1c99eb928228e6366f
Rowan Winsemius <rowanwins@yahoo.com.au>
2017-11-05 21:19:43 +1100

    Resolve #1008 - dissolve docs

--- packages/turf-dissolve/index.js
+++ packages/turf-dissolve/index.js
@@ -9,8 +9,8 @@ import { lineString, isObject } from '@turf/helpers';
 import { closestGreaterNumber } from './get-closest';
 
 /**
- * Dissolves a FeatureCollection of polygons, filtered by an optional property name:value.
- * Note that multipart features within the collection are not supported
+ * Dissolves a FeatureCollection of {@link polygon} features, filtered by an optional property name:value.
+ * Note that {@link mulitpolygon} features within the collection are not supported
  *
  * @name dissolve
  * @param {FeatureCollection<Polygon>} featureCollection input feature collection to be dissolved
