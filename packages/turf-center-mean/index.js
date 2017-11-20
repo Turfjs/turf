bef7d2bd7387bff816084904328179eafaefec04
Denis <carriere.denis@gmail.com>
2017-11-20 01:35:18 -0800

    Update commits

--- packages/turf-center-mean/index.js
+++ packages/turf-center-mean/index.js
@@ -42,15 +42,14 @@ function centerMean(geojson, options) {
         var weightValue = properties[weight] || 1;
         if (!isNumber(weightValue)) throw new Error('weight value must be a number for feature index ' + featureIndex);
         weightValue = Number(weightValue);
-        if (weightValue < 0) throw new Error('weight value must be positive for feature index ' + featureIndex);
-        coordEach(geom, function (coord) {
-            sumXs += coord[0] * weightValue;
-            sumYs += coord[1] * weightValue;
-            sumNs += weightValue;
-        });
+        if (weightValue > 0) {
+            coordEach(geom, function (coord) {
+                sumXs += coord[0] * weightValue;
+                sumYs += coord[1] * weightValue;
+                sumNs += weightValue;
+            });
+        }
     });
-    // if (sumNs) sumNs = 1;
-    if (sumNs === 0) throw new Error('sum of weights equals zero');
     return point([sumXs / sumNs, sumYs / sumNs], properties);
 }
 
