6e8e7eb8111a69394c5043a537b566403e994fe7
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2017-11-21 01:24:01 -0500

    Add typescript file.

---
+++ packages/turf-standard-deviational-ellipse/index.d.ts
@@ -0,0 +1,13 @@
+import { Feature, Polygon, Properties, Point } from '@turf/helpers'
+ 
+/**
+ * http://turfjs.org/docs/#standarddeviational-ellipse
+ */
+
+export default function (
+    points: Feature<Point>
+    options? :
+        properties?: Properties
+        weight?: string
+        steps?: number
+): Feature<Polygon>
