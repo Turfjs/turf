ed64ebf704eeb63c49b64f9c6c544f9ffd789101
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2017-11-25 11:26:29 -0500

    Update/Correct typescript

--- packages/turf-center-median/index.d.ts
+++ packages/turf-center-median/index.d.ts
@@ -1,10 +1,22 @@
-import { Feature, GeometryObject } from '@turf/helpers'
+import { Feature, Point, Coord, FeatureCollection } from '@turf/helpers'
  
 /**
  * http://turfjs.org/docs/#centermedian
  */
 
+export interface medianProps {
+    medianCandidates: Array<Coord>,
+    [key: string]: any
+}
+
+export interface medianCenter extends Feature<Point> {
+    properties: medianProps
+}
+
 export default function (
-    points: Feature<any> | GeometryObject,
-    tolerance: number
-): Feature<Point>;
+    features: FeatureCollection<Point>,
+    options?: {
+        weight?: string,
+        tolerance?: number
+    }
+): medianCenter;
