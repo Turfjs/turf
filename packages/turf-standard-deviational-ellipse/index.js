8ae09611b26af9548f591da9d75f9e0a04c45a07
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2017-11-24 23:22:43 -0500

    delint index

--- packages/turf-standard-deviational-ellipse/index.js
+++ packages/turf-standard-deviational-ellipse/index.js
@@ -10,16 +10,16 @@ import ellipse from '@turf/ellipse';
  * also known as a “directional distribution.” The standard deviational ellipse
  * aims to show the direction and the distribution of a dataset by drawing
  * an ellipse that contains about one standard deviation’s worth (~ 70%) of the
- * data. 
- * 
+ * data.
+ *
  * This module was created in consultation with the following articles:
  *
- * • Robert S. Yuill, “The Standard Deviational Ellipse; An Updated Tool for 
- * Spatial Description,” _Geografiska Annaler_ 53, no. 1 (1971): 28–39, 
+ * • Robert S. Yuill, “The Standard Deviational Ellipse; An Updated Tool for
+ * Spatial Description,” _Geografiska Annaler_ 53, no. 1 (1971): 28–39,
  * doi:{@link https://doi.org/10.2307/490885|10.2307/490885}.
  *
- * • Paul Hanly Furfey, “A Note on Lefever’s “Standard Deviational Ellipse,” 
- * _American Journal of Sociology_ 33, no. 1 (1927): 94—98, 
+ * • Paul Hanly Furfey, “A Note on Lefever’s “Standard Deviational Ellipse,”
+ * _American Journal of Sociology_ 33, no. 1 (1927): 94—98,
  * doi:{@link https://doi.org/10.1086/214336|10.1086/214336}.
  *
  * It mirrors the functionality of {@link http://desktop.arcgis.com/en/arcmap/10.3/tools/spatial-statistics-toolbox/directional-distribution.htm|Directional Distribution} in ArcGIS and the {@link http://arken.nmbu.no/~havatv/gis/qgisplugins/SDEllipse/|QGIS Standard Deviational Ellipse Plugin}.
@@ -36,7 +36,7 @@ import ellipse from '@turf/ellipse';
  * var bbox = [-74, 40.72, -73.98, 40.74];
  * var points = turf.randomPoint(400, {bbox: bbox});
  * var sdEllipse = turf.standardDeviationalEllipse(points, options);
- * 
+ *
  * //addToMap
  * var addToMap = [points, sdEllipse];
  *
@@ -53,7 +53,8 @@ function standardDeviationalEllipse(points, options) {
     if (!isNumber(steps)) throw new Error('steps must be a number');
     if (!isObject(properties)) throw new Error('properties must be a number');
 
-    // Calculate mean center:
+    // Calculate mean center & number of features:
+    var numberOfFeatures = coordAll(points).length;
     var meanCenter = centerMean(points, {weight: weightTerm});
 
     // Calculate angle of rotation:
@@ -67,12 +68,12 @@ function standardDeviationalEllipse(points, options) {
     var yDeviationSquaredSum = 0;
     var xyDeviationSum = 0;
 
-    featureEach(points, function(point){
-      var weight = point.properties[weightTerm] || 1;
-      var deviation = getDeviations(getCoords(point), getCoords(meanCenter)); 
-      xDeviationSquaredSum += Math.pow(deviation.x, 2) * weight;
-      yDeviationSquaredSum += Math.pow(deviation.y, 2) * weight;
-      xyDeviationSum += deviation.x * deviation.y * weight;
+    featureEach(points, function (point) {
+        var weight = point.properties[weightTerm] || 1;
+        var deviation = getDeviations(getCoords(point), getCoords(meanCenter));
+        xDeviationSquaredSum += Math.pow(deviation.x, 2) * weight;
+        yDeviationSquaredSum += Math.pow(deviation.y, 2) * weight;
+        xyDeviationSum += deviation.x * deviation.y * weight;
     });
 
     var bigA = xDeviationSquaredSum - yDeviationSquaredSum;
@@ -87,12 +88,12 @@ function standardDeviationalEllipse(points, options) {
     var sigmaXsum = 0;
     var sigmaYsum = 0;
     var weightsum = 0;
-    featureEach(points, function(point){
-      var weight = point.properties[weightTerm] || 1;
-      var deviation = getDeviations(getCoords(point), getCoords(meanCenter)); 
-      sigmaXsum += Math.pow((deviation.x * Math.cos(theta)) - (deviation.y * Math.sin(theta)), 2) * weight;
-      sigmaYsum += Math.pow((deviation.x * Math.sin(theta)) + (deviation.y * Math.cos(theta)), 2) * weight;
-      weightsum += weight;
+    featureEach(points, function (point) {
+        var weight = point.properties[weightTerm] || 1;
+        var deviation = getDeviations(getCoords(point), getCoords(meanCenter));
+        sigmaXsum += Math.pow((deviation.x * Math.cos(theta)) - (deviation.y * Math.sin(theta)), 2) * weight;
+        sigmaYsum += Math.pow((deviation.x * Math.sin(theta)) + (deviation.y * Math.cos(theta)), 2) * weight;
+        weightsum += weight;
     });
 
     var sigmaX = Math.sqrt(2 * sigmaXsum / weightsum);
@@ -105,27 +106,28 @@ function standardDeviationalEllipse(points, options) {
         meanCenterCoordinates: getCoords(meanCenter),
         semiMajorAxis: sigmaX,
         semiMinorAxis: sigmaY,
+        numberOfFeatures: numberOfFeatures,
         angle: thetaDeg,
         // eccentricity: eccentricity,
-        pctWithinEllipse: 100 * coordAll(pointsWithinEllipse).length / points.features.length
+        pctWithinEllipse: 100 * coordAll(pointsWithinEllipse).length / numberOfFeatures
     };
     theEllipse.properties.standardDeviationalEllipse = standardDeviationalEllipseProperties;
 
     return theEllipse;
-};
+}
 
 /**
  * Get x_i - X and y_i - Y
  *
  * @private
- * @param {array} coordinates Array of [x_i, y_i]
- * @param {array} center Array of [X, Y]
- * @returns {object} { x: n, y: m }
+ * @param {Array} coordinates Array of [x_i, y_i]
+ * @param {Array} center Array of [X, Y]
+ * @returns {Object} { x: n, y: m }
  */
 function getDeviations(coordinates, center) {
     return {
-      x: coordinates[0] - center[0], 
-      y: coordinates[1] - center[1]
+        x: coordinates[0] - center[0],
+        y: coordinates[1] - center[1]
     };
 }
 
