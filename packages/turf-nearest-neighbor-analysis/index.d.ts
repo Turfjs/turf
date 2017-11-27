bd66ec16ef9475b0906b1600e3da975bff192aa6
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2017-11-27 17:30:21 -0500

    Initial crack at documentation, etc.

---
+++ packages/turf-nearest-neighbor-analysis/index.d.ts
@@ -0,0 +1,30 @@
+import { FeatureCollection, Feature, Point, Polygon, Units, Properties } from '@turf/helpers';
+
+/**
+ * http://turfjs.org/docs/#nearestneighboranalysis
+ */
+
+export interface NearestNeighborStatistics {
+    observedMeanDistance: number,
+    expectedMeanDistance: number,
+    numberOfPoints: number,
+    zScore: number
+}
+ 
+export interface NearestNeighborStudyArea extends Feature<Polygon> {
+    properties: {
+        nearestNeighborStatistics: NearestNeighborStatistics,
+        [key: string]: any
+    }
+}
+
+
+export default function (
+      dataset: FeatureCollection<Point>,
+      options?: {
+          studyAreaPolygon?: Feature<Polygon>, 
+          studyAreaArea?: number,
+          units: Units,
+          properties?: Properties
+      }
+ ): NearestNeighborStudyArea;
