b618126c62d2f8708d9fc8e259b0cf15fdab0d7e
Denis <carriere.denis@gmail.com>
2017-11-27 01:36:34 -0800

    Add Babel to Rollup publish

---
+++ packages/turf/rollup.config.js
@@ -0,0 +1,31 @@
+import babel from 'rollup-plugin-babel'
+import node from 'rollup-plugin-node-resolve'
+import uglify from 'rollup-plugin-uglify'
+
+export default [{
+  input: 'index.js',
+  extend: true,
+  output: {
+    file: 'main.js',
+    format: 'cjs'
+  },
+  plugins: [node()]
+}, {
+  input: 'index.js',
+  extend: true,
+  output: {
+    file: 'turf.js',
+    format: 'umd',
+    name: 'turf'
+  },
+  plugins: [node(), babel()]
+},  {
+  input: 'index.js',
+  extend: true,
+  output: {
+    file: 'turf.min.js',
+    format: 'umd',
+    name: 'turf'
+  },
+  plugins: [node(), babel(), uglify()]
+}]
