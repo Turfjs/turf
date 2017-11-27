391ddc175df370101cc21634d8dfa9bd7f06a0f4
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2017-11-27 17:30:21 -0500

    Initial crack at documentation, etc.

---
+++ packages/turf-nearest-neighbor-analysis/index.js
@@ -0,0 +1,45 @@
+import area from '@turf/area';
+import bbox from '@turf/bbox';
+import bboxPolygon from '@turf/bbox-polygon';
+import centroid from '@turf/centroid';
+import distance from '@turf/distance';
+import nearestPoint from '@turf/nearest-point';
+import { featureEach } from '@turf/meta';
+import { featureCollection } from '@turf/helpers';
+
+/**
+ * Nearest Neighbor Analysis calculates an index based the average distances
+ * between points in the dataset, thereby providing inference as to whether the
+ * data is clustered, dispersed, or randomly distributed within the study area.
+ *
+ * It returns a {@link Feature<Polygon>} of the study area, with the results of
+ * the analysis attached as part of of the `nearestNeighborAnalysis` property
+ * of the study area's `properties`.
+ *
+ * The calculations for this function are found in:
+ *
+ * Philip J. Clark and Francis C. Evans, “Distance to Nearest Neighbor as a
+ * Measure of Spatial Relationships in Populations,” _Ecology_ 35, no. 4
+ * (1954): 445–453, doi:[10.2307/1931034](http://doi.org/10.2307/1931034).
+ *
+ * @name nearestNeighborAnalysis
+ * @param {FeatureCollection<Point>} dataset FeatureCollection of points to study
+ * @param {Object} [options={}] Optional parameters
+ * @param {Feature<Polygon} [options.studyAreaPolygon] A polygon representing the study area 
+ * @param {number} [options.studyAreaArea] The area of the study area
+ * @param {string} [options.units='kilometers'] unit of measurement for for distances and, squared, area.
+ * @param {Object} [options.properties={}] properties
+ * @returns {Feature<Polygon>} A polygon of the study area or an approximation of one.
+ * @example
+ * var bbox = [-65, 40, -63, 42];
+ * var dataset = turf.randomPoint(100, { bbox: bbox });
+ * var nearestNeighborStudyArea = turf.nearestNeighborAnalysis(dataset);
+ * 
+ * //addToMap
+ * var addToMap = [dataset, nearestNeighborStudyArea];
+ */
+function nearestNeighborAnalysis(feature1, feature2) {
+    return true;
+};
+
+export default nearestNeighborAnalysis;
