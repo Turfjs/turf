a4a4273e9185cd2962f81e2c5248c6735af72973
Denis Carriere <carriere.denis@gmail.com>
2017-11-27 18:42:54 -0800

    Update rollup config

--- packages/turf/rollup.config.js
+++ packages/turf/rollup.config.js
@@ -1,27 +1,24 @@
-import babel from 'rollup-plugin-babel'
-import node from 'rollup-plugin-node-resolve'
-import uglify from 'rollup-plugin-uglify'
-import { minify } from 'uglify-es'
+import babel from 'rollup-plugin-babel';
+import node from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
 
-export default [
-{
-  input: 'index.js',
-  extend: true,
-  output: {
-    file: 'turf.js',
-    format: 'umd',
-    name: 'turf'
-  },
-  plugins: [node(), babel()]
+export default [{
+    input: 'index.js',
+    extend: true,
+    output: {
+        file: 'turf.js',
+        format: 'umd',
+        name: 'turf'
+    },
+    plugins: [node(), babel()]
 },
 {
-  input: 'index.js',
-  extend: true,
-  output: {
-    file: 'turf.min.js',
-    format: 'umd',
-    name: 'turf'
-  },
-  plugins: [node(), babel(), uglify({}, minify)]
-}
-]
+    input: 'index.js',
+    extend: true,
+    output: {
+        file: 'turf.min.js',
+        format: 'umd',
+        name: 'turf'
+    },
+    plugins: [node(), babel(), uglify()]
+}];
