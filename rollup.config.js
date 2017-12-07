2ff9121718c5510ae5f42bd460029ac1c2216a38
Denis <carriere.denis@gmail.com>
2017-12-07 04:41:31 -0500

    Fix great-circle publishing

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
 import typescriptExport from './rollup-plugins/typescript-export';
 import validES5 from './rollup-plugins/valid-es5';
 
-export default {
+export default [{
     input: 'index.js',
     output: {
-        extend: true,
+        file: 'main.mjs',
+        format: 'es'
+    }
+}, {
+    input: 'index.js',
+    output: {
         file: 'main.js',
         format: 'cjs'
     },
     plugins: [typescriptExport(), validES5()]
-};
+}]
