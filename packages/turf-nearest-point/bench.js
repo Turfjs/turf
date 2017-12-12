76c42d06f8d97af35dcfdeee58276a3ef2d136e7
Rowan Winsemius <rowanwins@yahoo.com.au>
2017-12-12 23:12:51 +1100

    Performance improvement. Refactor tests

--- packages/turf-nearest-point/bench.js
+++ packages/turf-nearest-point/bench.js
@@ -1,14 +1,19 @@
 import fs from 'fs';
 import path from 'path';
 import Benchmark from 'benchmark';
+import { point } from '@turf/helpers';
 import nearestPoint from './';
 
-var pt = JSON.parse(fs.readFileSync(path.join(__dirname, 'test', 'pt.geojson')));
-var pts = JSON.parse(fs.readFileSync(path.join(__dirname, 'test', 'pts.geojson')));
+var pts = JSON.parse(fs.readFileSync(path.join(__dirname, 'test', 'in', 'points.json')));
 
+/**
+ * Benchmark Results
+ *
+ * turf-nearest-point x 72,623 ops/sec ±9.23% (73 runs sampled)
+ */
 var suite = new Benchmark.Suite('turf-nearest-point');
 suite
-  .add('turf-nearest-point', () => nearestPoint(pt, pts))
-  .on('cycle', e => console.log(String(e.target)))
-  .on('complete', () => {})
-  .run();
+    .add('turf-nearest-point', () => nearestPoint(point(pts.properties.targetPoint), pts))
+    .on('cycle', e => console.log(String(e.target)))
+    .on('complete', () => {})
+    .run();
