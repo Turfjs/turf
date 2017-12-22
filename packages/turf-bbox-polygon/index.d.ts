f9cce044cb1cf130511f84e012543e8961a46897
Denis <carriere.denis@gmail.com>
2017-12-21 22:34:15 -0500

    Translate BBox to Polygon

--- packages/turf-bbox-polygon/index.d.ts
+++ packages/turf-bbox-polygon/index.d.ts
@@ -1,6 +1,12 @@
-import { BBox, Feature, Polygon } from '@turf/helpers';
+import { BBox, Feature, Polygon, Properties, Id } from '@turf/helpers';
 
 /**
  * http://turfjs.org/docs/#bboxpolygon
  */
-export default function (bbox: BBox): Feature<Polygon>;
+export default function bboxPolygon<P = Properties>(
+    bbox: BBox,
+    options?: {
+        properties?: P,
+        id?: Id
+    }
+): Feature<Polygon, P>;
