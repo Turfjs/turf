14b2bc045c32f1f728206cde13e41d130d1c52ff
Denis <carriere.denis@gmail.com>
2018-01-30 11:47:22 -0500

    Reduce "strictness" for Typescript params

--- packages/turf-bbox-clip/types.ts
+++
@@ -1,7 +0,0 @@
-import {polygon, BBox} from '@turf/helpers'
-import bboxClip from './'
-
-const bbox: BBox = [0, 0, 10, 10];
-const poly = polygon([[[2, 2], [8, 4], [12, 8], [3, 7], [2, 2]]])
-
-const clipped = bboxClip(poly, bbox);
