7b70a1ed917b9854f89f16a9c84a6020b6f3fe1b
Denis <carriere.denis@gmail.com>
2018-02-03 17:51:02 -0500

    Implement bezier-spline

--- packages/turf-bezier-spline/test.js
+++ packages/turf-bezier-spline/test.js
@@ -1,10 +1,10 @@
-import fs from 'fs';
-import test from 'tape';
-import path from 'path';
-import load from 'load-json-file';
-import write from 'write-json-file';
-import { featureCollection } from '@turf/helpers';
-import bezierSpline from '.';
+const fs = require('fs');
+const test = require('tape');
+const path = require('path');
+const load = require('load-json-file');
+const write = require('write-json-file');
+const { featureCollection } = require('@turf/helpers');
+const bezierSpline = require('.').default;
 
 const directories = {
     in: path.join(__dirname, 'test', 'in') + path.sep,
