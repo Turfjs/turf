d02f2687fe0d5c8db9e5810637c57f10e035ba2a
Denis <carriere.denis@gmail.com>
2018-03-23 11:27:59 -0400

    Refactor boolean-disjoint

---
+++ packages/turf-boolean-disjoint/index.d.ts
@@ -0,0 +1,17 @@
+import { Feature, Geometry } from "@turf/helpers";
+/**
+ * Boolean-disjoint returns (TRUE) if the intersection of the two geometries is an empty set.
+ *
+ * @name booleanDisjoint
+ * @param {Geometry|Feature<any>} feature1 GeoJSON Feature or Geometry
+ * @param {Geometry|Feature<any>} feature2 GeoJSON Feature or Geometry
+ * @returns {boolean} true/false
+ * @example
+ * var point = turf.point([2, 2]);
+ * var line = turf.lineString([[1, 1], [1, 2], [1, 3], [1, 4]]);
+ *
+ * turf.booleanDisjoint(line, point);
+ * //=true
+ */
+declare function booleanDisjoint(feature1: Feature<any> | Geometry, feature2: Feature<any> | Geometry): boolean;
+export default booleanDisjoint;
