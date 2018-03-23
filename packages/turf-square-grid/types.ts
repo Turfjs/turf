e8dad5e322f2bb98f886b2436d8f8c2e919a0cc4
Denis <carriere.denis@gmail.com>
2018-03-23 11:30:39 -0400

    Refactor square-grid

--- packages/turf-square-grid/types.ts
+++
@@ -1,8 +0,0 @@
-import { BBox } from '@turf/helpers'
-import squareGrid from './'
-
-const bbox: BBox = [-95, 30, -85, 40]
-
-squareGrid(bbox, 50)
-squareGrid(bbox, 50, {units: 'miles'})
-squareGrid(bbox, 50, {units: 'miles', properties: {foo: 'bar'}})
