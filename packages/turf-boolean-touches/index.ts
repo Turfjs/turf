b208a83d8f1b4916bd8592a39af5079d678b5709
Rowan Winsemius <rowanwins@yahoo.com.au>
2018-03-28 22:34:55 +1100

    FIx docs and add readme

--- packages/turf-boolean-touches/index.ts
+++ packages/turf-boolean-touches/index.ts
@@ -5,17 +5,15 @@ import { getGeom, getType } from '@turf/invariant';
 import { Feature, Geometry, LineString } from '@turf/helpers';
 
 /**
- * Boolean-touches returns true if the first geometry is completely within the second geometry.
- * The interiors of both geometries must intersect and, the interior and boundary of the primary (geometry a)
- * must not intersect the exterior of the secondary (geometry b).
- *
+ * Boolean-touches true if none of the points common to both geometries 
+ * intersect the interiors of both geometries.
  * @name booleanTouches
  * @param {Geometry|Feature<any>} feature1 GeoJSON Feature or Geometry
  * @param {Geometry|Feature<any>} feature2 GeoJSON Feature or Geometry
  * @returns {boolean} true/false
  * @example
  * var line = turf.lineString([[1, 1], [1, 2], [1, 3], [1, 4]]);
- * var point = turf.point([1, 2]);
+ * var point = turf.point([1, 1]);
  *
  * turf.booleanTouches(point, line);
  * //=true
