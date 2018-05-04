baaa2630ab9dc2ccd4a616ab1f1d33a0fb80098a
Rowan Winsemius <rowanwins@yahoo.com.au>
2018-05-04 22:01:12 +1000

    Fix for along module to use rhumb calculations.

--- packages/turf-along/test.js
+++ packages/turf-along/test.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const test = require('tape');
 const load = require('load-json-file');
+const write = require('write-json-file');
 const { featureCollection } = require('@turf/helpers');
 const along = require('./').default;
 
@@ -17,15 +18,25 @@ test('turf-along', t => {
     const pt7 = along(line, 100, options);
     const pt8 = along(line.geometry, 0, options);
     const fc = featureCollection([pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8]);
-
+    
     fc.features.forEach(f => {
         t.ok(f);
         t.equal(f.type, 'Feature');
         t.equal(f.geometry.type, 'Point');
     });
     t.equal(fc.features.length, 8);
-    t.equal(fc.features[7].geometry.coordinates[0], pt8.geometry.coordinates[0]);
-    t.equal(fc.features[7].geometry.coordinates[1], pt8.geometry.coordinates[1]);
+    
+    if (process.env.REGEN) write.sync(path.join(__dirname, 'test', 'fixtures', 'dc-points.geojson'), fc);
+    t.deepEqual(fc, load.sync(path.join(__dirname, 'test', 'fixtures', 'dc-points.geojson')));
 
     t.end();
+});
+
+const longLine = load.sync(path.join(__dirname, 'test', 'fixtures', 'long-line.geojson'));
+
+test('turf-along-long', t => {
+  const pt1 = along(longLine.features[0], 200, {units: 'miles'});
+  t.deepEqual(pt1, longLine.features[1]);
+
+  t.end();
 });
