f746e041bc074bfbfeebc16cc6a13d9db35065b6
Steve Bennett <stevage@gmail.com>
2018-06-08 11:40:01 +1000

    Remove junk from test file.

--- packages/turf-rectangle-grid/test.js
+++ packages/turf-rectangle-grid/test.js
@@ -1,26 +1,3 @@
-/*import test from 'tape';
-import glob from 'glob';
-import path from 'path';
-import load from 'load-json-file';
-import write from 'write-json-file';
-import rectangleGrid from '.';
-
-test('turf-rectangle_grid', t => {
-  glob.sync(path.join(__dirname, 'test', 'in', '*.json')).forEach(filepath => {
-    // Define params
-    const {name} = path.parse(filepath);
-    const geojson = load.sync(filepath);
-    // etc.
-
-    const results = [];
-    const out = filepath.replace(path.join('test', 'in'), path.join('test', 'out'));
-    if (process.env.REGEN) write.sync(out, results);
-    t.deepEqual(results, load.sync(out), name);
-  });
-  t.end();
-});
-
-*/
 
 const fs = require('fs');
 const test = require('tape');
