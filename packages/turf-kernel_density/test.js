43d7d3e206c1655f5149fce9af599aed0b3d92b1
abelvm <abel.vm@gmail.com>
2018-07-30 16:33:38 +0200

    typos everywhere

--- packages/turf-kernel_density/test.js
+++ packages/turf-kernel_density/test.js
@@ -13,7 +13,7 @@ test('turf-kernel_density', t => {
 
     const processed = kernelDensity(geojson);
 
-    const results = styleResult(clustered);
+    const results = styleResult(processed);
     const out = filepath.replace(path.join('test', 'in'), path.join('test', 'out'));
     if (process.env.REGEN) write.sync(out, results);
     t.deepEqual(results, load.sync(out), name);
