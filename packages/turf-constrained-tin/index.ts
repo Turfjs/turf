ecf1eb1eeddd48fd63eeb4431848198470c79eed
Kohei Otsuka <kochizufan@gmail.com>
2019-06-06 14:10:31 +0900

    Modify README etc.

--- packages/turf-constrained-tin/index.ts
+++ packages/turf-constrained-tin/index.ts
@@ -14,7 +14,7 @@ const cdt2d = require('cdt2d');
  * and `c` representing its value at each of the points that represent the corners of the
  * triangle.
  *
- * @name tin
+ * @name constrainedTin
  * @param {FeatureCollection<Point>} points input points
  * @param {Array<Array<number>>} [edges] list of edges
  * @param {Object} [options] option switches
