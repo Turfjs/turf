53240dc03a0328a8d02d847b9df56d9cbdc646d3
Oliver Castillo <oliver.castillo@compass.com>
2019-06-19 16:30:05 -0400

    add tsconfig and type definitions file

---
+++ packages/turf-center/index.d.ts
@@ -0,0 +1,31 @@
+import { BBox, Id, AllGeoJSON, Feature, Point, Properties } from '@turf/helpers';
+/**
+ * Takes a {@link Feature} or {@link FeatureCollection} and returns the absolute center point of all features.
+ *
+ * @name center
+ * @param {GeoJSON} geojson GeoJSON to be centered
+ * @param {Object} [options={}] Optional parameters
+ * @param {Object} [options.properties={}] Translate GeoJSON Properties to Point
+ * @param {Object} [options.bbox={}] Translate GeoJSON BBox to Point
+ * @param {Object} [options.id={}] Translate GeoJSON Id to Point
+ * @returns {Feature<Point>} a Point feature at the absolute center point of all input features
+ * @example
+ * var features = turf.points([
+ *   [-97.522259, 35.4691],
+ *   [-97.502754, 35.463455],
+ *   [-97.508269, 35.463245]
+ * ]);
+ *
+ * var center = turf.center(features);
+ *
+ * //addToMap
+ * var addToMap = [features, center]
+ * center.properties['marker-size'] = 'large';
+ * center.properties['marker-color'] = '#000';
+ */
+declare function center<P = Properties>(geojson: AllGeoJSON, options?: {
+    properties?: P;
+    bbox?: BBox;
+    id?: Id;
+}): Feature<Point, P>;
+export default center;
