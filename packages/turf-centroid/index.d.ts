234c7dcc9aca3d4cab2f10b8a0ff501d2f148096
bozdoz <howaboutben+github@gmail.com>
2019-06-27 14:52:22 -0300

    Update centroid types; hopefully fix tests

--- packages/turf-centroid/index.d.ts
+++ packages/turf-centroid/index.d.ts
@@ -1,12 +1,24 @@
 import {
-    Point,
-    Polygon,
+    AllGeoJSON,
     Feature,
+    Point,
+    Properties,
 } from '@turf/helpers';
 
+
 /**
- * http://turfjs.org/docs/#centroid
+ * Takes one or more features and calculates the centroid using the mean of all vertices. 
+ * This lessens the effect of small islands and artifacts when calculating the centroid of a set of polygons.
+ *
+ * @name centroid
+ * @param {GeoJSON} GeoJSON to be centered
+ * @param {Properties} an Object that is used as the Feature's properties
+ * @returns {Feature<Point>} the centroid of the input features
+ * @example
+ * var polygon = turf.polygon([[[-81, 41], [-88, 36], [-84, 31], [-80, 33], [-77, 39], [-81, 41]]]);
+ *
+ * var centroid = turf.centroid(polygon);
  */
-declare function centroid(feature: Feature<Polygon>): Feature<Point>;
+declare function centroid(geojson: AllGeoJSON, properties?: Properties): Feature<Point>;
 
 export default centroid;
