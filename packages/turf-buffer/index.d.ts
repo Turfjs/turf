fdd8e0ae5393661ee5f3475f35b921a4c2635249
Gabriel Axel <guznik@gmail.com>
2019-08-22 13:26:03 +0300

    Buffer TypeScript definition for negative radius
    When a negative `radius` is used with `buffer` for a polygon, it is possible the result will be a multi-polygon when the original polygon is shaped like several "blobs" connected with narrow "tubes" - the tubes disappear entirely if they are thinner than radius*2 and the blobs shrink into separated polygons in a multi-polygon.

--- packages/turf-buffer/index.d.ts
+++ packages/turf-buffer/index.d.ts
@@ -21,9 +21,11 @@ interface Options {
 /**
  * http://turfjs.org/docs/#buffer
  */
-declare function buffer<Geom extends Point | LineString | Polygon>(feature: Feature<Geom>|Geom, radius?: number, options?: Options): Feature<Polygon>;
+declare function buffer<Geom extends Point | LineString>(feature: Feature<Geom>|Geom, radius?: number, options?: Options): Feature<Polygon>;
+declare function buffer<Polygon>(feature: Feature<Polygon>|Polygon, radius?: number, options?: Options): Feature<Polygon | MultiPolygon>;
 declare function buffer<Geom extends MultiPoint | MultiLineString | MultiPolygon>(feature: Feature<Geom>|Geom, radius?: number, options?: Options): Feature<MultiPolygon>;
-declare function buffer<Geom extends Point | LineString | Polygon>(feature: FeatureCollection<Geom>, radius?: number, options?: Options): FeatureCollection<Polygon>;
+declare function buffer<Geom extends Point | LineString>(feature: FeatureCollection<Geom>, radius?: number, options?: Options): FeatureCollection<Polygon>;
+declare function buffer<Polygon>(feature: FeatureCollection<Polygon>, radius?: number, options?: Options): FeatureCollection<Polygon | MultiPolygon>;
 declare function buffer<Geom extends MultiPoint | MultiLineString | MultiPolygon>(feature: FeatureCollection<Geom>, radius?: number, options?: Options): FeatureCollection<MultiPolygon>;
 declare function buffer(feature: FeatureCollection<any> | Feature<GeometryCollection> | GeometryCollection, radius?: number, options?: Options): FeatureCollection<Polygon | MultiPolygon>;
 declare function buffer(feature: Feature<any> | GeometryObject, radius?: number, options?: Options): Feature<Polygon | MultiPolygon>;
