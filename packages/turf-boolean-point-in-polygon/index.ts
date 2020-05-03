36a9e91326dbf6b3f196aee2ade5c06f06175ac4
Rowan Winsemius <rowanwins@yahoo.com.au>
2020-05-03 15:36:32 +1000

    use point-in-oplygon-hao

--- packages/turf-boolean-point-in-polygon/index.ts
+++ packages/turf-boolean-point-in-polygon/index.ts
@@ -1,3 +1,4 @@
+import pip from 'point-in-polygon-hao'
 import { BBox, Coord, Feature, MultiPolygon, Polygon, Properties } from "@turf/helpers";
 import { getCoord, getCoords, getGeom } from "@turf/invariant";
 
@@ -49,63 +50,20 @@ export default function booleanPointInPolygon<G extends Polygon | MultiPolygon, 
     if (bbox && inBBox(pt, bbox) === false) {
         return false;
     }
-    // normalize to multipolygon
-    if (type === "Polygon") {
-        polys = [polys];
+
+    if (type === 'Polygon') {
+        polys = [polys]
     }
-    let insidePoly = false;
-    for (let i = 0; i < polys.length && !insidePoly; i++) {
-        // check if it is in the outer ring first
-        if (inRing(pt, polys[i][0], options.ignoreBoundary)) {
-            let inHole = false;
-            let k = 1;
-            // check for the point in any of the holes
-            while (k < polys[i].length && !inHole) {
-                if (inRing(pt, polys[i][k], !options.ignoreBoundary)) {
-                    inHole = true;
-                }
-                k++;
-            }
-            if (!inHole) {
-                insidePoly = true;
-            }
-        }
+    let result = false
+    for (var i = 0; i < polys.length; ++i) {
+        const polyResult = pip(pt, polys[i])
+        if (polyResult === 0) return options.ignoreBoundary ? false : true
+        else if (polyResult) result = true
     }
-    return insidePoly;
+
+    return result
 }
 
-/**
- * inRing
- *
- * @private
- * @param {Array<number>} pt [x,y]
- * @param {Array<Array<number>>} ring [[x,y], [x,y],..]
- * @param {boolean} ignoreBoundary ignoreBoundary
- * @returns {boolean} inRing
- */
-function inRing(pt: number[], ring: number[][], ignoreBoundary?: boolean) {
-    let isInside = false;
-    if (ring[0][0] === ring[ring.length - 1][0] && ring[0][1] === ring[ring.length - 1][1]) {
-        ring = ring.slice(0, ring.length - 1);
-    }
-    for (let i = 0, j = ring.length - 1; i < ring.length; j = i++) {
-        const xi = ring[i][0];
-        const yi = ring[i][1];
-        const xj = ring[j][0];
-        const yj = ring[j][1];
-        const onBoundary = (pt[1] * (xi - xj) + yi * (xj - pt[0]) + yj * (pt[0] - xi) === 0) &&
-            ((xi - pt[0]) * (xj - pt[0]) <= 0) && ((yi - pt[1]) * (yj - pt[1]) <= 0);
-        if (onBoundary) {
-            return !ignoreBoundary;
-        }
-        const intersect = ((yi > pt[1]) !== (yj > pt[1])) &&
-            (pt[0] < (xj - xi) * (pt[1] - yi) / (yj - yi) + xi);
-        if (intersect) {
-            isInside = !isInside;
-        }
-    }
-    return isInside;
-}
 /**
  * inBBox
  *
