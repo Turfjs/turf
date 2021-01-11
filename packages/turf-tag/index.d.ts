8327f87226861edfe734bb86e4a6c659aec882bb
Bryce Roney <bryceroney@users.noreply.github.com>
2021-01-12 01:35:07 +1100

    [@turf/tag] Confirm support for multipolygons and add to TypeScript definitions (#1996)
    * Confirm multipolygon support for turf-tag
    
    * Update documentation
    
    Co-authored-by: mfedderly <mdfedderly@mdfedderly.com>

--- packages/turf-tag/index.d.ts
+++ packages/turf-tag/index.d.ts
@@ -1,11 +1,11 @@
-import { Point, FeatureCollection, Polygon } from "@turf/helpers";
+import { Point, FeatureCollection, Polygon, MultiPolygon } from "@turf/helpers";
 
 /**
  * http://turfjs.org/docs/#tag
  */
 export default function tag(
   points: FeatureCollection<Point>,
-  polygons: FeatureCollection<Polygon>,
+  polygons: FeatureCollection<Polygon | MultiPolygon>,
   field: string,
   outField: string
 ): FeatureCollection<Point>;
