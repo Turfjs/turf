a8ebc82f9e61b1afbaf1b176f25dc9e33d06d7ad
Rowan Winsemius <rowanwins@yahoo.com.au>
2021-06-18 15:29:36 +1000

    Use non-haversine distance calc

--- packages/turf-rectangle-grid/index.ts
+++ packages/turf-rectangle-grid/index.ts
@@ -2,6 +2,7 @@ import intersect from "@turf/boolean-intersects";
 import distance from "@turf/distance";
 import {
   BBox,
+  convertLength,
   Feature,
   featureCollection,
   FeatureCollection,
@@ -54,17 +55,14 @@ function rectangleGrid<P = Properties>(
   const east = bbox[2];
   const north = bbox[3];
 
-  const xFraction = cellWidth / distance([west, south], [east, south], options);
-  const cellWidthDeg = xFraction * (east - west);
-  const yFraction =
-    cellHeight / distance([west, south], [west, north], options);
-  const cellHeightDeg = yFraction * (north - south);
-
-  // rows & columns
   const bboxWidth = east - west;
+  const cellWidthDeg = convertLength(cellWidth, options.units, "degrees");
+
   const bboxHeight = north - south;
-  const columns = Math.floor(bboxWidth / cellWidthDeg);
-  const rows = Math.floor(bboxHeight / cellHeightDeg);
+  const cellHeightDeg = convertLength(cellHeight, options.units, "degrees");
+
+  const columns = Math.floor(Math.abs(bboxWidth) / cellWidthDeg);
+  const rows = Math.floor(Math.abs(bboxHeight) / cellHeightDeg);
 
   // if the grid does not fill the bbox perfectly, center it.
   const deltaX = (bboxWidth - columns * cellWidthDeg) / 2;
