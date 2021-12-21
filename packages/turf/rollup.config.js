b3090ec55d1d0c14899ff45cb6ccad16451b85c1
James <jamesmilner22@hotmail.com>
2021-12-21 22:42:27 +0000

    Use buble to prohibit ES6 code ending up in turf.min.js

--- packages/turf/rollup.config.js
+++ packages/turf/rollup.config.js
@@ -2,6 +2,7 @@ import node from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import base from "../../rollup.config";
+import buble from "@rollup/plugin-buble";
 
 const pckg = require("./package.json");
 const input = "index.mjs";
@@ -11,6 +12,6 @@ export default [
   {
     input,
     output: [{ file: pckg.browser, format: "umd", name: "turf" }],
-    plugins: [commonjs(), node(), terser()],
+    plugins: [commonjs(), node(), buble(), terser()],
   },
 ];
