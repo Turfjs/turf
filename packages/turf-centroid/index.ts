0849c61cd10eb70ffca6299bcac1f1e14eb71bf4
Steve Bennett <stevage@gmail.com>
2022-01-11 22:10:18 +1100

    Update centroid doc (#2213)
    Fixes #2212
    
    Co-authored-by: Rowan Winsemius <rowanwins@yahoo.com.au>

--- packages/turf-centroid/index.ts
+++ packages/turf-centroid/index.ts
@@ -3,14 +3,13 @@ import { point, AllGeoJSON } from "@turf/helpers";
 import { coordEach } from "@turf/meta";
 
 /**
- * Takes one or more features and calculates the centroid using the mean of all vertices.
- * This lessens the effect of small islands and artifacts when calculating the centroid of a set of polygons.
+ * Computes the centroid as the mean of all vertices within the object.
  *
  * @name centroid
  * @param {GeoJSON} geojson GeoJSON to be centered
  * @param {Object} [options={}] Optional Parameters
  * @param {Object} [options.properties={}] an Object that is used as the {@link Feature}'s properties
- * @returns {Feature<Point>} the centroid of the input features
+ * @returns {Feature<Point>} the centroid of the input object
  * @example
  * var polygon = turf.polygon([[[-81, 41], [-88, 36], [-84, 31], [-80, 33], [-77, 39], [-81, 41]]]);
  *
