8c5ead7d515655bc7462fa5240f0f2851e21d810
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-09-08 22:29:58 +0530

    Update geojson-polygon-self-intersections.js

--- packages/turf-unkink-polygon/lib/geojson-polygon-self-intersections.js
+++ packages/turf-unkink-polygon/lib/geojson-polygon-self-intersections.js
@@ -6,34 +6,34 @@ export default function (feature, filterFn, useSpatialIndex) {
     throw new Error("The input feature must be a Polygon");
   if (useSpatialIndex === undefined) useSpatialIndex = 1;
 
-  var coord = feature.geometry.coordinates;
+  let coord = feature.geometry.coordinates;
 
-  var output = [];
-  var seen = {};
+  let output = [];
+  let seen = {};
 
   if (useSpatialIndex) {
-    var allEdgesAsRbushTreeItems = [];
-    for (var ring0 = 0; ring0 < coord.length; ring0++) {
-      for (var edge0 = 0; edge0 < coord[ring0].length - 1; edge0++) {
+    let allEdgesAsRbushTreeItems = [];
+    for (let ring0 = 0; ring0 < coord.length; ring0++) {
+      for (let edge0 = 0; edge0 < coord[ring0].length - 1; edge0++) {
         allEdgesAsRbushTreeItems.push(rbushTreeItem(ring0, edge0));
       }
     }
-    var tree = new rbush();
+    let tree = new rbush();
     tree.load(allEdgesAsRbushTreeItems);
   }
 
-  for (var ringA = 0; ringA < coord.length; ringA++) {
-    for (var edgeA = 0; edgeA < coord[ringA].length - 1; edgeA++) {
+  for (let ringA = 0; ringA < coord.length; ringA++) {
+    for (let edgeA = 0; edgeA < coord[ringA].length - 1; edgeA++) {
       if (useSpatialIndex) {
-        var bboxOverlaps = tree.search(rbushTreeItem(ringA, edgeA));
+        let bboxOverlaps = tree.search(rbushTreeItem(ringA, edgeA));
         bboxOverlaps.forEach(function (bboxIsect) {
-          var ring1 = bboxIsect.ring;
-          var edge1 = bboxIsect.edge;
+          let ring1 = bboxIsect.ring;
+          let edge1 = bboxIsect.edge;
           ifIsectAddToOutput(ringA, edgeA, ring1, edge1);
         });
       } else {
-        for (var ring1 = 0; ring1 < coord.length; ring1++) {
-          for (var edge1 = 0; edge1 < coord[ring1].length - 1; edge1++) {
+        for (let ring1 = 0; ring1 < coord.length; ring1++) {
+          for (let edge1 = 0; edge1 < coord[ring1].length - 1; edge1++) {
             // TODO: speedup possible if only interested in unique: start last two loops at ringA and edgeA+1
             ifIsectAddToOutput(ringA, edgeA, ring1, edge1);
           }
@@ -51,16 +51,16 @@ export default function (feature, filterFn, useSpatialIndex) {
 
   // Function to check if two edges intersect and add the intersection to the output
   function ifIsectAddToOutput(ring0, edge0, ring1, edge1) {
-    var start0 = coord[ring0][edge0];
-    var end0 = coord[ring0][edge0 + 1];
-    var start1 = coord[ring1][edge1];
-    var end1 = coord[ring1][edge1 + 1];
+    let start0 = coord[ring0][edge0];
+    let end0 = coord[ring0][edge0 + 1];
+    let start1 = coord[ring1][edge1];
+    let end1 = coord[ring1][edge1 + 1];
 
-    var isect = intersect(start0, end0, start1, end1);
+    let isect = intersect(start0, end0, start1, end1);
 
     if (isect === null) return; // discard parallels and coincidence
-    var frac0;
-    var frac1;
+    let frac0;
+    let frac1;
     if (end0[0] !== start0[0]) {
       frac0 = (isect[0] - start0[0]) / (end0[0] - start0[0]);
     } else {
@@ -73,8 +73,8 @@ export default function (feature, filterFn, useSpatialIndex) {
     }
     if (frac0 >= 1 || frac0 <= 0 || frac1 >= 1 || frac1 <= 0) return; // require segment intersection
 
-    var key = isect;
-    var unique = !seen[key];
+    let key = isect;
+    let unique = !seen[key];
     if (unique) {
       seen[key] = true;
     }
@@ -103,12 +103,12 @@ export default function (feature, filterFn, useSpatialIndex) {
 
   // Function to return a rbush tree item given an ring and edge number
   function rbushTreeItem(ring, edge) {
-    var start = coord[ring][edge];
-    var end = coord[ring][edge + 1];
-    var minX;
-    var maxX;
-    var minY;
-    var maxY;
+    let start = coord[ring][edge];
+    let end = coord[ring][edge + 1];
+    let minX;
+    let maxX;
+    let minY;
+    let maxY;
     if (start[0] < end[0]) {
       minX = start[0];
       maxX = end[0];
@@ -143,7 +143,7 @@ function intersect(start0, end0, start1, end1) {
     equalArrays(end1, start1)
   )
     return null;
-  var x0 = start0[0],
+  let x0 = start0[0],
     y0 = start0[1],
     x1 = end0[0],
     y1 = end0[1],
@@ -151,11 +151,11 @@ function intersect(start0, end0, start1, end1) {
     y2 = start1[1],
     x3 = end1[0],
     y3 = end1[1];
-  var denom = (x0 - x1) * (y2 - y3) - (y0 - y1) * (x2 - x3);
+  let denom = (x0 - x1) * (y2 - y3) - (y0 - y1) * (x2 - x3);
   if (denom === 0) return null;
-  var x4 =
+  let x4 =
     ((x0 * y1 - y0 * x1) * (x2 - x3) - (x0 - x1) * (x2 * y3 - y2 * x3)) / denom;
-  var y4 =
+  let y4 =
     ((x0 * y1 - y0 * x1) * (y2 - y3) - (y0 - y1) * (x2 * y3 - y2 * x3)) / denom;
   return [x4, y4];
 }
@@ -168,7 +168,7 @@ function equalArrays(array1, array2) {
   // compare lengths - can save a lot of time
   if (array1.length !== array2.length) return false;
 
-  for (var i = 0, l = array1.length; i < l; i++) {
+  for (let i = 0, l = array1.length; i < l; i++) {
     // Check if we have nested arrays
     if (array1[i] instanceof Array && array2[i] instanceof Array) {
       // recurse into the nested arrays
