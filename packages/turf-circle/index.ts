9f91c98379d2846afecb11bf667f7a4d84eeac10
FinnosMauritz <mouiylus@gmail.com>
2023-10-19 16:42:17 +0300

    🛠 Add Circle Options

--- packages/turf-circle/index.ts
+++ packages/turf-circle/index.ts
@@ -1,6 +1,6 @@
-import { GeoJsonProperties, Feature, Point, Polygon } from "geojson";
+import { GeoJsonProperties, Feature, Point, Polygon, BBox } from "geojson";
 import destination from "@turf/destination";
-import { polygon, Units } from "@turf/helpers";
+import { Id, polygon, Units } from "@turf/helpers";
 
 /**
  * Takes a {@link Point} and calculates the circle polygon given a radius in degrees, radians, miles, or kilometers; and steps for precision.
@@ -12,15 +12,9 @@ import { polygon, Units } from "@turf/helpers";
  * @param {number} [options.steps=64] number of steps
  * @param {string} [options.units='kilometers'] miles, kilometers, degrees, or radians
  * @param {Object} [options.properties={}] properties
+ * @param {Array<number>} [options.bbox] Bounding Box Array [west, south, east, north] associated with the Feature
+ * @param {string|number} [options.id] Identifier associated with the Feature
  * @returns {Feature<Polygon>} circle polygon
- * @example
- * var center = [-75.343, 39.984];
- * var radius = 5;
- * var options = {steps: 10, units: 'kilometers', properties: {foo: 'bar'}};
- * var circle = turf.circle(center, radius, options);
- *
- * //addToMap
- * var addToMap = [turf.point(center), circle]
  */
 function circle<P = GeoJsonProperties>(
   center: number[] | Point | Feature<Point, P>,
@@ -29,27 +23,45 @@ function circle<P = GeoJsonProperties>(
     steps?: number;
     units?: Units;
     properties?: P;
+    bbox?: BBox;
+    id?: Id
   } = {}
 ): Feature<Polygon, P> {
   // default params
   const steps = options.steps || 64;
-  const properties: any = options.properties
-    ? options.properties
-    : !Array.isArray(center) && center.type === "Feature" && center.properties
-    ? center.properties
-    : {};
+  const stepAngle = -360 / steps;
 
-  // main
+  let properties: P | undefined = options.properties;
+  let bboxValue: BBox | undefined;
+  let idValue: Id | undefined;
+
+  if (!Array.isArray(center) && center.type === "Feature") {
+    properties = properties || center.properties;
+    bboxValue = center.bbox;
+    idValue = center.id;
+  }
+
+  bboxValue = bboxValue || options.bbox;
+  idValue = idValue || options.id;
+
+  let _options: { bbox?: BBox; id?: Id; } | undefined;
+  if (bboxValue || idValue) {
+    _options = {
+      ...(bboxValue ? { bbox: bboxValue } : {}),
+      ...(idValue ? { id: idValue } : {})
+    };
+  }
+
+  // Calculate circle coordinates
   const coordinates = [];
   for (let i = 0; i < steps; i++) {
     coordinates.push(
-      destination(center, radius, (i * -360) / steps, options).geometry
-        .coordinates
+      destination(center, radius, i * stepAngle, options).geometry.coordinates
     );
   }
   coordinates.push(coordinates[0]);
 
-  return polygon([coordinates], properties);
+  return polygon([coordinates], properties, _options);
 }
 
 export default circle;
