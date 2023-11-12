4057efbbd25ee7cf350ed05134c71afaaeba9d8d
James Beard <james@smallsaucepan.com>
2023-11-12 11:08:17 +1100

    Converting turf-ellipse to typescript.

--- packages/turf-ellipse/index.ts
+++ packages/turf-ellipse/index.ts
@@ -1,7 +1,15 @@
-import { degreesToRadians, polygon, isObject, isNumber } from "@turf/helpers";
+import {
+  degreesToRadians,
+  polygon,
+  isObject,
+  isNumber,
+  Coord,
+  Units,
+} from "@turf/helpers";
 import rhumbDestination from "@turf/rhumb-destination";
 import transformRotate from "@turf/transform-rotate";
 import { getCoord } from "@turf/invariant";
+import { GeoJsonProperties, Feature, Polygon } from "geojson";
 
 /**
  * Takes a {@link Point} and calculates the ellipse polygon given two semi-axes expressed in variable units and steps for precision.
@@ -11,7 +19,7 @@ import { getCoord } from "@turf/invariant";
  * @param {number} ySemiAxis semi (minor) axis of the ellipse along the y-axis
  * @param {Object} [options={}] Optional parameters
  * @param {number} [options.angle=0] angle of rotation in decimal degrees, positive clockwise
- * @param {Coord} [options.pivot='origin'] point around which the rotation will be performed
+ * @param {Coord} [options.pivot=center] point around which any rotation will be performed
  * @param {number} [options.steps=64] number of steps
  * @param {string} [options.units='kilometers'] unit of measurement for axes
  * @param {Object} [options.properties={}] properties
@@ -25,14 +33,25 @@ import { getCoord } from "@turf/invariant";
  * //addToMap
  * var addToMap = [turf.point(center), ellipse]
  */
-function ellipse(center, xSemiAxis, ySemiAxis, options) {
+function ellipse(
+  center: Coord,
+  xSemiAxis: number,
+  ySemiAxis: number,
+  options: {
+    steps?: number;
+    units?: Units;
+    angle?: number;
+    pivot?: Coord;
+    properties?: GeoJsonProperties;
+  }
+): Feature<Polygon> {
   // Optional params
   options = options || {};
-  var steps = options.steps || 64;
-  var units = options.units || "kilometers";
-  var angle = options.angle || 0;
-  var pivot = options.pivot || center;
-  var properties = options.properties || center.properties || {};
+  const steps = options.steps || 64;
+  const units = options.units || "kilometers";
+  const angle = options.angle || 0;
+  const pivot = options.pivot || center;
+  const properties = options.properties || {};
 
   // validation
   if (!center) throw new Error("center is required");
@@ -42,26 +61,24 @@ function ellipse(center, xSemiAxis, ySemiAxis, options) {
   if (!isNumber(steps)) throw new Error("steps must be a number");
   if (!isNumber(angle)) throw new Error("angle must be a number");
 
-  var centerCoords = getCoord(center);
-  if (units === "degrees") {
-    var angleRad = degreesToRadians(angle);
-  } else {
-    xSemiAxis = rhumbDestination(center, xSemiAxis, 90, { units: units });
-    ySemiAxis = rhumbDestination(center, ySemiAxis, 0, { units: units });
-    xSemiAxis = getCoord(xSemiAxis)[0] - centerCoords[0];
-    ySemiAxis = getCoord(ySemiAxis)[1] - centerCoords[1];
+  const centerCoords = getCoord(center);
+  if (units !== "degrees") {
+    const xDest = rhumbDestination(center, xSemiAxis, 90, { units });
+    const yDest = rhumbDestination(center, ySemiAxis, 0, { units });
+    xSemiAxis = getCoord(xDest)[0] - centerCoords[0];
+    ySemiAxis = getCoord(yDest)[1] - centerCoords[1];
   }
 
-  var coordinates = [];
-  for (var i = 0; i < steps; i += 1) {
-    var stepAngle = (i * -360) / steps;
-    var x =
+  const coordinates: number[][] = [];
+  for (let i = 0; i < steps; i += 1) {
+    const stepAngle = (i * -360) / steps;
+    let x =
       (xSemiAxis * ySemiAxis) /
       Math.sqrt(
         Math.pow(ySemiAxis, 2) +
           Math.pow(xSemiAxis, 2) * Math.pow(getTanDeg(stepAngle), 2)
       );
-    var y =
+    let y =
       (xSemiAxis * ySemiAxis) /
       Math.sqrt(
         Math.pow(xSemiAxis, 2) +
@@ -71,8 +88,9 @@ function ellipse(center, xSemiAxis, ySemiAxis, options) {
     if (stepAngle < -90 && stepAngle >= -270) x = -x;
     if (stepAngle < -180 && stepAngle >= -360) y = -y;
     if (units === "degrees") {
-      var newx = x * Math.cos(angleRad) + y * Math.sin(angleRad);
-      var newy = y * Math.cos(angleRad) - x * Math.sin(angleRad);
+      const angleRad = degreesToRadians(angle);
+      const newx = x * Math.cos(angleRad) + y * Math.sin(angleRad);
+      const newy = y * Math.cos(angleRad) - x * Math.sin(angleRad);
       x = newx;
       y = newy;
     }
@@ -84,7 +102,7 @@ function ellipse(center, xSemiAxis, ySemiAxis, options) {
     return polygon([coordinates], properties);
   } else {
     return transformRotate(polygon([coordinates], properties), angle, {
-      pivot: pivot,
+      pivot,
     });
   }
 }
@@ -96,8 +114,8 @@ function ellipse(center, xSemiAxis, ySemiAxis, options) {
  * @param {number} deg Degrees
  * @returns {number} Tan Degrees
  */
-function getTanDeg(deg) {
-  var rad = (deg * Math.PI) / 180;
+function getTanDeg(deg: number) {
+  const rad = (deg * Math.PI) / 180;
   return Math.tan(rad);
 }
 
