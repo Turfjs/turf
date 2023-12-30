f207b05e8f034b54e6f5fce10aa58bf05d31e9e0
Matthew Fedderly <mdfedderly@mdfedderly.com>
2023-12-30 12:26:28 -0500

    Fix issues with prerelease builds
    - Added a fetch-depth: 0 config that should give us the git history and correctly calculate the canary versions
    - Don't run scripts as part of the lerna release step, they were already run by the github actions steps
    - Fix issue with reference to the node 'util' package that was preventing the browser version of @turf/turf from working

--- packages/turf/rollup.config.js
+++ packages/turf/rollup.config.js
@@ -2,6 +2,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
 import { babel } from "@rollup/plugin-babel";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 const pckg = require("./package.json");
 const input = "index.ts";
@@ -13,6 +14,7 @@ export default [
     plugins: [
       commonjs(),
       nodeResolve(),
+      nodePolyfills(),
       babel({ babelHelpers: "bundled" }),
       terser(),
     ],
