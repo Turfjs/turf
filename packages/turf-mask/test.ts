9196b3efdbffc54dc7b1fd0279fb3c83f582109e
Mark Farnum <mark.farnum@propagateag.com>
2024-06-26 11:38:25 -0400

    Add a new test for the behavior we want

--- packages/turf-mask/test.ts
+++ packages/turf-mask/test.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 import { loadJsonFileSync } from "load-json-file";
 import { writeJsonFileSync } from "write-json-file";
 import { mask } from "./index.js";
+import { clone } from "../turf-clone/index.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -36,5 +37,21 @@ test("turf-mask", (t) => {
       writeJsonFileSync(directories.out + filename, results);
     t.deepEquals(results, loadJsonFileSync(directories.out + filename), name);
   }
+  t.end();
+});
+
+test("turf-mask -- doesn't mutate inputs", (t) => {
+  const basicFixture = fixtures.find(
+    ({ filename }) => filename === "basic.geojson"
+  );
+  if (!basicFixture) throw new Error("basic.geojson not found");
+
+  const [polygon, masking] = basicFixture.geojson.features;
+  const maskClone = clone(masking);
+
+  mask(polygon, masking);
+
+  t.deepEquals(masking, maskClone, "mask input should not be mutated");
+
   t.end();
 });
