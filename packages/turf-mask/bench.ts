cf6a856de9e1545e82c1415af96f657fc83d3873
James Beard <james@smallsaucepan.com>
2024-07-08 00:46:43 +1000

    Converted turf-mask to Typescript. Fixed an apparently unreported bug where the typings suggested it was ok to pass a Polygon or MultiPolygon Geometry (rather than a complete Feature), but the code would bomb out.

--- packages/turf-mask/bench.ts
+++ packages/turf-mask/bench.ts
@@ -1,8 +1,9 @@
+import { Feature, FeatureCollection, Polygon, MultiPolygon } from "geojson";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { loadJsonFileSync } from "load-json-file";
-import Benchmark from "benchmark";
+import Benchmark, { Event } from "benchmark";
 import { mask as turfMask } from "./index.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -17,13 +18,15 @@ const directories = {
 let fixtures = fs.readdirSync(directories.in).map((filename) => {
   return {
     name: path.parse(filename).name,
-    geojson: loadJsonFileSync(path.join(directories.in, filename)),
+    geojson: loadJsonFileSync(
+      path.join(directories.in, filename)
+    ) as FeatureCollection<Polygon | MultiPolygon>,
   };
 });
 
 for (const { name, geojson } of fixtures) {
   const [polygon, masking] = geojson.features;
-  suite.add(name, () => turfMask(polygon, masking));
+  suite.add(name, () => turfMask(polygon, masking as Feature<Polygon>));
 }
 
 // basic x 4,627 ops/sec ±25.23% (21 runs sampled)
@@ -31,7 +34,7 @@ for (const { name, geojson } of fixtures) {
 // multi-polygon x 5,837 ops/sec ±3.03% (91 runs sampled)
 // overlapping x 22,326 ops/sec ±1.34% (90 runs sampled)
 suite
-  .on("cycle", (event) => {
+  .on("cycle", (event: Event) => {
     console.log(String(event.target));
   })
   .run();
--- packages/turf-mask/index.ts
+++ packages/turf-mask/index.ts
@@ -1,32 +1,54 @@
+import {
+  Feature,
+  FeatureCollection,
+  Polygon,
+  Position,
+  MultiPolygon,
+} from "geojson";
 import { polygon as createPolygon, multiPolygon } from "@turf/helpers";
-import polygonClipping from "polygon-clipping";
+import polygonClipping, { Geom } from "polygon-clipping";
 
 /**
- * Takes any type of {@link Polygon|polygon} and an optional mask and returns a {@link Polygon|polygon} exterior ring with holes.
+ * Takes polygons or multipolygons and an optional mask, and returns an exterior
+ * ring polygon with holes.
  *
  * @name mask
- * @param {FeatureCollection|Feature<Polygon|MultiPolygon>} polygon GeoJSON Polygon used as interior rings or holes.
- * @param {Feature<Polygon>} [mask] GeoJSON Polygon used as the exterior ring (if undefined, the world extent is used)
+ * @param {Polygon|MultiPolygon|Feature<Polygon|MultiPolygon>|FeatureCollection<Polygon|MultiPolygon>} polygon GeoJSON polygon used as interior rings or holes
+ * @param {Polygon|Feature<Polygon>} [mask] GeoJSON polygon used as the exterior ring (if undefined, the world extent is used)
  * @returns {Feature<Polygon>} Masked Polygon (exterior ring with holes).
  * @example
- * var polygon = turf.polygon([[[112, -21], [116, -36], [146, -39], [153, -24], [133, -10], [112, -21]]]);
- * var mask = turf.polygon([[[90, -55], [170, -55], [170, 10], [90, 10], [90, -55]]]);
+ * const polygon = turf.polygon([[[112, -21], [116, -36], [146, -39], [153, -24], [133, -10], [112, -21]]]);
+ * const mask = turf.polygon([[[90, -55], [170, -55], [170, 10], [90, 10], [90, -55]]]);
  *
- * var masked = turf.mask(polygon, mask);
+ * const masked = turf.mask(polygon, mask);
  *
  * //addToMap
- * var addToMap = [masked]
+ * const addToMap = [masked]
  */
-function mask(polygon, mask) {
+function mask<T extends Polygon | MultiPolygon>(
+  polygon: T | Feature<T> | FeatureCollection<T>,
+  mask?: Polygon | Feature<Polygon>
+): Feature<Polygon> {
   // Define mask
-  var maskPolygon = createMask(mask);
+  const maskPolygon = createMask(mask);
 
   var polygonOuters = null;
-  if (polygon.type === "FeatureCollection") polygonOuters = unionFc(polygon);
-  else
+  if (polygon.type === "FeatureCollection") {
+    polygonOuters = unionFc(polygon);
+  } else if (polygon.type === "Feature") {
+    // Need to cast below as Position[][] isn't quite as strict as Geom, even
+    // though they should be equivalent.
     polygonOuters = createGeomFromPolygonClippingOutput(
-      polygonClipping.union(polygon.geometry.coordinates)
+      polygonClipping.union(polygon.geometry.coordinates as Geom)
     );
+  } else {
+    // Geometry
+    // Need to cast below as Position[][] isn't quite as strict as Geom, even
+    // though they should be equivalent.
+    polygonOuters = createGeomFromPolygonClippingOutput(
+      polygonClipping.union(polygon.coordinates as Geom)
+    );
+  }
 
   polygonOuters.geometry.coordinates.forEach(function (contour) {
     maskPolygon.geometry.coordinates.push(contour[0]);
@@ -35,23 +57,24 @@ function mask(polygon, mask) {
   return maskPolygon;
 }
 
-function unionFc(fc) {
-  var unioned =
+function unionFc(fc: FeatureCollection<Polygon | MultiPolygon>) {
+  // Need to cast below as Position[][] isn't quite as strict as Geom, even
+  // though they should be equivalent.
+  const unioned =
     fc.features.length === 2
       ? polygonClipping.union(
-          fc.features[0].geometry.coordinates,
-          fc.features[1].geometry.coordinates
+          fc.features[0].geometry.coordinates as Geom,
+          fc.features[1].geometry.coordinates as Geom
         )
-      : polygonClipping.union.apply(
-          polygonClipping,
-          fc.features.map(function (f) {
+      : polygonClipping.union(
+          ...(fc.features.map(function (f) {
             return f.geometry.coordinates;
-          })
+          }) as [Geom, ...Geom[]])
         );
   return createGeomFromPolygonClippingOutput(unioned);
 }
 
-function createGeomFromPolygonClippingOutput(unioned) {
+function createGeomFromPolygonClippingOutput(unioned: Position[][][]) {
   return multiPolygon(unioned);
 }
 
@@ -60,9 +83,9 @@ function createGeomFromPolygonClippingOutput(unioned) {
  *
  * @private
  * @param {Feature<Polygon>} [mask] default to world if undefined
- * @returns {Feature<Polygon>} mask coordinate
+ * @returns {Feature<Polygon>} mask as a polygon
  */
-function createMask(mask) {
+function createMask(mask: Feature<Polygon> | Polygon | undefined) {
   var world = [
     [
       [180, 90],
@@ -72,7 +95,16 @@ function createMask(mask) {
       [180, 90],
     ],
   ];
-  var coordinates = (mask && mask.geometry.coordinates) || world;
+  let coordinates = world;
+  if (mask) {
+    if (mask.type === "Feature") {
+      // polygon feature
+      coordinates = mask.geometry.coordinates;
+    } else {
+      // polygon geometry
+      coordinates = mask.coordinates;
+    }
+  }
   return createPolygon(coordinates);
 }
 
