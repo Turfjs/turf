b8701ac2a9e6bd6099fec08471663879e9ca4c6c
James Beard <james@smallsaucepan.com>
2024-09-15 19:18:42 +1000

    Fixed #2703. turf-angle was not doing what was described in the docs. Reviewed the implementations of azimuthToBearing, radiansToDegrees, and degreesToRadians to be easier to follow. Small discrepancies in the angle() geojson test fixtures caused by the bearing be taken from the wrong end of each line needed to be fixed. These tests included additional illustrative info meaning they were overly fragile though, so retired those in favour of vanilla unit tests which should be sufficient for a simple function like angle.

--- packages/turf-angle/index.ts
+++ packages/turf-angle/index.ts
@@ -49,19 +49,23 @@ function angle(
   const B = endPoint;
 
   // Main
-  const azimuthAO = bearingToAzimuth(
-    options.mercator !== true ? bearing(A, O) : rhumbBearing(A, O)
+  const azimuthOA = bearingToAzimuth(
+    options.mercator !== true ? bearing(O, A) : rhumbBearing(O, A)
   );
-  const azimuthBO = bearingToAzimuth(
-    options.mercator !== true ? bearing(B, O) : rhumbBearing(B, O)
+  let azimuthOB = bearingToAzimuth(
+    options.mercator !== true ? bearing(O, B) : rhumbBearing(O, B)
   );
-  const angleAO = Math.abs(azimuthAO - azimuthBO);
+  // If OB "trails" OA advance OB one revolution so we get the clockwise angle.
+  if (azimuthOB < azimuthOA) {
+    azimuthOB = azimuthOB + 360;
+  }
+  const angleAOB = azimuthOB - azimuthOA;
 
   // Explementary angle
   if (options.explementary === true) {
-    return 360 - angleAO;
+    return 360 - angleAOB;
   }
-  return angleAO;
+  return angleAOB;
 }
 
 export { angle };
