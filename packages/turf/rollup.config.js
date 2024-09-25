aabc39aacfb734c9f92f82ea5e547959485b6169
Matt Fedderly <mfedderly@palantir.com>
2024-09-25 11:47:37 -0400

    Fix issues from rollup 2->3 upgrade

--- packages/turf/rollup.config.js
+++ packages/turf/rollup.config.js
@@ -1,10 +1,11 @@
-import nodeResolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import terser from "@rollup/plugin-terser";
 import { babel } from "@rollup/plugin-babel";
+import { readFileSync } from "fs";
+import commonjs from "@rollup/plugin-commonjs";
 import nodePolyfills from "rollup-plugin-polyfill-node";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 
-const pckg = require("./package.json");
+const pckg = JSON.parse(readFileSync("./package.json", "utf-8"));
 const input = "index.ts";
 
 export default [
