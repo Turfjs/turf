b9562c9b28fe68ee2689e6e6cd563398408afb05
SeHyun <43470398+tpgus@users.noreply.github.com>
2024-10-25 23:12:53 +0900

    Unused functions have been removed.

--- packages/turf-helpers/index.ts
+++ packages/turf-helpers/index.ts
@@ -869,33 +869,3 @@ export function validateBBox(bbox: any): void {
     }
   });
 }
-
-/**
- * Validate Id
- *
- * @private
- * @param {any} id Id to validate
- * @returns {void}
- * @throws {Error} if Id is not valid
- * @example
- * validateId([-180, -40, 110, 50])
- * //=Error
- * validateId([-180, -40])
- * //=Error
- * validateId('Foo')
- * //=OK
- * validateId(5)
- * //=OK
- * validateId(null)
- * //=Error
- * validateId(undefined)
- * //=Error
- */
-export function validateId(id: any): void {
-  if (!id) {
-    throw new Error("id is required");
-  }
-  if (["string", "number"].indexOf(typeof id) === -1) {
-    throw new Error("id must be a number or a string");
-  }
-}
