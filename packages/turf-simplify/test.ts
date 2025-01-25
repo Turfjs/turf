e8d47a1571d162cdf55af6ec7b31cf937936fdca
James Beard <james@smallsaucepan.com>
2025-01-25 15:45:49 +1100

    Fixed simplify to not get stuck in an infinite loop when it can't reduce the tolerance far enough to generate a valid polygon. Tidied up docs.

--- packages/turf-simplify/test.ts
+++ packages/turf-simplify/test.ts
@@ -214,3 +214,60 @@ test("simplify -- issue #918", (t) => {
   );
   t.end();
 });
+
+test("simplify - issue 1788 - infinite loop", (t) => {
+  // For particularly small polygons simplify was getting stuck in an
+  // infinite loop.
+
+  // https://github.com/Turfjs/turf/issues/1788#issue-521052548
+  const poly1 = polygon([
+    [
+      [11.662180661499999, 50.1081498005],
+      [11.662192661499999, 50.108041800500004],
+      [11.6621866615, 50.1080958005],
+      [11.662180661499999, 50.1081498005],
+    ],
+  ]);
+  simplify(poly1, {
+    tolerance: 0.000001,
+    highQuality: true,
+    mutate: false,
+  });
+  t.pass("issue 1788 test 1 didn't hang");
+
+  // https://github.com/Turfjs/turf/issues/1788#issuecomment-951109683
+  const poly2 = polygon([
+    [
+      [4.0881641, 6.8121605],
+      [4.0881639, 6.8121607],
+      [4.0881638, 6.8121608],
+      [4.0881641, 6.8121605],
+    ],
+  ]);
+  simplify(poly2);
+  t.pass("issue 1788 test 2 didn't hang");
+
+  // https://github.com/Turfjs/turf/issues/1788#issuecomment-1362073785
+  const poly3 = multiPolygon([
+    [
+      [
+        [-10.06762725, -17.428977611],
+        [-10.067626613, -17.428977323],
+        [-10.067625943, -17.428976957],
+        [-10.06762725, -17.428977611],
+      ],
+    ],
+    [
+      [
+        [-10.067625943, -17.428976957],
+        [-10.067599027, -17.428963499],
+        [-10.067625941, -17.428976956],
+        [-10.067625943, -17.428976957],
+      ],
+    ],
+  ]);
+  simplify(poly3, { tolerance: 0.00001, highQuality: true });
+  t.pass("issue 1788 test 3 didn't hang");
+
+  t.end();
+});
