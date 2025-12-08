a3fba0f22dda1476916188f0dc11ec8e52dee8ce
Matthew Fedderly <24275386+mfedderly@users.noreply.github.com>
2025-12-08 08:30:47 -0500

    Remove arc.d.ts which is no longer needed, regenerate pnpm-lock

--- packages/turf-great-circle/lib/arc.d.ts
+++
@@ -1,8 +0,0 @@
-export class GreatCircle {
-  constructor(
-    start: { x: number; y: number },
-    end: { x: number; y: number },
-    properties?: any
-  );
-  Arc(npoints: number, options: { offset: number }): { json: () => any };
-}
