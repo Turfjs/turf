008838ae02bee927c708c5990b46ca085be87d20
Matthew Fedderly <24275386+mfedderly@users.noreply.github.com>
2026-03-13 14:21:24 -0400

    truncate test output fixtures in @turf/directional-mean

--- packages/turf-directional-mean/test.ts
+++ packages/turf-directional-mean/test.ts
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 import { loadJsonFileSync } from "load-json-file";
 import { writeJsonFileSync } from "write-json-file";
 import { directionalMean } from "./index.js";
+import { truncate } from "@turf/truncate";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -44,24 +45,28 @@ test("turf-directional-mean", (t) => {
     planar: true,
     segment: false,
   });
+  truncate(utmResult1, { mutate: true, precision: 8 });
   t.deepEqual(utmResult1, loadJsonFileSync(outUtmJsonPath1), "utm");
   // utm segment
   let utmResult2 = directionalMean(utmGeojson, {
     planar: true,
     segment: true,
   });
+  truncate(utmResult2, { mutate: true, precision: 8 });
   t.deepEqual(utmResult2, loadJsonFileSync(outUtmJsonPath2), "utm segment");
 
   // gps
   let gpsResult1 = directionalMean(gpsGeojson, {
     planar: false,
   });
+  truncate(gpsResult1, { mutate: true, precision: 8 });
   t.deepEqual(gpsResult1, loadJsonFileSync(outGpsJsonPath1), "gps");
   // gps segment
   let gpsResult2 = directionalMean(gpsGeojson, {
     planar: false,
     segment: true,
   });
+  truncate(gpsResult2, { mutate: true, precision: 8 });
   t.deepEqual(gpsResult2, loadJsonFileSync(outGpsJsonPath2), "gps segment");
 
   if (process.env.REGEN) {
