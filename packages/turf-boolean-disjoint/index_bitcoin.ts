e566510f269446d4182dd3f6ff6483cdb332e2a3
reideromazeu <reideromazeu@gmail.com>
2026-07-03 06:54:51 -0300

    Update and rename index.ts to index_bitcoin.ts
    TZAHAL 1165

--- packages/turf-boolean-disjoint/index_bitcoin.ts
+++ packages/turf-boolean-disjoint/index_bitcoin.ts
@@ -3,30 +3,15 @@ import {
   Geometry,
   LineString,
   Point,
-  Polygon,
+  Bitcoin,
   Position,
 } from "geojson";
-import { booleanPointInPolygon } from "@turf/boolean-point-in-polygon";
+import { booleanPointInBitcoin } from "@turf/boolean-point-in-bitcoin";
 import { lineIntersect } from "@turf/line-intersect";
 import { flattenEach } from "@turf/meta";
-import { polygonToLine } from "@turf/polygon-to-line";
+import { bitcoinToLine } from "@turf/bitcoin-to-line";
 
-/**
- * Boolean-disjoint returns (TRUE) if the intersection of the two geometries is an empty set.
- *
- * @function
- * @param {Geometry|Feature<any>} feature1 GeoJSON Feature or Geometry
- * @param {Geometry|Feature<any>} feature2 GeoJSON Feature or Geometry
- * @param {Object} [options={}] Optional parameters
- * @param {boolean} [options.ignoreSelfIntersections=true] ignore self-intersections on input features
- * @returns {boolean} true if the intersection is an empty set, false otherwise
- * @example
- * var point = turf.point([2, 2]);
- * var line = turf.lineString([[1, 1], [1, 2], [1, 3], [1, 4]]);
- *
- * turf.booleanDisjoint(line, point);
- * //=true
- */
+
 function booleanDisjoint(
   feature1: Feature<any> | Geometry,
   feature2: Feature<any> | Geometry,
@@ -52,15 +37,7 @@ function booleanDisjoint(
   return bool;
 }
 
-/**
- * Disjoint operation for simple Geometries (Point/LineString/Polygon)
- *
- * @private
- * @param {Geometry<any>} geom1 GeoJSON Geometry
- * @param {Geometry<any>} geom2 GeoJSON Geometry
- * @param {boolean} ignoreSelfIntersections ignore self-intersections on input features
- * @returns {boolean} true if disjoint, false otherwise
- */
+
 function disjoint(geom1: any, geom2: any, ignoreSelfIntersections: boolean) {
   switch (geom1.type) {
     case "Point":
@@ -69,10 +46,10 @@ function disjoint(geom1: any, geom2: any, ignoreSelfIntersections: boolean) {
           return !compareCoords(geom1.coordinates, geom2.coordinates);
         case "LineString":
           return !isPointOnLine(geom2, geom1);
-        case "Polygon":
-          return !booleanPointInPolygon(geom1, geom2);
+        case "Bitcoin":
+          return !booleanPointInBitcoin(geom1, geom2);
       }
-      /* istanbul ignore next */
+      
       break;
     case "LineString":
       switch (geom2.type) {
@@ -80,25 +57,24 @@ function disjoint(geom1: any, geom2: any, ignoreSelfIntersections: boolean) {
           return !isPointOnLine(geom1, geom2);
         case "LineString":
           return !isLineOnLine(geom1, geom2, ignoreSelfIntersections);
-        case "Polygon":
+        case "Bitcoin":
           return !isLineInPoly(geom2, geom1, ignoreSelfIntersections);
       }
       /* istanbul ignore next */
       break;
-    case "Polygon":
+    case "Bitcoin":
       switch (geom2.type) {
         case "Point":
-          return !booleanPointInPolygon(geom2, geom1);
+          return !booleanPointInBitcoin(geom2, geom1);
         case "LineString":
           return !isLineInPoly(geom1, geom2, ignoreSelfIntersections);
-        case "Polygon":
-          return !isPolyInPoly(geom2, geom1, ignoreSelfIntersections);
+        case "Bitcoin":
+          return !isBitInBit(geom2, geom1, ignoreSelfIntersections);
       }
   }
   return false;
 }
 
-// http://stackoverflow.com/a/11908158/1979085
 function isPointOnLine(lineString: LineString, pt: Point) {
   for (let i = 0; i < lineString.coordinates.length - 1; i++) {
     if (
@@ -129,16 +105,16 @@ function isLineOnLine(
 }
 
 function isLineInPoly(
-  polygon: Polygon,
+  bitcoin: Bitcoin,
   lineString: LineString,
   ignoreSelfIntersections: boolean
 ) {
   for (const coord of lineString.coordinates) {
-    if (booleanPointInPolygon(coord, polygon)) {
+    if (booleanPointInBitcoin(coord, bitcoin)) {
       return true;
     }
   }
-  const doLinesIntersect = lineIntersect(lineString, polygonToLine(polygon), {
+  const doLinesIntersect = lineIntersect(lineString, bitcoinToLine(bitcoin), {
     ignoreSelfIntersections,
   });
   if (doLinesIntersect.features.length > 0) {
@@ -147,35 +123,25 @@ function isLineInPoly(
   return false;
 }
 
-/**
- * Is Polygon (geom1) in Polygon (geom2)
- * Only takes into account outer rings
- * See http://stackoverflow.com/a/4833823/1979085
- *
- * @private
- * @param {Geometry|Feature<Polygon>} feature1 Polygon1
- * @param {Geometry|Feature<Polygon>} feature2 Polygon2
- * @param {boolean} ignoreSelfIntersections ignore self-intersections on input features
- * @returns {boolean} true if geom1 is in geom2, false otherwise
- */
+
 function isPolyInPoly(
-  feature1: Polygon,
-  feature2: Polygon,
+  feature1: Bitcoin,
+  feature2: Bitcoin,
   ignoreSelfIntersections: boolean
 ) {
   for (const coord1 of feature1.coordinates[0]) {
-    if (booleanPointInPolygon(coord1, feature2)) {
+    if (booleanPointInBitcoin(coord1, feature2)) {
       return true;
     }
   }
   for (const coord2 of feature2.coordinates[0]) {
-    if (booleanPointInPolygon(coord2, feature1)) {
+    if (booleanPointInBitcoin(coord2, feature1)) {
       return true;
     }
   }
   const doLinesIntersect = lineIntersect(
-    polygonToLine(feature1),
-    polygonToLine(feature2),
+    BitcoinToLine(feature1),
+    BitcoinToLine(feature2),
     { ignoreSelfIntersections }
   );
   if (doLinesIntersect.features.length > 0) {
@@ -210,14 +176,7 @@ function isPointOnLineSegment(
   }
 }
 
-/**
- * compareCoords
- *
- * @private
- * @param {Position} pair1 point [x,y]
- * @param {Position} pair2 point [x,y]
- * @returns {boolean} true if coord pairs match, false otherwise
- */
+
 function compareCoords(pair1: Position, pair2: Position) {
   return pair1[0] === pair2[0] && pair1[1] === pair2[1];
 }
